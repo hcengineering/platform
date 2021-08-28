d764d18a4ab845ef7e894aef2146c1da50a8e991
Andrey Platov <andrey@hardcoreeng.com>
2021-08-28 09:47:05 +0200

    introduce "contributing" client
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

---
+++ server/contrib/src/connection.ts
@@ -0,0 +1,51 @@
+//
+// Copyright © 2020, 2021 Anticrm Platform Contributors.
+// Copyright © 2021 Hardcore Engineering Inc.
+//
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import type { Tx, WithTx } from '@anticrm/core'
+import { serialize } from '@anticrm/platform'
+import WebSocket from 'ws'
+
+/**
+ * @public
+ */
+export class ContributingClient implements WithTx {
+  constructor (private readonly websocket: WebSocket) {
+  }
+
+  async tx (tx: Tx): Promise<void> {
+    this.websocket.send(serialize({
+      method: 'tx',
+      params: [tx]
+    }))
+  }
+}
+
+/**
+ * @public
+ * @param url -
+ * @returns
+ */
+export async function createContributingClient (url: string): Promise<ContributingClient> {
+  return await new Promise((resolve, reject) => {
+    const websocket = new WebSocket(url)
+    websocket.on('open', () => {
+      resolve(new ContributingClient(websocket))
+    })
+    websocket.on('error', (err) => {
+      reject(err)
+    })
+  })
+}
