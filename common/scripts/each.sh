7d22264c297fb820b6de9be7ac477fb8bff29904
Andrey Platov <andrey@hardcoreeng.com>
2021-09-11 09:53:22 +0200

    fix
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

--- common/scripts/each.sh
+++ common/scripts/each.sh
@@ -24,6 +24,8 @@ roots=$(node $sourceDir/install-run-rush.js list -f --json | grep "fullPath" | c
 for i in $roots
 do  
   pushd ${i}
+  echo ${runScript}
+  node ${runScript}
   checkScript=$(node ${runScript} | grep $1: | wc -l)
   if [ $checkScript -gt 0 ]; then
     node ${runScript} $@  
