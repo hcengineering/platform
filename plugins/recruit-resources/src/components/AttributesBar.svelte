4fd981b3039f066d17a19c1ea2e561c503f2d558
Alexander Platov <sas_lord@mail.ru>
2021-10-04 17:32:27 +0300

    Add AttributesBar, YesNoPresenter (#230)
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

---
+++ plugins/recruit-resources/src/components/AttributesBar.svelte
@@ -0,0 +1,71 @@
+<!--
+// Copyright © 2020 Anticrm Platform Contributors.
+// 
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 
+// See the License for the specific language governing permissions and
+// limitations under the License.
+-->
+
+<script lang="ts">
+  import { CircleButton, Label } from '@anticrm/ui'
+  import Location from './icons/Location.svelte'
+  import YesNoPresenter from './YesNoPresenter.svelte'
+
+  export let minimize: boolean = true
+</script>
+
+<div class="flex-row-center small-text">
+  <div class="icon-wrap column">
+    <CircleButton icon={Location} size={'large'} />
+    {#if !minimize}
+      <div class="noicon-column">
+        <Label label={'Location'} />
+        <div class="value">Moscow</div>
+      </div>
+    {/if}
+  </div>
+  <div class="noicon-column column">
+    <Label label={'Onsite'} />
+    <YesNoPresenter state={'yes'} />
+  </div>
+  <div class="noicon-column column">
+    <Label label={'Remote'} />
+    <YesNoPresenter state={'no'} />
+  </div>
+</div>
+
+<style lang="scss">
+  .icon-wrap {
+    display: flex;
+    align-items: center;
+  }
+  .noicon-column {
+    display: flex;
+    flex-direction: column;
+  }
+  .icon-wrap .noicon-column { margin-left: .5rem; }
+  .value {
+    font-weight: 500;
+    color: var(--theme-caption-color);
+  }
+  .column + .column {
+    position: relative;
+    margin-left: 3rem;
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: -1.5rem;
+      width: 1px;
+      background-color: var(--theme-bg-accent-hover);
+    }
+  }
+</style>
---
+++ plugins/recruit-resources/src/components/YesNoPresenter.svelte
@@ -0,0 +1,56 @@
+<!--
+// Copyright © 2020, 2021 Anticrm Platform Contributors.
+// 
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 
+// See the License for the specific language governing permissions and
+// limitations under the License.
+-->
+
+<script lang="ts">
+  import { Label } from '@anticrm/ui'
+
+  export let state: 'yes' | 'no' | 'unknown'
+</script>
+
+<div class="flex-row-center yesno-container {state}" on:click={() => {
+  if (state === 'yes') state = 'no'
+  else if (state === 'no') state = 'unknown'
+  else state = 'yes'
+}}>
+  <svg class="svg-small" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+    <circle class={state} cx="8" cy="8" r="6"/>
+    {#if state === 'yes'}
+      <polygon fill="#fff" points="7.4,10.9 4.9,8.4 5.7,7.6 7.3,9.1 10.2,5.6 11.1,6.4 "/>
+    {:else if state === 'no'}
+      <polygon fill="#fff" points="10.7,6 10,5.3 8,7.3 6,5.3 5.3,6 7.3,8 5.3,10 6,10.7 8,8.7 10,10.7 10.7,10 8.7,8 "/>
+    {:else}
+      <path fill="#fff" d="M7.3,9.3h1.3V9c0.1-0.5,0.6-0.9,1.1-1.4c0.4-0.4,0.8-0.9,0.8-1.6c0-1.1-0.8-1.8-2.2-1.8c-1.4,0-2.4,0.8-2.5,2.2 h1.4c0.1-0.6,0.4-1,1-1C8.8,5.4,9,5.7,9,6.2c0,0.4-0.3,0.7-0.7,1.1c-0.5,0.5-1,0.9-1,1.7V9.3z M8,11.6c0.5,0,0.9-0.4,0.9-0.9 c0-0.5-0.4-0.9-0.9-0.9c-0.5,0-0.9,0.4-0.9,0.9C7.1,11.2,7.5,11.6,8,11.6z"/>
+    {/if}
+  </svg>
+  <span><Label label={state} /></span>
+</div>
+
+<style lang="scss">
+  .yesno-container {
+    max-width: fit-content;
+    user-select: none;
+    cursor: pointer;
+
+    &.yes { fill: #77C07B; }
+    &.no { fill: #F96E50; }
+    &.unknown { fill: #77818E; }
+    span {
+      margin-left: .25rem;
+      text-transform: capitalize;
+      font-weight: 500;
+      color: var(--theme-caption-color);
+    }
+  }
+</style>
