a5809075ec3446b733106e1e969d191e97662a63
Andrey Platov <andrey@hardcoreeng.com>
2021-10-14 17:51:23 +0200

    bring kanban back
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

--- models/recruit/src/index.ts
+++ models/recruit/src/index.ts
@@ -145,6 +145,20 @@ export function createModel (builder: Builder): void {
     config: ['', '$lookup.candidate', '$lookup.state', '$lookup.candidate.city', '$lookup.candidate.channels']
   })
 
+  builder.createDoc(view.class.Viewlet, core.space.Model, {
+    attachTo: recruit.class.Applicant,
+    descriptor: view.viewlet.Kanban,
+    open: recruit.component.EditCandidate,
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    options: {
+      lookup: {
+        candidate: recruit.class.Candidate,
+        state: core.class.State
+      }
+    } as FindOptions<Doc>, // TODO: fix
+    config: ['$lookup.candidate', '$lookup.state', '$lookup.candidate.city', '$lookup.candidate.channels']
+  })
+
   builder.mixin(recruit.class.Applicant, core.class.Class, view.mixin.KanbanCard, {
     card: recruit.component.KanbanCard
   })
