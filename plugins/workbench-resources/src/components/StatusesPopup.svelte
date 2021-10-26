33356bfc618e4599801c28ee27d922c9698886c7
Alexander Platov <sas_lord@mail.ru>
2021-10-26 12:29:15 +0300

    Add StatusesPopup (#288)
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

---
+++ plugins/workbench-resources/src/components/StatusesPopup.svelte
@@ -0,0 +1,58 @@
+<!--
+// Copyright © 2020 Anticrm Platform Contributors.
+// 
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 
+// See the License for the specific language governing permissions and
+// limitations under the License.
+-->
+
+<script lang="ts">
+  // import type { IntlString, Asset, Resource } from '@anticrm/platform'
+  import { createEventDispatcher } from 'svelte'
+  import { Label } from '@anticrm/ui'
+  import Delete from './icons/Delete.svelte'
+
+  const dispatch = createEventDispatcher()
+</script>
+
+<div class="flex-col popup">
+  <div class="flex-row-center red-color menu-item" on:click={() => { dispatch('close', 'delete') }}>
+    <div class="icon">
+      <Delete size={'medium'} />
+    </div>
+    <div class="flex-grow"><Label label={'Delete'} /></div>
+  </div>
+</div>
+
+<style lang="scss">
+  .popup {
+    padding: .5rem;
+    min-width: 12rem;
+    background-color: var(--theme-button-bg-focused);
+    border: 1px solid var(--theme-button-border-enabled);
+    border-radius: .75rem;
+    box-shadow: 0 .75rem 1.25rem rgba(0, 0, 0, .2);
+  }
+
+  .menu-item {
+    display: flex;
+    align-items: center;
+    padding: .5rem;
+    border-radius: .5rem;
+    cursor: pointer;
+
+    .icon {
+      margin-right: .75rem;
+      transform-origin: center center;
+      transform: scale(.75);
+    }
+    &:hover { background-color: var(--theme-button-bg-hovered); }
+  }
+</style>
