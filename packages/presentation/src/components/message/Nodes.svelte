0076c4bb4eccab2a6ebbd4b6da1938a6678c706d
Andrey Platov <andrey@hardcoreeng.com>
2021-12-02 18:29:43 +0100

    fixes #480, fixes #482
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

--- packages/presentation/src/components/message/Nodes.svelte
+++ packages/presentation/src/components/message/Nodes.svelte
@@ -37,12 +37,16 @@
         <code><svelte:self nodes={node.childNodes}/></code>
       {:else if node.nodeName === 'BR'}
         <br/>
+      {:else if node.nodeName === 'HR'}
+        <hr/>
       {:else if node.nodeName === 'H1'}
         <h1><svelte:self nodes={node.childNodes}/></h1>
       {:else if node.nodeName === 'H2'}
         <h2><svelte:self nodes={node.childNodes}/></h2>
       {:else if node.nodeName === 'UL'}
         <ul><svelte:self nodes={node.childNodes}/></ul>
+      {:else if node.nodeName === 'OL'}
+        <ol><svelte:self nodes={node.childNodes}/></ol>
       {:else if node.nodeName === 'LI'}
         <li><svelte:self nodes={node.childNodes}/></li>
       {:else if node.nodeName === 'SPAN'}
