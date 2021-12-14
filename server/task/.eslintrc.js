352a0066c59590c973004c2eccd41e8a657c66ff
Ilya Sumbatyants <ilya.sumb@gmail.com>
2021-12-14 14:09:11 +0700

    Get rid of server-view
    Signed-off-by: Ilya Sumbatyants <ilya.sumb@gmail.com>

--- server/task/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['./node_modules/@anticrm/platform-rig/profiles/default/config/eslint.config.json'],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: './tsconfig.json'
-  }
-}
