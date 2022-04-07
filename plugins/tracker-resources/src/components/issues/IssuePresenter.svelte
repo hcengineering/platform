ad2e1e21ebd5a53d9d692ab35e95e4e40aeccc60
Artyom Grigorovich <grigorovichartyom@gmail.com>
2022-04-07 13:50:38 +0700

    Add priority icon
    Signed-off-by: Artyom Grigorovich <grigorovichartyom@gmail.com>

--- plugins/tracker-resources/src/components/issues/IssuePresenter.svelte
+++ plugins/tracker-resources/src/components/issues/IssuePresenter.svelte
@@ -16,12 +16,21 @@
 <script lang="ts">
   import { getClient } from '@anticrm/presentation'
   import type { Issue, Team } from '@anticrm/tracker'
+  import { IssuePriority } from '@anticrm/tracker'
   import { Icon, showPanel } from '@anticrm/ui'
   import tracker from '../../plugin'
 
   export let value: Issue
   export let currentTeam: Team
   export let inline: boolean = false
+
+  const priorityIconsMap = Object.freeze({
+    [IssuePriority.NoPriority]: tracker.icon.PriorityNoPriority,
+    [IssuePriority.Low]: tracker.icon.PriorityLow,
+    [IssuePriority.Medium]: tracker.icon.PriorityMedium,
+    [IssuePriority.High]: tracker.icon.PriorityHigh,
+    [IssuePriority.Urgent]: tracker.icon.PriorityUrgent
+  })
 
   const client = getClient()
   const shortLabel = client.getHierarchy().getClass(value._class).shortLabel
@@ -37,6 +46,9 @@
     class:inline-presenter={inline}
     on:click={show}
   >
+    <div class="icon">
+      <Icon icon={priorityIconsMap[value.priority]} size={'small'} />
+    </div>
     <div class="icon">
       <Icon icon={tracker.icon.Issue} size={'small'} />
     </div>
