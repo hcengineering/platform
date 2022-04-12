f0acff14ed3881fbcc198d501acc0cffa44f1c3f
Anna No <anna.no@xored.com>
2022-04-12 14:10:13 +0700

    Board: add Card DatePresenter.svelte
    Signed-off-by: Anna No <anna.no@xored.com>

--- plugins/board-resources/src/components/presenters/DatePresenter.svelte
+++ plugins/board-resources/src/components/presenters/DatePresenter.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
   import type { CardDate } from '@anticrm/board'
-  import { Button, CheckBox } from '@anticrm/ui'
+  import { CheckBox, DatePresenter } from '@anticrm/ui'
 
   // TODO: implement
   export let value: CardDate
-  const dateLabel = value.dueDate ? `${value.startDate} - ${value.dueDate}` : value.startDate
+
 </script>
 
 {#if value}
-  <div class="flex">
+  <div class="flex-presenter flex-gap-1 h-full">
     <CheckBox checked={value.isChecked} />
-    <Button size="small" kind="transparent" on:click>
-      <div class="flex-center h-full w-full" slot="content">
-        {dateLabel}
+    <div class="flex-center h-full">
+      <div class="flex-row-center background-button-bg-color border-radius-1 w-full">
+        {#if value.startDate && value.dueDate}
+          <DatePresenter bind:value={value.startDate} showIcon={false} />
+          -
+          <DatePresenter bind:value={value.dueDate} withTime={true} showIcon={false} />
+        {:else if value.startDate}
+          <DatePresenter bind:value={value.startDate} />
+        {:else if value.dueDate}
+          <DatePresenter bind:value={value.dueDate} withTime={true} showIcon={false} />
+        {/if}
       </div>
-    </Button>
+    </div>
   </div>
 {/if}
