57287812e8e6610322beda7c270f0c35ebbfd046
Andrey Sobolev <haiodo@gmail.com>
2022-04-18 17:11:43 +0700

    Optimize assets build process
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- plugins/notification-assets/.eslintrc.js
+++ plugins/notification-assets/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['./node_modules/@anticrm/platform-rig/profiles/default/config/eslint.config.json'],
+  extends: ['./node_modules/@anticrm/platform-rig/profiles/assets/config/eslint.config.json'],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: './tsconfig.json'
