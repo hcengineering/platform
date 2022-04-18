ffa04233fe9235768336f8e12eef43c99e39a585
Alex <41288429+Dvinyanin@users.noreply.github.com>
2022-04-18 17:25:52 +0700

    Fix date (#1430)
    Signed-off-by: Dvinyanin Alexandr <dvinyanin.alexandr@gmail.com>

--- plugins/board-resources/src/components/editor/CardDetails.svelte
+++ plugins/board-resources/src/components/editor/CardDetails.svelte
@@ -99,7 +99,9 @@
       <div class="text-md font-medium">
         <Label label={board.string.Dates} />
       </div>
-      <DatePresenter {value} on:click={dateHandler} />
+      {#key value.date}
+        <DatePresenter {value} on:click={dateHandler} />
+      {/key}
     </div>
   {/if}
 {/if}
