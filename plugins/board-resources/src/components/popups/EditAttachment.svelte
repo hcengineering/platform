2e5982f2fe1b6d6dd13a2c8f570bc02ca88029fd
Alex <41288429+Dvinyanin@users.noreply.github.com>
2022-04-20 13:07:22 +0700

    Add AttachmentPresenter (#1452)
    Signed-off-by: Dvinyanin Alexandr <dvinyanin.alexandr@gmail.com>

---
+++ plugins/board-resources/src/components/popups/EditAttachment.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+  import { Label, Button, ActionIcon, IconClose, EditBox } from '@anticrm/ui'
+  import board from '../../plugin'
+  import { getClient } from '@anticrm/presentation';
+  import { Attachment } from '@anticrm/attachment';
+
+  export let object: Attachment
+
+  let {name} = object
+
+  const client = getClient()
+  const dispatch = createEventDispatcher()
+</script>
+
+<div class="antiPopup antiPopup-withHeader antiPopup-withCategory w-85">
+  <div class="ap-space"/>
+  <div class="flex-row-center header">
+    <div class="flex-center flex-grow">
+      <Label label={board.string.Edit}/>
+    </div>
+    <div class="close-icon">
+      <ActionIcon icon={IconClose} size={'small'} action={() => {dispatch("close")}} />
+    </div>
+  </div>
+  <div class="ap-space bottom-divider"/>
+  <div class="ap-category">
+    <div class="ap-categoryItem">
+      <EditBox bind:value={name} maxWidth="18rem" label={board.string.LinkName} placeholder={board.string.LinkName}/>
+    </div>
+  </div>
+  <div class="ap-footer">
+    <Button
+      size={'small'}
+      label={board.string.Update}
+      kind={'primary'}
+      on:click={() => {
+        if (!name) return
+        client.update(object, {name})
+        dispatch('close')
+      }}
+    />
+  </div>
+</div>
