346ef424f2c642a7cd78875951247a04a5468f5f
Andrey Sobolev <haiodo@gmail.com>
2022-04-29 23:52:27 +0700

    Fix formatting issues
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- plugins/board-resources/src/components/Archive.svelte
+++ plugins/board-resources/src/components/Archive.svelte
@@ -1,29 +1,33 @@
 <script lang="ts">
-    import { Ref, Space } from '@anticrm/core'
-    import { Button } from '@anticrm/ui'
-    import board from '../plugin'
-    import CardsArchive from './CardArchive.svelte'
-    import ListArchive from './ListArchive.svelte'
-    import TextArea from '@anticrm/ui/src/components/TextArea.svelte'
+  import { Ref, Space } from '@anticrm/core'
+  import { Button } from '@anticrm/ui'
+  import board from '../plugin'
+  import CardsArchive from './CardArchive.svelte'
+  import ListArchive from './ListArchive.svelte'
+  import TextArea from '@anticrm/ui/src/components/TextArea.svelte'
 
-    export let space: Ref<Space>
+  export let space: Ref<Space>
 
-    let isCardArchive = true
-    let search: string = ''
-    $: query = { space, title: { $like: '%' + search + '%' } }
-    $: label = isCardArchive
-      ? board.string.SwitchToLists
-      : board.string.SwitchToCards
+  let isCardArchive = true
+  let search: string = ''
+  $: query = { space, title: { $like: '%' + search + '%' } }
+  $: label = isCardArchive ? board.string.SwitchToLists : board.string.SwitchToCards
 </script>
 
 <div class="p-4">
-    <Button {label} width={'100%'}  on:click={() => { isCardArchive = !isCardArchive }} />
-    <div class="pt-4">
-        <TextArea bind:value={search} placeholder={board.string.SearchArchive}/>
-    </div>
+  <Button
+    {label}
+    width={'100%'}
+    on:click={() => {
+      isCardArchive = !isCardArchive
+    }}
+  />
+  <div class="pt-4">
+    <TextArea bind:value={search} placeholder={board.string.SearchArchive} />
+  </div>
 </div>
 {#if isCardArchive}
-    <CardsArchive {query}/>
+  <CardsArchive {query} />
 {:else}
-    <ListArchive {query}/>
+  <ListArchive {query} />
 {/if}
--- plugins/board-resources/src/components/MenuMainPage.svelte
+++ plugins/board-resources/src/components/MenuMainPage.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-    import { Button } from '@anticrm/ui'
-    import { createEventDispatcher } from 'svelte'
-    import board from '@anticrm/board'
-    import plugin from '../plugin'
+  import { Button } from '@anticrm/ui'
+  import { createEventDispatcher } from 'svelte'
+  import board from '@anticrm/board'
+  import plugin from '../plugin'
 
-    const dispatch = createEventDispatcher()
+  const dispatch = createEventDispatcher()
 </script>
 
 <div class="p-4">
-    <Button label={plugin.string.Archive} width={'100%'}  on:click={() => { dispatch('change', board.menuPageId.Archive) }} />
+  <Button
+    label={plugin.string.Archive}
+    width={'100%'}
+    on:click={() => {
+      dispatch('change', board.menuPageId.Archive)
+    }}
+  />
 </div>
