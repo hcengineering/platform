12602cea3f7e6d90ac4f2ba090adad77225fe54d
Alex <41288429+Dvinyanin@users.noreply.github.com>
2022-05-02 11:22:42 +0700

    Add board activity (#1613)
    Signed-off-by: Dvinyanin Alexandr <dvinyanin.alexandr@gmail.com>

--- plugins/board-resources/src/components/MenuMainPage.svelte
+++ plugins/board-resources/src/components/MenuMainPage.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
-  import { Button } from '@anticrm/ui'
+  import { Button, Component } from '@anticrm/ui'
   import { createEventDispatcher } from 'svelte'
+  import activity from '@anticrm/activity'
   import board from '@anticrm/board'
   import plugin from '../plugin'
+  import { createQuery } from '@anticrm/presentation'
+  import core, { Ref, Space } from '@anticrm/core'
+
+  export let space: Ref<Space>
+
+  let object: Space
 
   const dispatch = createEventDispatcher()
+  const query = createQuery()
+  $: query.query(core.class.Space, { _id: space }, (result) => {
+    ;[object] = result
+  })
 </script>
 
 <div class="p-4">
@@ -15,4 +26,7 @@
       dispatch('change', board.menuPageId.Archive)
     }}
   />
+  {#if object}
+    <Component is={activity.component.Activity} props={{ object, showCommenInput: false, transparent: true }} />
+  {/if}
 </div>
