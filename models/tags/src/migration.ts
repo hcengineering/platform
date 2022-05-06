da1b2b95934a3b78730537460e7f6b84e61b1f72
Denis Bykhov <80476319+BykhovDenis@users.noreply.github.com>
2022-05-06 14:59:37 +0600

    Bump tags version and migrations other fix
    Signed-off-by: Denis Bykhov <80476319+BykhovDenis@users.noreply.github.com>

--- models/tags/src/migration.ts
+++ models/tags/src/migration.ts
@@ -1,9 +1,17 @@
 import core, { TxOperations } from '@anticrm/core'
 import { MigrateOperation, MigrationClient, MigrationUpgradeClient } from '@anticrm/model'
+import { DOMAIN_TAGS } from './index'
 import tags from './plugin'
 
 export const tagsOperation: MigrateOperation = {
-  async migrate (client: MigrationClient): Promise<void> {},
+  async migrate (client: MigrationClient): Promise<void> {
+    client.update(DOMAIN_TAGS, {
+      _class: tags.class.TagElement,
+      category: "tags:category:Other"
+    }, {
+      category: "recruit:category:Other"
+    })
+  },
   async upgrade (client: MigrationUpgradeClient): Promise<void> {
     const tx = new TxOperations(client, core.account.System)
     const current = await tx.findOne(core.class.Space, {
