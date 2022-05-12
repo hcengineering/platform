927503e4992b9bf8f2a73870438a2220629847b9
Andrey Sobolev <haiodo@gmail.com>
2022-05-12 10:45:18 +0700

    Fix review comment
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- packages/ui/src/components/FocusHandler.svelte
+++ packages/ui/src/components/FocusHandler.svelte
@@ -4,14 +4,10 @@
   export let manager: FocusManager
 
   function handleKey (evt: KeyboardEvent): void {
-    if (evt.code === 'Tab') {
-      console.log('FOCUS Container', evt)
-
-      if (manager.hasFocus()) {
-        evt.preventDefault()
-        evt.stopPropagation()
-        manager.next(evt.shiftKey ? -1 : 1)
-      }
+    if (evt.code === 'Tab' && manager.hasFocus()) {
+      evt.preventDefault()
+      evt.stopPropagation()
+      manager.next(evt.shiftKey ? -1 : 1)
     }
   }
 </script>
