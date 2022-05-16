0231234f9a892975907e4d28bd2e573f893cd43c
Denis Bykhov <80476319+BykhovDenis@users.noreply.github.com>
2022-05-16 11:02:40 +0600

    Employee status requests flood fix (#1759)

--- plugins/contact-resources/src/components/EmployeePreviewPopup.svelte
+++ plugins/contact-resources/src/components/EmployeePreviewPopup.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
   import { Employee, EmployeeAccount, formatName, Status } from '@anticrm/contact'
-  import { getCurrentAccount, Ref, Space, WithLookup } from '@anticrm/core'
+  import { getCurrentAccount, Ref, Hierarchy, WithLookup } from '@anticrm/core'
   import { Avatar, createQuery, getClient } from '@anticrm/presentation'
-  import { Button, Label, showPopup } from '@anticrm/ui'
+  import { Button, getPanelURI, Label, showPopup } from '@anticrm/ui'
   import EmployeeSetStatusPopup from './EmployeeSetStatusPopup.svelte'
   import contact from '../plugin'
   import EmployeeStatusPresenter from './EmployeeStatusPresenter.svelte'
   import Edit from './icons/Edit.svelte'
   import { createEventDispatcher } from 'svelte'
+  import view from '@anticrm/view'
 
   export let employeeId: Ref<Employee>
-  export let space: Ref<Space>
 
   const client = getClient()
   const me = (getCurrentAccount() as EmployeeAccount).employee
   $: editable = employeeId === me
 
-  const stattusQuery = createQuery()
-  let status: WithLookup<Status>
-  $: employee = status?.$lookup?.attachedTo
-  stattusQuery.query(contact.class.Status, { attachedTo: employeeId }, (res) => (status = res[0]), {
+  const employeeQuery = createQuery()
+  $: status = employee?.$lookup?.statuses?.[0]
+  let employee: WithLookup<Employee> | undefined
+  employeeQuery.query(contact.class.Employee, { _id: employeeId }, (res) => (employee = res[0]), {
     lookup: {
-      attachedTo: contact.class.Employee
+      _id: { statuses: contact.class.Status }
     }
   })
 
@@ -41,7 +41,7 @@
         } else if (status && !newStatus) {
           client.removeDoc(contact.class.Status, status.space, status._id)
         } else {
-          client.createDoc(contact.class.Status, space, {
+          client.createDoc(contact.class.Status, employee!.space, {
             attachedTo: employeeId,
             attachedToClass: contact.class.Employee,
             collection: 'statuses',
@@ -56,28 +56,33 @@
 </script>
 
 <div class="antiPopup p-4 flex-col">
-  <div class="flex-col-center pb-2">
-    <Avatar size="x-large" avatar={employee?.avatar} />
-  </div>
-  <div class="pb-2">{formatName(employee?.name ?? '')}</div>
-  {#if status}
-    <div class="pb-2">
-      <Label label={contact.string.Status} />
-      <div class="flex-row-stretch statusContainer">
-        <div class="pr-2">
-          <EmployeeStatusPresenter {employeeId} withTooltip={false} />
-        </div>
-        {#if editable}
-          <div class="setStatusButton">
-            <Button icon={Edit} title={contact.string.SetStatus} on:click={onEdit} />
+  {#if employee}
+    <div class="flex-col-center pb-2">
+      <Avatar size="x-large" avatar={employee?.avatar} />
+    </div>
+    <div class="pb-2">{formatName(employee?.name ?? '')}</div>
+    <a href={`#${getPanelURI(view.component.EditDoc, employee._id, Hierarchy.mixinOrClass(employee), 'content')}`}
+      ><Label label={contact.string.ViewFullProfile} /></a
+    >
+    {#if status}
+      <div class="pb-2">
+        <Label label={contact.string.Status} />
+        <div class="flex-row-stretch statusContainer">
+          <div class="pr-2">
+            <EmployeeStatusPresenter {employee} withTooltip={false} />
           </div>
-        {/if}
+          {#if editable}
+            <div class="setStatusButton">
+              <Button icon={Edit} title={contact.string.SetStatus} on:click={onEdit} />
+            </div>
+          {/if}
+        </div>
       </div>
-    </div>
-  {:else if editable}
-    <div class="flex-row-stretch over-underline pb-2" on:click={onEdit}>
-      <Label label={contact.string.SetStatus} />
-    </div>
+    {:else if editable}
+      <div class="flex-row-stretch over-underline pb-2" on:click={onEdit}>
+        <Label label={contact.string.SetStatus} />
+      </div>
+    {/if}
   {/if}
 </div>
 
