5be2fb23dd224b25b34476e840d71f21ad455d1e
Alexander Platov <sas_lord@mail.ru>
2022-05-17 13:07:02 +0300

    Fix resolve
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- plugins/telegram-resources/src/components/Chat.svelte
+++ plugins/telegram-resources/src/components/Chat.svelte
@@ -74,7 +74,7 @@
       telegram.class.Message,
       { attachedTo: channelId },
       (res) => {
-        messages = res
+        messages = res.reverse()
         if (channel !== undefined) {
           notificationClient.updateLastView(channel._id, channel._class, undefined, true)
         }
@@ -82,7 +82,7 @@
         updateAccountsQuery(accountsIds)
       },
       {
-        sort: { sendOn: SortingOrder.Ascending },
+        sort: { sendOn: SortingOrder.Descending },
         limit: 500,
         lookup: {
           _id: { attachments: attachment.class.Attachment }
