fa22aa5f7e4dfda6d00cc7fdacc45fc7cb2e87f6
Anna No <anna.no@xored.com>
2022-05-19 17:36:45 +0700

    Set particular date for reminder (#1801)
    Signed-off-by: Anna No <anna.no@xored.com>

--- packages/ui/src/types.ts
+++ packages/ui/src/types.ts
@@ -82,6 +82,11 @@ export type HorizontalAlignment = 'left' | 'right'
 
 export type IconSize = 'inline' | 'tiny' | 'x-small' | 'small' | 'medium' | 'large' | 'x-large' | 'full'
 
+export interface DateOrShift {
+  date?: number
+  shift?: number
+}
+
 export interface LabelAndProps {
   label: IntlString | undefined
   element: HTMLElement | undefined
