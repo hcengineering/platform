d9641f86c4efd2d2781421e70f57dc27777475f0
Dvinyanin Alexandr <dvinyanin.alexandr@gmail.com>
2022-05-20 20:23:14 +0700

    Use standart action for archive state
    Signed-off-by: Dvinyanin Alexandr <dvinyanin.alexandr@gmail.com>

--- plugins/board-resources/src/components/ListHeader.svelte
+++ plugins/board-resources/src/components/ListHeader.svelte
@@ -1,25 +1,29 @@
 <script lang="ts">
   import { Button, Component, getPlatformColor, IconEdit, showPopup } from '@anticrm/ui'
   import { State } from '@anticrm/task'
-  import { getElementPopupAlignment } from '../utils/PopupUtils'
-  import ListInlineActions from './editor/ListInlineActions.svelte'
   import notification from '@anticrm/notification'
+  import { ContextMenu } from '@anticrm/view-resources'
   export let state: State
 
-  let ref: HTMLElement
-
-  function onArchive () {
-    showPopup(ListInlineActions, { value: state }, getElementPopupAlignment(ref, { h: 'right', v: 'bottom' }))
+  const showMenu = async (ev: MouseEvent): Promise<void> => {
+    ev.preventDefault()
+    showPopup(
+      ContextMenu,
+      { object: state },
+      {
+        getBoundingClientRect: () => DOMRect.fromRect({ width: 1, height: 1, x: ev.clientX, y: ev.clientY })
+      }
+    )
   }
 </script>
 
 <div class="flex-col h-16">
   <div class="h-2 border-radius-1" style="background-color: {getPlatformColor(state.color)}" />
-  <div class="flex-between h-full font-medium pr-2 pl-4" bind:this={ref}>
+  <div class="flex-between h-full font-medium pr-2 pl-4">
     <span class="lines-limit-2">{state.title}</span>
     <div class="flex">
       <Component is={notification.component.LastViewEditor} props={{ value: state }} />
-      <Button icon={IconEdit} kind="transparent" on:click={onArchive} />
+      <Button icon={IconEdit} kind="transparent" on:click={showMenu} />
     </div>
   </div>
 </div>
