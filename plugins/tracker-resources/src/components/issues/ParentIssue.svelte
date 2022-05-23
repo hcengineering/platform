e2ea17a1bad37b5d5aa5d3336a9b0f2ed7c49937
Sergei Ogorelkov <sergei.ogorelkov@xored.com>
2022-05-23 18:09:42 +0700

    Add tooltip to the "Remove parent" button (#1786)
    Signed-off-by: Sergei Ogorelkov <sergei.ogorelkov@xored.com>

--- plugins/tracker-resources/src/components/issues/ParentIssue.svelte
+++ plugins/tracker-resources/src/components/issues/ParentIssue.svelte
@@ -15,7 +15,7 @@
 <script lang="ts">
   import { createQuery } from '@anticrm/presentation'
   import { Team, Issue } from '@anticrm/tracker'
-  import { Spinner, IconClose } from '@anticrm/ui'
+  import { Spinner, IconClose, Tooltip } from '@anticrm/ui'
   import { createEventDispatcher } from 'svelte'
   import tracker from '../../plugin'
 
@@ -37,9 +37,11 @@
     <Spinner size="small" />
   {/if}
   <span class="overflow-label issue-title">{issue.title}</span>
-  <div class="button-close" on:click={() => dispatch('close')}>
-    <IconClose size="x-small" />
-  </div>
+  <Tooltip label={tracker.string.RemoveParent} direction="bottom" >
+    <div class="button-close" on:click={() => dispatch('close')}>
+      <IconClose size="x-small" />
+    </div>
+  </Tooltip>
 </div>
 
 <style lang="scss">
@@ -48,18 +50,22 @@
     line-height: 150%;
     max-width: fit-content;
     border: 1px solid var(--button-border-color);
+    border-radius: 0.25rem;
+    box-shadow: var(--primary-shadow);
   }
 
   .issue-title {
     margin: 0 0.75rem 0 0.5rem;
     padding-right: 0.75rem;
-    color: var(--theme-caption-color);
+    color: var(--theme-content-accent-color);
     border-right: 1px solid var(--button-border-color);
   }
 
   .button-close {
     cursor: pointer;
     position: relative;
+    color: var(--content-color);
+    transition: color 0.15s;
 
     &:hover {
       color: var(--theme-caption-color);
