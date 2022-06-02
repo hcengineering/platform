268f14b061a99f04d778563b721e5dfa903fa9eb
Andrey Sobolev <haiodo@users.noreply.github.com>
2022-06-03 00:00:00 +0700

    UI Sanity test fixes (#1986)
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- tests/sanity/tests/recruit.spec.ts
+++ tests/sanity/tests/recruit.spec.ts
@@ -82,6 +82,7 @@ test.describe('recruit tests', () => {
 
     await page.click('button:has-text("Vacancy")')
 
+    await page.fill('[placeholder="Search..."]', vacancyId)
     await page.click(`button:has-text("${vacancyId}")`)
 
     await page.click('button:has-text("Create")')
@@ -169,5 +170,22 @@ test.describe('recruit tests', () => {
     await page.click('button:has-text("Andrey P.")')
     await page.click('text=Create')
     await page.click('td:has-text("RVE-")')
+  })
+
+  test('test-create-skill', async ({ page }) => {
+    await page.click('[id="app-recruit\\:string\\:RecruitApplication"]')
+    await page.click('text=Skills')
+    await page.click('button:has-text("Skill")')
+    await page.click('[placeholder="Please\\ type\\ skill\\ title"]')
+    const skillId = 'custom-skill-' + generateId()
+    await page.fill('[placeholder="Please\\ type\\ skill\\ title"]', skillId)
+    await page.click('button:has-text("Other")')
+    await page.click('button:has-text("Design")')
+    await page.click('button:has-text("Create")')
+    await page.click(`text=${skillId}`)
+    await page.click('[placeholder="Please\\ type\\ description\\ here"]')
+    await page.fill('[placeholder="Please\\ type\\ description\\ here"]', 'description-' + skillId)
+    await page.click('button:has-text("Save")')
+    await page.click(`span:has-text("description-${skillId}")`)
   })
 })
