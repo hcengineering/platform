d168dac6efab7cb2602021e4b0de72944e19cc31
Sergei Ogorelkov <sergei.ogorelkov@xored.com>
2022-06-03 12:21:58 +0700

    Fix missing top border for sub-issues
    Signed-off-by: Sergei Ogorelkov <sergei.ogorelkov@xored.com>

--- plugins/tracker-resources/src/components/issues/edit/SubIssues.svelte
+++ plugins/tracker-resources/src/components/issues/edit/SubIssues.svelte
@@ -97,11 +97,11 @@
 
 <style lang="scss">
   .list {
-    border-bottom: 1px solid var(--divider-color);
+    border-top: 1px solid var(--divider-color);
 
     &.collapsed {
-      margin-top: 1px;
-      border-bottom: none;
+      padding-top: 1px;
+      border-top: none;
     }
   }
 </style>
