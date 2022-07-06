3a9f0c3ded39580452605b8e2bf2bd236d2a0332
Alexander Platov <sas_lord@mail.ru>
2022-07-06 15:24:09 +0300

    Fix tests
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- tests/create-local.sh
+++ tests/create-local.sh
@@ -5,6 +5,7 @@ export MINIO_ENDPOINT=localhost:9000
 export MONGO_URL=mongodb://localhost:27017
 export TRANSACTOR_URL=ws:/localhost:3333
 export ELASTIC_URL=http://localhost:9200
+export SERVER_SECRET=secret
 
 # Create workspace record in accounts
 node ../dev/tool/bundle.js create-workspace sanity-ws -o SanityTest
--- tests/restore-local.sh
+++ tests/restore-local.sh
@@ -5,7 +5,7 @@ export MINIO_ENDPOINT=localhost:9000
 export MONGO_URL=mongodb://localhost:27017
 export TRANSACTOR_URL=ws:/localhost:3333
 export ELASTIC_URL=http://localhost:9200
-
+export SERVER_SECRET=secret
 
 # Restore workspace contents in mongo/elastic
 node ../dev/tool/bundle.js restore-workspace sanity-ws sanity-ws/
