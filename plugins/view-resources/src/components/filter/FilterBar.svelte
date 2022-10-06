51fda7c3ac6941641fea44b815c7dca29adc9f23
muhtimur <muhamedishin.timur@gmail.com>
2022-10-06 14:26:34 +0700

    Correct doneState check
    Signed-off-by: muhtimur <muhamedishin.timur@gmail.com>
    Signed-off-by: muhtimur <timur.mukhamedishin@xored.com>

--- plugins/view-resources/src/components/filter/FilterBar.svelte
+++ plugins/view-resources/src/components/filter/FilterBar.svelte
@@ -119,7 +119,7 @@
         filterKey = attr.attributeOf + '.' + filter.key.key
       }
 
-      if (!newQuery[filterKey]) {
+      if (newQuery[filterKey] === null || newQuery[filterKey] === undefined) {
         newQuery[filterKey] = newValue
       } else {
         let merged = false
