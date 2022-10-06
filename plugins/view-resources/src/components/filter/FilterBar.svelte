84e03b8a46e075c5f94abd69777d270748db1f95
muhtimur <muhamedishin.timur@gmail.com>
2022-10-06 12:57:57 +0700

    Fix bug with wrong filtering by doneState in Applications
    Signed-off-by: muhtimur <muhamedishin.timur@gmail.com>
    Signed-off-by: muhtimur <timur.mukhamedishin@xored.com>

--- plugins/view-resources/src/components/filter/FilterBar.svelte
+++ plugins/view-resources/src/components/filter/FilterBar.svelte
@@ -119,7 +119,7 @@
         filterKey = attr.attributeOf + '.' + filter.key.key
       }
 
-      if (newQuery[filterKey] === undefined) {
+      if (!newQuery[filterKey]) {
         newQuery[filterKey] = newValue
       } else {
         let merged = false
