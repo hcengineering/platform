4c1e3b75c9e192e157272671449c8d5b30ab3b16
Alexander Platov <sas_lord@mail.ru>
2022-11-01 18:48:20 +0300

    Applications list updated layout (#2343)
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- plugins/workbench-resources/src/components/Applications.svelte
+++ plugins/workbench-resources/src/components/Applications.svelte
@@ -17,38 +17,143 @@
   import type { Application } from '@hcengineering/workbench'
   import { createEventDispatcher } from 'svelte'
   import AppItem from './AppItem.svelte'
-  import { Scroller } from '@hcengineering/ui'
+  import { Scroller, IconDownOutline } from '@hcengineering/ui'
+  import { showApplication, hideApplication } from '../utils'
+  import { createQuery } from '@hcengineering/presentation'
+  import workbench from '@hcengineering/workbench'
 
   export let active: Ref<Application> | undefined
   export let apps: Application[] = []
   export let direction: 'vertical' | 'horizontal' = 'vertical'
 
   const dispatch = createEventDispatcher()
+
+  let hiddenAppsIds: Ref<Application>[] = []
+  const hiddenAppsIdsQuery = createQuery()
+  hiddenAppsIdsQuery.query(workbench.class.HiddenApplication, {}, (res) => {
+    hiddenAppsIds = res.map((r) => r.attachedTo)
+  })
+
+  let shown: boolean = false
 </script>
 
-<div class="flex align-center clear-mins apps-{direction}">
-  <Scroller invertScroll padding={'.5rem .5rem'} horizontal={direction === 'horizontal'} contentDirection={direction}>
-    {#each apps as app}
+<div class="flex-row-center clear-mins apps-{direction} relative">
+  <Scroller
+    invertScroll
+    padding={direction === 'horizontal' ? '.25rem 0' : '0 .5rem'}
+    horizontal={direction === 'horizontal'}
+    contentDirection={direction}
+  >
+    <div class="apps-space-{direction}" />
+    {#each apps.filter((it) => (shown ? true : !hiddenAppsIds.includes(it._id))) as app}
       <AppItem
         selected={app._id === active}
         icon={app.icon}
         label={app.label}
+        hidden={hiddenAppsIds.includes(app._id)}
+        editable={shown}
         action={async () => {
           dispatch('active', app)
         }}
         notify={false}
+        on:visible={(res) => {
+          if (res.detail === undefined) return
+          if (res.detail) showApplication(app)
+          else hideApplication(app)
+        }}
       />
     {/each}
+    <div class="apps-space-{direction}" />
   </Scroller>
+  <div class="thinButton {direction}" class:shown on:click={() => (shown = !shown)}>
+    <div class="clear-mins pointer-events-none" class:rotate90={direction === 'horizontal'}>
+      <IconDownOutline size={'medium'} />
+    </div>
+  </div>
 </div>
 
 <style lang="scss">
   .apps-horizontal {
+    justify-content: center;
     margin: 0 1rem;
     padding: 0 0.25rem;
+    min-height: 4rem;
   }
   .apps-vertical {
     margin: 1rem 0;
     padding: 0.25rem 0;
+    min-width: 4rem;
+  }
+  .apps-space {
+    &-vertical {
+      min-height: 0.25rem;
+      height: 0.25rem;
+    }
+    &-horizontal {
+      min-width: 0.25rem;
+      width: 0.25rem;
+    }
+  }
+
+  .thinButton {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    transform-origin: center center;
+    border-radius: 0.5rem;
+    opacity: 0.2;
+    cursor: pointer;
+
+    transition-property: opacity, transform;
+    transition-timing-function: var(--timing-main);
+    transition-duration: 0.1s;
+
+    &.vertical {
+      top: 100%;
+      left: 50%;
+      width: 2.5rem;
+      transform: translateX(-50%) scale(0.6) rotate(0deg);
+      &:hover {
+        transform: translateX(-50%) scale(0.8);
+      }
+    }
+    &.horizontal {
+      left: 100%;
+      top: 50%;
+      height: 2.5rem;
+      transform: translateY(-50%) scale(0.6);
+      &:hover {
+        transform: translateY(-50%) scale(0.8);
+      }
+      &.shown {
+        transform: translateY(-50%) scale(0.9) rotate(180deg);
+        &:hover {
+          transform: translateY(-50%) scale(1) rotate(180deg);
+        }
+      }
+    }
+
+    &:hover {
+      transform: translateX(-50%) scale(0.8);
+      background-color: var(--accent-bg-color);
+      opacity: 0.9;
+    }
+
+    &.shown {
+      transform: translateX(-50%) scale(0.9) rotate(180deg);
+      opacity: 0.8;
+
+      &:hover {
+        transform: translateX(-50%) scale(1) rotate(180deg);
+        background-color: var(--accent-bg-color);
+        opacity: 1;
+      }
+    }
+  }
+  .rotate90 {
+    transform-origin: center center;
+    transform: rotate(-90deg);
   }
 </style>
