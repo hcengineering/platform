a65d8646833bcc3a9d1f792da9d5597e4066bf38
Andrey Sobolev <haiodo@gmail.com>
2023-01-18 15:21:14 +0700

    Fix issue templates
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- plugins/tracker-resources/src/components/templates/IssueTemplatesContent.svelte
+++ plugins/tracker-resources/src/components/templates/IssueTemplatesContent.svelte
@@ -2,11 +2,12 @@
   import { DocumentQuery, WithLookup } from '@hcengineering/core'
   import { IssueTemplate } from '@hcengineering/tracker'
   import { Component } from '@hcengineering/ui'
-  import { Viewlet } from '@hcengineering/view'
+  import { Viewlet, ViewOptions } from '@hcengineering/view'
   import tracker from '../../plugin'
   import CreateIssueTemplate from './CreateIssueTemplate.svelte'
 
   export let viewlet: WithLookup<Viewlet>
+  export let viewOptions: ViewOptions
   export let query: DocumentQuery<IssueTemplate> = {}
 
   const createItemDialog = CreateIssueTemplate
@@ -22,7 +23,8 @@
       options: viewlet.options,
       createItemDialog,
       createItemLabel,
-      viewOptions: viewlet.viewOptions?.other,
+      viewOptions,
+      viewOptionsConfig: viewlet.viewOptions?.other,
       viewlet,
       query
     }}
