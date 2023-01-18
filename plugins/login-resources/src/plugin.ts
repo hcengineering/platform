810353a42d2f60ec1e3464ad593f4d7f1c28edde
Denis Bykhov <bykhov.denis@gmail.com>
2023-01-18 23:36:01 +0600

    Fix typo
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- plugins/login-resources/src/plugin.ts
+++ plugins/login-resources/src/plugin.ts
@@ -48,7 +48,7 @@ export default mergeIds(loginId, login, {
     WantAnotherWorkspace: '' as IntlString,
     NotSeeingWorkspace: '' as IntlString,
     ChangeAccount: '' as IntlString,
-    WorksaceNameRule: '' as IntlString,
+    WorkspaceNameRule: '' as IntlString,
     LinkValidHours: '' as IntlString,
     GetLink: '' as IntlString
   }
