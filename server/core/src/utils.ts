9c66502ff07ec8870dde6943fbda3ac79a3f5da6
Vyacheslav Tumanov <me@slavatumanov.me>
2023-02-15 11:06:56 +0500

    make a clone of the object on return
    Signed-off-by: Vyacheslav Tumanov <me@slavatumanov.me>

--- server/core/src/utils.ts
+++ server/core/src/utils.ts
@@ -4,6 +4,7 @@ import {
   DocumentQuery,
   FindOptions,
   FindResult,
+  Hierarchy,
   MeasureContext,
   Ref,
   ServerStorage
@@ -15,6 +16,7 @@ import {
 export function createCacheFindAll (storage: ServerStorage): ServerStorage['findAll'] {
   // We will cache all queries for same objects for all derived data checks.
   const queryCache = new Map<string, FindResult<Doc>>()
+  const hierarchy = new Hierarchy()
 
   return async <T extends Doc>(
     ctx: MeasureContext,
@@ -29,6 +31,6 @@ export function createCacheFindAll (storage: ServerStorage): ServerStorage['find
     }
     cacheResult = await storage.findAll(ctx, clazz, query, options)
     queryCache.set(key, cacheResult)
-    return cacheResult as FindResult<T>
+    return hierarchy.clone(cacheResult) as FindResult<T>
   }
 }
