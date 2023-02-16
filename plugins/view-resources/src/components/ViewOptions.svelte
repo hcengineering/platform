aeefe3f318acfab06f52c6c7d78bf2e2055e2582
Denis Bykhov <bykhov.denis@gmail.com>
2023-02-16 12:59:22 +0600

    Fix view options
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- plugins/view-resources/src/components/ViewOptions.svelte
+++ plugins/view-resources/src/components/ViewOptions.svelte
@@ -13,10 +13,9 @@
 
   const dispatch = createEventDispatcher()
 
-  const groups = (viewOptions.groupBy =
-    viewOptions.groupBy[viewOptions.groupBy.length - 1] === noCategory
-      ? viewOptions.groupBy
-      : [...viewOptions.groupBy, noCategory])
+  const groups = viewOptions.groupBy[viewOptions.groupBy.length - 1] === noCategory
+    ? [...viewOptions.groupBy]
+    : [...viewOptions.groupBy, noCategory]
 
   const client = getClient()
   const hierarchy = client.getHierarchy()
