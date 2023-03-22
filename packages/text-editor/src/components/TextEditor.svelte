e0ba4a0bc891538af2767fa5fe028cf6f1b624b5
Vyacheslav Tumanov <me@slavatumanov.me>
2023-03-22 10:52:46 +0500

    TSK-858: Handle if empty in ReferenceInput
    Signed-off-by: Vyacheslav Tumanov <me@slavatumanov.me>

--- packages/text-editor/src/components/TextEditor.svelte
+++ packages/text-editor/src/components/TextEditor.svelte
@@ -51,10 +51,8 @@
     if (editor) editor.setEditable(editable)
   }
   export function submit (): void {
-    if (!editor.isEmpty || haveAttachment) {
-      content = editor.getHTML()
-      dispatch('content', content)
-    }
+    content = editor.getHTML()
+    dispatch('content', content)
   }
   export function setContent (newContent: string): void {
     if (content !== newContent) {
