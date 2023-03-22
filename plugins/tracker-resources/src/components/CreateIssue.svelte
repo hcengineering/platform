68cecd2828efcca2f2479d52bae0b2322263a1b1
Denis Bykhov <bykhov.denis@gmail.com>
2023-03-22 22:11:57 +0600

    Fix sub issue draft
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- plugins/tracker-resources/src/components/CreateIssue.svelte
+++ plugins/tracker-resources/src/components/CreateIssue.svelte
@@ -703,6 +703,7 @@
     project={currentProject}
     sprint={object.sprint}
     component={object.component}
+    bind:subIssues={subIssues}
   />
   <svelte:fragment slot="pool">
     <div id="status-editor">
