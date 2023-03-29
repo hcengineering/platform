d95d11c53c899075ea066a0e07a55cf933720c98
Alexander Platov <sas_lord@mail.ru>
2023-03-29 10:33:27 +0300

    Revert TSK-966
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- models/tracker/src/index.ts
+++ models/tracker/src/index.ts
@@ -866,6 +866,10 @@ export function createModel (builder: Builder): void {
     titleProvider: tracker.function.IssueTitleProvider
   })
 
+  builder.mixin(tracker.class.Issue, core.class.Class, view.mixin.ListHeaderExtra, {
+    presenters: [tracker.component.IssueStatistics]
+  })
+
   builder.mixin(tracker.class.IssueStatus, core.class.Class, view.mixin.SortFuncs, {
     func: tracker.function.IssueStatusSort
   })
