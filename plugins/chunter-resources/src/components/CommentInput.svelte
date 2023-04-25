1ce134c292bbacd015a778a51b7b8a0576f6516d
Denis Bykhov <bykhov.denis@gmail.com>
2023-04-25 22:15:52 +0600

    Fix draft comment stuck (#3072)
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- plugins/chunter-resources/src/components/CommentInput.svelte
+++ plugins/chunter-resources/src/components/CommentInput.svelte
@@ -79,25 +79,30 @@
   async function onMessage (event: CustomEvent) {
     loading = true
     const { message, attachments } = event.detail
-    await client.addCollection<Doc, Comment>(
-      _class,
-      object.space,
-      object._id,
-      object._class,
-      'comments',
-      { message, attachments },
-      _id
-    )
+    try {
+      draftController.remove()
+      commentInputBox.removeDraft(false)
+      await client.addCollection<Doc, Comment>(
+        _class,
+        object.space,
+        object._id,
+        object._class,
+        'comments',
+        { message, attachments },
+        _id
+      )
 
-    // Create an backlink to document
-    await createBacklinks(client, object._id, object._class, _id, message)
+      // Create an backlink to document
+      await createBacklinks(client, object._id, object._class, _id, message)
 
-    // Remove draft from Local Storage
-    _id = generateId()
-    comment = getDefault()
-    draftController.remove()
-    commentInputBox.removeDraft(false)
-    loading = false
+      // Remove draft from Local Storage
+      _id = generateId()
+      comment = getDefault()
+    } catch (err) {
+      console.error(err)
+    } finally {
+      loading = false
+    }
   }
 </script>
 
