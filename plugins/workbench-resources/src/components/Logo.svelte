310a9562e68052a6a9023c763b86b36d65335f03
Alexander Platov <sas_lord@mail.ru>
2023-04-26 20:04:00 +0300

    Fix format
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- plugins/workbench-resources/src/components/Logo.svelte
+++ plugins/workbench-resources/src/components/Logo.svelte
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 -->
-<div class="antiLogo red">
-  P
-</div>
+<div class="antiLogo red">P</div>
 
 <style lang="scss">
   .antiLogo {
