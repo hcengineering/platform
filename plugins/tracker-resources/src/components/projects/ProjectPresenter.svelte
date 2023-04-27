1c0d04132a68715bfe044e4e1424dcf22ef91fc3
Andrey Sobolev <haiodo@users.noreply.github.com>
2023-04-28 00:21:52 +0700

    TSK-1378: Qfix for exception (#3097)
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- plugins/tracker-resources/src/components/projects/ProjectPresenter.svelte
+++ plugins/tracker-resources/src/components/projects/ProjectPresenter.svelte
@@ -27,7 +27,7 @@
   export let deselect: boolean = false
 </script>
 
-{#if model.specials}
+{#if model?.specials}
   <TreeNode icon={space?.icon ?? model.icon} title={space.name} actions={() => getActions(space)}>
     {#each model.specials as special}
       <NavLink space={space._id} special={special.id}>
