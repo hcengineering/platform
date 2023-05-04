7b4b24a54339657e9d057c4b939044b222c0f2f4
Andrey Sobolev <haiodo@gmail.com>
2023-05-04 18:08:24 +0700

    Formatting fix
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- packages/ui/src/plugin.ts
+++ packages/ui/src/plugin.ts
@@ -75,14 +75,14 @@ export const uis = plugin(uiId, {
     DueDatePopupOverdueTitle: '' as IntlString,
     DueDatePopupDescription: '' as IntlString,
     DueDatePopupOverdueDescription: '' as IntlString,
-    
+
     GettingWorkDone: '' as IntlString,
     Smileys: '' as IntlString,
     Nature: '' as IntlString,
     Symbols: '' as IntlString,
     TravelAndPlaces: '' as IntlString,
     Food: '' as IntlString,
-    Objects: '' as IntlString,
+    Objects: '' as IntlString
   },
   metadata: {
     DefaultApplication: '' as Metadata<AnyComponent>,
