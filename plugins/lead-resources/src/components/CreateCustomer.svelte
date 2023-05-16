c06999f4d4caa51dfc6147500d969926a087eca7
Vyacheslav Tumanov <me@slavatumanov.me>
2023-05-16 18:19:07 +0500

    UBER-76: Add custom fields for organization customers in Leads
    Signed-off-by: Vyacheslav Tumanov <me@slavatumanov.me>

--- plugins/lead-resources/src/components/CreateCustomer.svelte
+++ plugins/lead-resources/src/components/CreateCustomer.svelte
@@ -18,7 +18,7 @@
   import contact from '@hcengineering/contact-resources/src/plugin'
   import { AttachedData, Class, Data, Doc, generateId, MixinData, Ref, WithLookup } from '@hcengineering/core'
   import type { Customer } from '@hcengineering/lead'
-  import { Card, getClient } from '@hcengineering/presentation'
+  import { Card, getClient, InlineAttributeBar } from '@hcengineering/presentation'
   import {
     Button,
     createFocusManager,
@@ -234,6 +234,15 @@
       editable
       highlighted={matchedChannels.map((it) => it.provider)}
     />
+    {#if targetClass._id === contact.class.Organization}
+      <InlineAttributeBar
+        _class={contact.class.Organization}
+        {object}
+        toClass={contact.class.Contact}
+        on:update
+        extraProps={{ showNavigate: false }}
+      />
+    {/if}
   </svelte:fragment>
   <svelte:fragment slot="footer">
     {#if matches.length > 0}
