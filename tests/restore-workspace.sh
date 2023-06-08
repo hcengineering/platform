ec67458591b7419c5650dde85f7aa057611ed78e
Andrey Sobolev <haiodo@gmail.com>
2023-06-08 17:12:02 +0700

    Fix ui testing
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- tests/restore-workspace.sh
+++ tests/restore-workspace.sh
@@ -3,7 +3,7 @@
 # Restore workspace contents in mongo/elastic
 ./tool.sh backup-restore ./sanity-ws sanity-ws
 
-node ../dev/tool/bundle.js upgrade-workspace sanity-ws
+./tool.sh upgrade-workspace sanity-ws
 
 # Re-assign user to workspace.
 ./tool.sh assign-workspace user1 sanity-ws
