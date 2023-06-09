01842637ae9cf253f18a9e284368d1f0190f546d
Vyacheslav Tumanov <me@slavatumanov.me>
2023-06-09 13:53:44 +0500

    UBER-458: fix submenu
    Signed-off-by: Vyacheslav Tumanov <me@slavatumanov.me>

--- packages/ui/src/tooltips.ts
+++ packages/ui/src/tooltips.ts
@@ -74,7 +74,7 @@ export function showTooltip (
     props,
     anchor,
     onUpdate,
-    kind: undefined
+    kind
   }
   tooltipstore.update((old) => {
     if (old.component === storedValue.component) {
