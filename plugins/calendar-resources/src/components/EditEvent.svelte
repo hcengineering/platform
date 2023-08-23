998689401a9c6dc601ee470293c85ac2b75610ea
Alexander Platov <sas_lord@mail.ru>
2023-08-23 06:11:35 +0300

    UBER-667: UI fixes, displaying All day, time editor. (#3619)
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- plugins/calendar-resources/src/components/EditEvent.svelte
+++ plugins/calendar-resources/src/components/EditEvent.svelte
@@ -30,7 +30,6 @@
   import EventReminders from './EventReminders.svelte'
 
   export let object: Event
-
   $: readOnly = isReadOnly(object)
 
   let title = object.title
@@ -232,6 +231,7 @@
     <div>
       {#if !allDay && rules.length === 0}
         <div class="flex-row-center flex-gap-3 ext">
+          <!-- svelte-ignore a11y-click-events-have-key-events -->
           <div class="cursor-pointer" on:click={() => (allDay = true)}>
             <Label label={calendar.string.AllDay} />
           </div>
@@ -239,6 +239,7 @@
             <Label label={calendar.string.TimeZone} />
           </div>
           {#if rules.length > 0}
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
             <div class="cursor-pointer" on:click={setRecurrance}>
               <Label label={calendar.string.Repeat} />
             </div>
@@ -255,6 +256,7 @@
             <Label label={calendar.string.TimeZone} />
           </div>
           {#if rules.length > 0}
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
             <div class="flex-row-center flex-gap-2 mt-1" on:click={setRecurrance}>
               <Icon size="small" icon={calendar.icon.Repeat} />
               {#if rules.length > 0}
@@ -297,7 +299,7 @@
     min-height: 0;
     background: var(--theme-popup-color);
     box-shadow: var(--theme-popup-shadow);
-    width: 25rem;
+    min-width: 25rem;
     border-radius: 1rem;
 
     .header {
