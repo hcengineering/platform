2a71d9b6d8fb70c83d5a123bed79da5da944998a
Denis Bykhov <bykhov.denis@gmail.com>
2023-10-03 23:24:51 +0600

    Fix tests
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- tests/sanity/tests/tracker.spec.ts
+++ tests/sanity/tests/tracker.spec.ts
@@ -143,7 +143,7 @@ test('report-time-from-issue-card', async ({ page }) => {
   await navigate(page)
   const assignee = 'Chen Rosamund'
   const status = 'In Progress'
-  const values = [0.25, 0.5, 0.75, 1]
+  const values = [2, 4, 6, 8]
   for (let i = 0; i < 5; i++) {
     const random = Math.floor(Math.random() * values.length)
     const time = values[random]
@@ -214,7 +214,7 @@ test('report-time-from-main-view', async ({ page }) => {
   await page.click('text="Modified date"')
   await page.keyboard.press('Escape')
 
-  const values = [0.25, 0.5, 0.75, 1]
+  const values = [2, 4, 6, 8]
   const assignee = 'Chen Rosamund'
   const status = 'In Progress'
   const name = getIssueName()
