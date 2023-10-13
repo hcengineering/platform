8772391971da7d21f11f89820f0d1dcc1e0b0db5
Denis Bykhov <bykhov.denis@gmail.com>
2023-10-13 12:21:01 +0600

    Remove old automation (#3835)
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- plugins/automation-resources/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['./node_modules/@hcengineering/platform-rig/profiles/ui/eslint.config.json'],
-  parserOptions: { tsconfigRootDir: __dirname },
-  settings: {
-    'svelte3/ignore-styles': () => true
-  }
-}
--- plugins/automation-resources/src/components/Automations.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import automation from '@hcengineering/automation'
-  import { TableBrowser } from '@hcengineering/view-resources'
-</script>
-
-<TableBrowser _class={automation.class.Automation} config={['name', 'description', 'modifiedOn']} query={{}} />
