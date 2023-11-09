96de2f914010bc65d1c7fd4ff4bc793ee7c43c6f
Maxim Karmatskikh <mkarmatskih@gmail.com>
2023-11-09 07:44:30 +0100

    UBERF-4229: Fix createAttachments runtime error
    Signed-off-by: Maxim Karmatskikh <mkarmatskih@gmail.com>

--- plugins/attachment-resources/src/components/AttachmentStyleBoxEditor.svelte
+++ plugins/attachment-resources/src/components/AttachmentStyleBoxEditor.svelte
@@ -1,14 +1,14 @@
 <!--
 // Copyright © 2023 Hardcore Engineering Inc.
-// 
+//
 // Licensed under the Eclipse Public License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License. You may
 // obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
 -->
@@ -86,7 +86,12 @@
       haveUnsavedChanges = false
     }
 
-    await descriptionBox.createAttachments()
+    /*
+      Delayed save can be invoked after dismount. In this case descriptionBox would be null
+    */
+    if (descriptionBox !== undefined && descriptionBox !== null) {
+      await descriptionBox.createAttachments()
+    }
   }
 
   let saveTrigger: any
