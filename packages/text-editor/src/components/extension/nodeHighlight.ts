5728af7bceb1d33e1d325efde224d1fd6ee98e81
Alexander Onnikov <alexander.onnikov@xored.com>
2023-11-14 13:06:35 +0700

    EZQMS-359 Refactor node highlight plugin
    Signed-off-by: Alexander Onnikov <alexander.onnikov@xored.com>

--- packages/text-editor/src/components/extension/nodeHighlight.ts
+++ packages/text-editor/src/components/extension/nodeHighlight.ts
@@ -1,13 +1,16 @@
-import { Extension, Range, getMarkRange, mergeAttributes } from '@tiptap/core'
+import { Command, CommandProps, Extension, Range, getMarkRange, mergeAttributes } from '@tiptap/core'
+import { Node as ProseMirrorNode, MarkType } from '@tiptap/pm/model'
+import { Plugin, PluginKey, TextSelection } from '@tiptap/pm/state'
+import { AddMarkStep, RemoveMarkStep } from '@tiptap/pm/transform'
+import { Decoration, DecorationSet } from '@tiptap/pm/view'
 import { NodeUuidExtension, NodeUuidOptions, NodeUuidStorage, findNodeUuidMark } from './nodeUuid'
-import { Plugin, PluginKey, TextSelection } from 'prosemirror-state'
-import { Decoration, DecorationSet } from 'prosemirror-view'
 
 export enum NodeHighlightType {
   WARNING = 'warning',
   ADD = 'add',
   DELETE = 'delete'
 }
+
 export interface NodeHighlightExtensionOptions extends NodeUuidOptions {
   getNodeHighlightType: (uuid: string) => NodeHighlightType | undefined | null
   isHighlightModeOn: () => boolean
@@ -41,14 +44,39 @@ const generateAttributes = (uuid: string, options: NodeHighlightExtensionOptions
   return classAttrs
 }
 
+const NodeHighlight = 'node-highlight'
+
+const NodeHighlightMeta = 'node-highlight'
+
+export interface NodeHighlightCommands<ReturnType> {
+  [NodeHighlight]: {
+    /**
+     * Force node to be re-rendered
+     */
+    updateNodeHighlight: (uuid: string) => ReturnType
+
+    /**
+     * Force all nodes to be re-rendered
+     */
+    updateHighlight: () => ReturnType
+  }
+}
+
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> extends NodeHighlightCommands<ReturnType> {}
+}
+
 /**
  * Extension allows to highlight nodes based on uuid
  */
 export const NodeHighlightExtension: Extension<NodeHighlightExtensionOptions, NodeUuidStorage> =
   Extension.create<NodeHighlightExtensionOptions>({
+    name: NodeHighlight,
+
     addStorage (): NodeUuidStorage {
       return { activeNodeUuid: null }
     },
+
     addProseMirrorPlugins () {
       const options = this.options
       const storage: NodeUuidStorage = this.storage
@@ -56,7 +84,7 @@ export const NodeHighlightExtension: Extension<NodeHighlightExtensionOptions, No
       const plugins = [
         ...(this.parent?.() ?? []),
         new Plugin({
-          key: new PluginKey('handle-node-highlight-click-plugin'),
+          key: new PluginKey('node-highlight-click-plugin'),
           props: {
             handleClick (view, pos) {
               if (!options.isHighlightModeOn() || options.isAutoSelect?.() !== true) {
@@ -79,45 +107,63 @@ export const NodeHighlightExtension: Extension<NodeHighlightExtensionOptions, No
             }
           }
         }),
+
         new Plugin({
-          key: new PluginKey('node-highlight-click-decorations-plugin'),
-          props: {
-            decorations (state) {
-              if (!options.isHighlightModeOn()) {
-                return undefined
-              }
-              const decorations: Decoration[] = []
+          key: new PluginKey('node-highlight-decoration-plugin'),
+          state: {
+            init (_config, state): DecorationSet {
               const { doc, schema } = state
-              doc.descendants((node, pos) => {
-                const nodeUuidMark = findNodeUuidMark(node)
+              const markType = schema.marks[NodeUuidExtension.name]
+              return createDecorations(doc, markType, storage, options)
+            },
 
-                if (nodeUuidMark !== null && nodeUuidMark !== undefined) {
-                  const nodeUuid = nodeUuidMark.attrs[NodeUuidExtension.name]
-                  const attributes = generateAttributes(nodeUuid, options)
-                  if (attributes === null || attributes === undefined) {
-                    return
+            apply (tr, decorations, oldState, newState) {
+              const markType = newState.schema.marks[NodeUuidExtension.name]
+
+              if (tr.getMeta(NodeHighlightMeta) !== undefined) {
+                return createDecorations(tr.doc, markType, storage, options)
+              }
+
+              if (!tr.docChanged) {
+                return decorations.map(tr.mapping, tr.doc)
+              }
+
+              // update all decorations when transaction has mark changes
+              if (
+                tr.steps.some(
+                  (step) =>
+                    (step instanceof AddMarkStep && step.mark.type === markType) ||
+                    (step instanceof RemoveMarkStep && step.mark.type === markType)
+                )
+              ) {
+                return createDecorations(tr.doc, markType, storage, options)
+              }
+
+              // update all decorations when changed content has mark changes
+              let hasMarkChanges = false
+              tr.mapping.maps.forEach((map, index) => {
+                if (hasMarkChanges) return
+
+                map.forEach((oldStart, oldEnd, newStart, newEnd) => {
+                  const oldDoc = tr.docs[index]
+                  const newDoc = tr.docs[index + 1] ?? tr.doc
+
+                  if (oldDoc.rangeHasMark(oldStart, oldEnd, markType) !== newDoc.rangeHasMark(newStart, newEnd, markType)) {
+                    hasMarkChanges = true
                   }
-
-                  // the first pos does not contain the mark, so we need to add 1 (pos + 1) to get the correct range
-                  const range = getMarkRange(doc.resolve(pos + 1), schema.marks[NodeUuidExtension.name])
-                  if (!isRange(range)) {
-                    return
-                  }
-
-                  decorations.push(
-                    Decoration.inline(
-                      range.from,
-                      range.to,
-                      mergeAttributes(
-                        attributes,
-                        nodeUuid === storage.activeNodeUuid ? { class: 'text-editor-highlighted-node-selected' } : {}
-                      )
-                    )
-                  )
-                }
+                })
               })
 
-              return DecorationSet.empty.add(doc, decorations)
+              if (hasMarkChanges) {
+                return createDecorations(tr.doc, markType, storage, options)
+              }
+
+              return decorations.map(tr.mapping, tr.doc)
+            }
+          },
+          props: {
+            decorations (state) {
+              return this.getState(state)
             }
           }
         })
@@ -125,6 +171,26 @@ export const NodeHighlightExtension: Extension<NodeHighlightExtensionOptions, No
 
       return plugins
     },
+
+    addCommands () {
+      const result: NodeHighlightCommands<Command>[typeof NodeHighlight] = {
+        updateHighlight:
+          () =>
+            ({ view: { dispatch, state } }: CommandProps) => {
+              dispatch(state.tr.setMeta(NodeHighlightMeta, ''))
+              return true
+            },
+        updateNodeHighlight:
+          (uuid: string) =>
+            ({ view: { dispatch, state } }: CommandProps) => {
+              dispatch(state.tr.setMeta(NodeHighlightMeta, uuid))
+              return true
+            }
+      }
+
+      return result
+    },
+
     addExtensions () {
       const options: NodeHighlightExtensionOptions = this.options
       const storage: NodeUuidStorage = this.storage
@@ -144,3 +210,43 @@ export const NodeHighlightExtension: Extension<NodeHighlightExtensionOptions, No
       ]
     }
   })
+
+const createDecorations = (
+  doc: ProseMirrorNode,
+  markType: MarkType,
+  storage: NodeUuidStorage,
+  options: NodeHighlightExtensionOptions
+): DecorationSet => {
+  const decorations: Decoration[] = []
+
+  doc.descendants((node, pos) => {
+    const nodeUuidMark = findNodeUuidMark(node)
+
+    if (nodeUuidMark !== null && nodeUuidMark !== undefined) {
+      const nodeUuid = nodeUuidMark.attrs[NodeUuidExtension.name]
+      const attributes = generateAttributes(nodeUuid, options)
+      if (attributes === null || attributes === undefined) {
+        return
+      }
+
+      // the first pos does not contain the mark, so we need to add 1 (pos + 1) to get the correct range
+      const range = getMarkRange(doc.resolve(pos + 1), markType)
+      if (!isRange(range)) {
+        return
+      }
+
+      decorations.push(
+        Decoration.inline(
+          range.from,
+          range.to,
+          mergeAttributes(
+            attributes,
+            nodeUuid === storage.activeNodeUuid ? { class: 'text-editor-highlighted-node-selected' } : {}
+          )
+        )
+      )
+    }
+  })
+
+  return DecorationSet.empty.add(doc, decorations)
+}
