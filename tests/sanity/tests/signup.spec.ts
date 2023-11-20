3006241d9b0421808c5ef600188a675b1f646006
Alex Velichko <nestor_007@mail.ru>
2023-11-20 22:32:58 +0300

    feat(test): TESTS-86 added signup test
    Signed-off-by: Alex Velichko <nestor_007@mail.ru>

---
+++ tests/sanity/tests/signup.spec.ts
@@ -0,0 +1,34 @@
+import { test } from '@playwright/test'
+import { generateId, PlatformSetting, PlatformURI } from './utils'
+import { LeftSideMenuPage } from './model/left-side-menu-page'
+import { ChunterPage } from './model/chunter-page'
+import { ChannelPage } from './model/channel-page'
+import { allure } from 'allure-playwright'
+
+test.use({
+  storageState: PlatformSetting
+})
+
+test.describe('channel tests', () => {
+  test.beforeEach(async ({ page }) => {
+    await allure.parentSuite('Channel tests')
+    await (await page.goto(`${PlatformURI}/workbench/sanity-ws`))?.finished()
+  })
+
+  test('create new private channel tests', async ({ page }) => {
+    const leftSideMenuPage = new LeftSideMenuPage(page)
+    await leftSideMenuPage.buttonChunter.click()
+
+    const chunterPage = new ChunterPage(page)
+    await chunterPage.buttonChannelBrowser.click()
+    await chunterPage.buttonNewChannelHeader.click()
+
+    const channel = 'channel-' + generateId()
+    await chunterPage.createNewChannel(channel, true)
+    await chunterPage.openChannel(channel)
+
+    const channelPage = new ChannelPage(page)
+    await channelPage.sendMessage('Test message')
+    await channelPage.checkMessageExist('Test message')
+  })
+})
