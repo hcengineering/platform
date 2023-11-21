62b5396e5c849108b791cb548fa9f646144df8b6
Alex Velichko <nestor_007@mail.ru>
2023-11-21 23:55:42 +0300

    feat(tests): TESTS-87 done tests
    Signed-off-by: Alex Velichko <nestor_007@mail.ru>

--- tests/sanity/tests/collaborative/issues.spec.ts
+++ tests/sanity/tests/collaborative/issues.spec.ts
@@ -18,7 +18,7 @@ test.describe('Collaborative test for issue', () => {
 
   test('Issues can be assigned to another users', async ({ page, browser }) => {
     const newIssue: NewIssue = {
-      title: `Collaborative test for issue-${generateId()}`,
+      title: `Collaborative test for issue`,
       description: 'Collaborative test for issue',
       status: 'Backlog',
       priority: 'Urgent',
@@ -33,8 +33,6 @@ test.describe('Collaborative test for issue', () => {
     }
 
     await (await page.goto(`${PlatformURI}/workbench/sanity-ws/tracker/`))?.finished()
-    const leftSideMenuPage = new LeftSideMenuPage(page)
-    await leftSideMenuPage.buttonTracker.click()
 
     const issuesPage = new IssuesPage(page)
     await issuesPage.createNewIssue(newIssue)
@@ -47,8 +45,6 @@ test.describe('Collaborative test for issue', () => {
     const userSecondContext = await browser.newContext({ storageState: PlatformSettingSecond })
     const userSecondPage = await userSecondContext.newPage()
     await (await userSecondPage.goto(`${PlatformURI}/workbench/sanity-ws/tracker/`))?.finished()
-    const leftSideMenuPageSecond = new LeftSideMenuPage(userSecondPage)
-    await leftSideMenuPageSecond.buttonTracker.click()
 
     const issuesPageSecond = new IssuesPage(userSecondPage)
     await issuesPageSecond.linkSidebarAll.click()
@@ -61,6 +57,44 @@ test.describe('Collaborative test for issue', () => {
       ...newIssue,
       milestone: 'Milestone',
       estimation: '2h'
+    })
+  })
+
+  test('Issues status can be changed by another users', async ({ page, browser }) => {
+    const issue: NewIssue = {
+      title: 'Issues status can be changed by another users',
+      description: 'Collaborative test for issue'
+    }
+
+    await (await page.goto(`${PlatformURI}/workbench/sanity-ws/tracker/`))?.finished()
+    const issuesPage = new IssuesPage(page)
+    await issuesPage.linkSidebarAll.click()
+    await issuesPage.modelSelectorBacklog.click()
+    await issuesPage.searchIssueByName(issue.title)
+    await issuesPage.openIssueByName(issue.title)
+
+    const issuesDetailsPage = new IssuesDetailsPage(page)
+    await issuesDetailsPage.editIssue({ status: 'In Progress' })
+
+    // check by another user
+    const userSecondContext = await browser.newContext({ storageState: PlatformSettingSecond })
+    const userSecondPage = await userSecondContext.newPage()
+    await (await userSecondPage.goto(`${PlatformURI}/workbench/sanity-ws/tracker/`))?.finished()
+
+    const issuesPageSecond = new IssuesPage(userSecondPage)
+    await issuesPageSecond.linkSidebarAll.click()
+    await issuesPageSecond.modelSelectorActive.click()
+    // not active for another user
+    await issuesPageSecond.checkIssueNotExist(issue.title)
+
+    await issuesPageSecond.modelSelectorAll.click()
+    await issuesPageSecond.searchIssueByName(issue.title)
+    await issuesPageSecond.openIssueByName(issue.title)
+
+    const issuesDetailsPageSecond = new IssuesDetailsPage(userSecondPage)
+    await issuesDetailsPageSecond.checkIssue({
+      ...issue,
+      status: 'In Progress'
     })
   })
 })
