7ba7b159a7e0cdf443effcfba1decd389d190e20
Alex Velichko <nestor_007@mail.ru>
2023-11-22 11:06:05 +0300

    feat(tests): TESTS-89 working on test
    Signed-off-by: Alex Velichko <nestor_007@mail.ru>

--- tests/sanity/tests/collaborative/issues.spec.ts
+++ tests/sanity/tests/collaborative/issues.spec.ts
@@ -4,6 +4,7 @@ import { allure } from 'allure-playwright'
 import { NewIssue } from '../model/tracker/types'
 import { IssuesPage } from '../model/tracker/issues-page'
 import { IssuesDetailsPage } from '../model/tracker/issues-details-page'
+import { LeftSideMenuPage } from '../model/left-side-menu-page'
 
 test.use({
   storageState: PlatformSetting
@@ -85,6 +86,48 @@ test.describe('Collaborative test for issue', () => {
     await issuesPageSecond.modelSelectorActive.click()
     // not active for another user
     await issuesPageSecond.checkIssueNotExist(issue.title)
+
+    await issuesPageSecond.modelSelectorAll.click()
+    await issuesPageSecond.searchIssueByName(issue.title)
+    await issuesPageSecond.openIssueByName(issue.title)
+
+    const issuesDetailsPageSecond = new IssuesDetailsPage(userSecondPage)
+    await issuesDetailsPageSecond.checkIssue({
+      ...issue,
+      status: 'In Progress'
+    })
+  })
+
+  test('First user change assignee, second user should see assigned issue', async ({ page, browser }) => {
+    const issue: NewIssue = {
+      title: 'First user change assignee, second user should see assigned issue',
+      description: 'Issue for collaborative test'
+    }
+
+    await (await page.goto(`${PlatformURI}/workbench/sanity-ws/tracker/`))?.finished()
+    const issuesPage = new IssuesPage(page)
+    await issuesPage.linkSidebarAll.click()
+    await issuesPage.modelSelectorBacklog.click()
+    await issuesPage.searchIssueByName(issue.title)
+    await issuesPage.openIssueByName(issue.title)
+
+    const issuesDetailsPage = new IssuesDetailsPage(page)
+    await issuesDetailsPage.editIssue({ assignee: 'Dirak Kainin' })
+
+    // check by another user
+    const userSecondContext = await browser.newContext({ storageState: PlatformSettingSecond })
+    const userSecondPage = await userSecondContext.newPage()
+
+    // check notification
+    const leftSideMenuPage = new LeftSideMenuPage(page)
+    await leftSideMenuPage.buttonNotification.click()
+
+    // check issue
+    await (await userSecondPage.goto(`${PlatformURI}/workbench/sanity-ws/tracker/`))?.finished()
+
+    const issuesPageSecond = new IssuesPage(userSecondPage)
+    await issuesPageSecond.linkSidebarMyIssue.click()
+    await issuesPageSecond.modelSelectorActive.click()
 
     await issuesPageSecond.modelSelectorAll.click()
     await issuesPageSecond.searchIssueByName(issue.title)
--- tests/sanity/tests/model/left-side-menu-page.ts
+++ tests/sanity/tests/model/left-side-menu-page.ts
@@ -5,11 +5,13 @@ export class LeftSideMenuPage {
   readonly buttonChunter: Locator
   readonly buttonContacts: Locator
   readonly buttonTracker: Locator
+  readonly buttonNotification: Locator
 
   constructor (page: Page) {
     this.page = page
     this.buttonChunter = page.locator('button[id$="ApplicationLabelChunter"]')
     this.buttonContacts = page.locator('button[id$="Contacts"]')
     this.buttonTracker = page.locator('button[id$="TrackerApplication"]')
+    this.buttonNotification = page.locator('a[href$="notification"]')
   }
 }
