1fe85ceba82bf2ffd21baf422729f12280900d38
Alex Velichko <nestor_007@mail.ru>
2023-11-27 12:14:42 +0300

    TESTS-56: feat(tests): done Create a Template test (#4063)
    Signed-off-by: Alex Velichko <nestor_007@mail.ru>

---
+++ tests/sanity/tests/model/tracker/template-details-page.ts
@@ -0,0 +1,49 @@
+import { expect, type Locator, type Page } from '@playwright/test'
+import { CommonTrackerPage } from './common-tracker-page'
+import { NewIssue } from './types'
+
+export class TemplateDetailsPage extends CommonTrackerPage {
+  readonly page: Page
+  readonly inputTitle: Locator
+  readonly inputDescription: Locator
+  readonly buttonPriority: Locator
+  readonly buttonAssignee: Locator
+  readonly buttonAddLabel: Locator
+  readonly textLabels: Locator
+  readonly buttonComponent: Locator
+  readonly textEstimation: Locator
+
+  constructor (page: Page) {
+    super(page)
+    this.page = page
+    this.inputTitle = page.locator('div.popupPanel-body input[type="text"]')
+    this.inputDescription = page.locator('div.popupPanel-body div.textInput p')
+    this.buttonPriority = page.locator('//span[text()="Priority"]/../button[1]//span')
+    this.buttonAssignee = page.locator('(//span[text()="Assignee"]/../div/button)[1]')
+    this.textLabels = page.locator('div.menu-group span')
+    this.buttonAddLabel = page.locator('//span[text()="Labels"]/../button[2]//span')
+    this.buttonComponent = page.locator('//span[text()="Component"]/../div/div/button')
+    this.textEstimation = page.locator('(//span[text()="Estimation"]/../div/button)[3]')
+  }
+
+  async checkTemplate (data: NewIssue): Promise<void> {
+    await expect(this.inputTitle).toHaveValue(data.title)
+    await expect(this.inputDescription).toHaveText(data.description)
+    if (data.priority != null) {
+      await expect(this.buttonPriority).toHaveText(data.priority)
+    }
+    if (data.assignee != null) {
+      await expect(this.buttonAssignee).toHaveText(data.assignee)
+    }
+    if (data.labels != null) {
+      await this.buttonAddLabel.click()
+      await expect(this.page.locator('div.menu-group span', { hasText: data.labels })).toBeVisible()
+    }
+    if (data.component != null) {
+      await expect(this.buttonComponent).toHaveText(data.component)
+    }
+    if (data.estimation != null) {
+      await expect(this.textEstimation).toHaveText(data.estimation)
+    }
+  }
+}
---
+++ tests/sanity/tests/tracker/template.spec.ts
@@ -0,0 +1,49 @@
+import { test } from '@playwright/test'
+import { generateId, PlatformSetting, PlatformURI } from '../utils'
+import { LeftSideMenuPage } from '../model/left-side-menu-page'
+import { NewIssue } from '../model/tracker/types'
+import { allure } from 'allure-playwright'
+import { TrackerNavigationMenuPage } from '../model/tracker/tracker-navigation-menu-page'
+import { TemplatePage } from '../model/tracker/templates-page'
+import { TemplateDetailsPage } from '../model/tracker/template-details-page'
+
+test.use({
+  storageState: PlatformSetting
+})
+
+test.describe('Tracker template tests', () => {
+  test.beforeEach(async ({ page }) => {
+    await allure.parentSuite('Tracker tests')
+    await (await page.goto(`${PlatformURI}/workbench/sanity-ws`))?.finished()
+  })
+
+  test('Create a Template', async ({ page }) => {
+    const newTemplate: NewIssue = {
+      title: `Template with all parameters-${generateId()}`,
+      description: 'Created template with all parameters',
+      priority: 'Urgent',
+      assignee: 'Dirak Kainin',
+      createLabel: true,
+      labels: `CREATE-TEMPLATE-${generateId()}`,
+      component: 'No component',
+      estimation: '2',
+      milestone: 'No Milestone'
+    }
+
+    const leftSideMenuPage = new LeftSideMenuPage(page)
+    await leftSideMenuPage.buttonTracker.click()
+
+    const trackerNavigationMenuPage = new TrackerNavigationMenuPage(page)
+    await trackerNavigationMenuPage.buttonTemplates.click()
+
+    const templatePage = new TemplatePage(page)
+    await templatePage.createNewTemplate(newTemplate)
+    await templatePage.openTemplate(newTemplate.title)
+
+    const templateDetailsPage = new TemplateDetailsPage(page)
+    await templateDetailsPage.checkTemplate({
+      ...newTemplate,
+      estimation: '2h'
+    })
+  })
+})
