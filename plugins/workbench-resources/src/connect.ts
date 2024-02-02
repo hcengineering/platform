4b4ee00267c3cbe9f64ea5b8743b2e72ce518a14
Alexander Onnikov <alexander.onnikov@xored.com>
2024-02-02 22:48:21 +0700

    Revert "UBERF-5237 Use path in token cookie (#4480)"
    This reverts commit e36ae8b069e14649b9fa84d2360bbdfaf1d78512.
    
    Signed-off-by: Alexander Onnikov <alexander.onnikov@xored.com>

--- plugins/workbench-resources/src/connect.ts
+++ plugins/workbench-resources/src/connect.ts
@@ -51,14 +51,9 @@ export async function connect (title: string): Promise<Client | undefined> {
   }
   const tokens: Record<string, string> = fetchMetadataLocalStorage(login.metadata.LoginTokens) ?? {}
   const token = tokens[ws]
-  const path = `/${loc.path[0]}/${loc.path[1]}`
   setMetadata(presentation.metadata.Token, token)
   document.cookie =
-    encodeURIComponent(presentation.metadata.Token.replaceAll(':', '-')) +
-    '=' +
-    encodeURIComponent(token) +
-    '; path=' +
-    path
+    encodeURIComponent(presentation.metadata.Token.replaceAll(':', '-')) + '=' + encodeURIComponent(token) + '; path=/'
 
   const endpoint = fetchMetadataLocalStorage(login.metadata.LoginEndpoint)
   const email = fetchMetadataLocalStorage(login.metadata.LoginEmail)
