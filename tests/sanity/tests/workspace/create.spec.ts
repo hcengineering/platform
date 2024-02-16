d4a5007e79ada04922e2f798b621dd230e1d0a5d
Andrey Sobolev <haiodo@users.noreply.github.com>
2024-02-17 00:51:32 +0700

    UBERF-5575: Fix workspace join (#4684)
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- tests/sanity/tests/workspace/create.spec.ts
+++ tests/sanity/tests/workspace/create.spec.ts
@@ -9,6 +9,7 @@ import { NewIssue } from '../model/tracker/types'
 import { IssuesPage } from '../model/tracker/issues-page'
 import { IssuesDetailsPage } from '../model/tracker/issues-details-page'
 import { TrackerNavigationMenuPage } from '../model/tracker/tracker-navigation-menu-page'
+import { SignInJoinPage } from '../model/signin-page'
 
 test.describe('Workspace tests', () => {
   test('Create a workspace with a custom name', async ({ page }) => {
@@ -121,5 +122,113 @@ test.describe('Workspace tests', () => {
     await selectWorkspacePage.checkInfo(page, 'Required field Workspace name')
     await selectWorkspacePage.buttonWorkspaceName.fill(newWorkspaceName)
     await selectWorkspacePage.checkInfoSectionNotExist(page)
+  })
+
+  test('Create a workspace with join link', async ({ page, browser }) => {
+    const newUser: SignUpData = {
+      firstName: `FirstName-${generateId()}`,
+      lastName: `LastName-${generateId()}`,
+      email: `email+${generateId()}@gmail.com`,
+      password: '1234'
+    }
+    const newWorkspaceName = `Some HULY #@$ WS - ${generateId(12)}`
+
+    const loginPage = new LoginPage(page)
+    await loginPage.goto()
+    await loginPage.linkSignUp.click()
+
+    const signUpPage = new SignUpPage(page)
+    await signUpPage.signUp(newUser)
+
+    const selectWorkspacePage = new SelectWorkspacePage(page)
+    await selectWorkspacePage.buttonCreateWorkspace.click()
+    await selectWorkspacePage.createWorkspace(newWorkspaceName)
+
+    const leftSideMenuPage = new LeftSideMenuPage(page)
+    await leftSideMenuPage.buttonTracker.click()
+
+    // Generate invite link
+
+    await page.click('#profile-button')
+    await page.click('button:has-text("Invite to workspace")')
+    await page.click('button:has-text("Get invite link")')
+
+    const linkText = await page.locator('.antiPopup .link').textContent()
+
+    const page2 = await browser.newPage()
+
+    await page2.goto(linkText ?? '')
+
+    const newUser2: SignUpData = {
+      firstName: `FirstName2-${generateId()}`,
+      lastName: `LastName2-${generateId()}`,
+      email: `email+${generateId()}@gmail.com`,
+      password: '1234'
+    }
+
+    await page2.getByRole('link', { name: 'Sign Up' }).click()
+    const signUpPage2 = new SignUpPage(page2)
+    await signUpPage2.signUp(newUser2, 'join')
+
+    const leftSideMenuPage2 = new LeftSideMenuPage(page2)
+    await leftSideMenuPage2.buttonTracker.click()
+  })
+
+  test('Create a workspace with join link - existing account', async ({ page, browser }) => {
+    const newUser: SignUpData = {
+      firstName: `FirstName-${generateId()}`,
+      lastName: `LastName-${generateId()}`,
+      email: `email+${generateId()}@gmail.com`,
+      password: '1234'
+    }
+    const newWorkspaceName = `Some HULY #@$ WS - ${generateId(12)}`
+
+    const loginPage = new LoginPage(page)
+    await loginPage.goto()
+    await loginPage.linkSignUp.click()
+
+    const signUpPage = new SignUpPage(page)
+    await signUpPage.signUp(newUser)
+
+    const selectWorkspacePage = new SelectWorkspacePage(page)
+    await selectWorkspacePage.buttonCreateWorkspace.click()
+    await selectWorkspacePage.createWorkspace(newWorkspaceName)
+
+    const leftSideMenuPage = new LeftSideMenuPage(page)
+    await leftSideMenuPage.buttonTracker.click()
+
+    // Generate invite link
+
+    await page.click('#profile-button')
+    await page.click('button:has-text("Invite to workspace")')
+    await page.click('button:has-text("Get invite link")')
+
+    const linkText = await page.locator('.antiPopup .link').textContent()
+
+    const page2 = await browser.newPage()
+
+    const loginPage2 = new LoginPage(page2)
+    await loginPage2.goto()
+    await loginPage2.linkSignUp.click()
+
+    const newUser2: SignUpData = {
+      firstName: `FirstName2-${generateId()}`,
+      lastName: `LastName2-${generateId()}`,
+      email: `email+${generateId()}@gmail.com`,
+      password: '1234'
+    }
+
+    const signUpPage2 = new SignUpPage(page2)
+    await signUpPage2.signUp(newUser2)
+
+    // Ok we signed in, and no workspace present.
+
+    await page2.goto(linkText ?? '')
+
+    const joinPage = new SignInJoinPage(page2)
+    await joinPage.join(newUser2)
+
+    const leftSideMenuPage2 = new LeftSideMenuPage(page2)
+    await leftSideMenuPage2.buttonTracker.click()
   })
 })
