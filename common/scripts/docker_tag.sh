279c863c8e00a8a34c4745afccd91fc9249a1dfa
Andrey Sobolev <haiodo@gmail.com>
2024-03-08 23:18:11 +0700

    Fix docker build
    We should tag containers locally with git revision, so in case of concurrent builds using same docker, we not mark wrong containers with a new version.
    
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- common/scripts/docker_tag.sh
+++ common/scripts/docker_tag.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 version=$(git describe --tags --abbrev=0)
+rev_version=$(git rev-parse HEAD)
 
 if [ "x$2" = "xstaging" ]
 then
@@ -9,11 +10,12 @@ then
   ((c++))
   version="${a[0]}.${a[1]}.${c}-staging"
   echo "Tagging stating $1 with version ${version}"
-  docker tag "$1" "$1:$version"
+  docker tag "$1:$rev_version" "$1:$version"
   docker push "$1:$version"
 else
   echo "Tagging release $1 with version ${version}"  
-  docker tag "$1" "$1:$version"
+  docker tag "$1:$rev_version" "$1:$version"
+  docker tag "$1:$rev_version" "$1:latest"
   docker push "$1:$version"
   docker push "$1:latest"
 fi
