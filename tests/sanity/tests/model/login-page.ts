33ad1027665f05c972e624534302ed9ee48f9b9f
Walter <w.obadha@gmail.com>
2024-04-05 08:38:25 +0300

    test coverage for login test cases
    Signed-off-by: Walter <w.obadha@gmail.com>

--- tests/sanity/tests/model/login-page.ts
+++ tests/sanity/tests/model/login-page.ts
@@ -7,23 +7,41 @@ export class LoginPage {
   readonly inputPassword: Locator
   readonly buttonLogin: Locator
   readonly linkSignUp: Locator
+  readonly incorrectEmailTxt: Locator
+  readonly incorrectPasswordTxt: Locator
 
-  constructor (page: Page) {
+  constructor(page: Page) {
     this.page = page
     this.inputEmail = page.locator('input[name=email]')
     this.inputPassword = page.locator('input[name=current-password]')
     this.buttonLogin = page.locator('button', { hasText: 'Log In' })
     this.linkSignUp = page.locator('a.title', { hasText: 'Sign Up' })
+    this.incorrectEmailTxt = page.locator('a.text-sm', { hasText: 'Account not found' })
+    this.incorrectPasswordTxt = page.locator('a.text-sm', { hasText: 'Invalid password' })
   }
 
-  async goto (): Promise<void> {
+  async goto(): Promise<void> {
     await (await this.page.goto(`${PlatformURI}/login/login`))?.finished()
   }
 
-  async login (email: string, password: string): Promise<void> {
+  async login(email: string, password: string): Promise<void> {
     await this.inputEmail.fill(email)
     await this.inputPassword.fill(password)
     expect(await this.buttonLogin.isEnabled()).toBe(true)
     await this.buttonLogin.click()
+  }
+
+  async incorrectEmailTxtVisible() {
+    const incorrectEmailTxt = this.incorrectEmailTxt
+    return await incorrectEmailTxt.isVisible()
+  }
+
+  async incorrectPasswordTxtVisible() {
+    const incorrectPasswordTxt = this.incorrectPasswordTxt
+    return await incorrectPasswordTxt.isVisible()
+  }
+
+  async verifyURL() {
+    await expect(this.page).toHaveURL(/login/)
   }
 }
