4fc7df15ad5cfcea7db47af207fbb4fe053fff8a
Andrey Vishin <vishin64@gmail.com>
2024-04-12 01:21:19 -0400

    Login Test: add negative scenarios

--- tests/sanity/tests/login.spec.ts
+++ tests/sanity/tests/login.spec.ts
@@ -1,19 +1,68 @@
-import { test } from '@playwright/test'
+import { Page, test } from '@playwright/test'
 import { PlatformUser } from './utils'
 import { LoginPage } from './model/login-page'
 import { SelectWorkspacePage } from './model/select-workspace-page'
 
+const setPageErrorListener = (page: Page): void => {
+  page.on('pageerror', (exception) => {
+    console.log('Uncaught exception:')
+    console.log(exception.message)
+  })
+}
+
+const gotoLoginPage = async (page: Page): Promise<LoginPage> => {
+  const loginPage = new LoginPage(page)
+  await loginPage.goto()
+  return loginPage
+}
+
 test.describe('login test', () => {
-  test('check login', async ({ page }) => {
-    page.on('pageerror', (exception) => {
-      console.log('Uncaught exception:')
-      console.log(exception.message)
-    })
-    const loginPage = new LoginPage(page)
-    await loginPage.goto()
+  const invalidUser: string = 'WrongUserName'
+  const invalidPass: string = '1111'
+  const validPass: string = '1234'
+  // TODO Move the valid password to the .env file or to the environment variables in IDE
+
+  test('Check the button and messages', async ({ page }) => {
+    setPageErrorListener(page)
+    const loginPage = await gotoLoginPage(page)
+    await loginPage.isButtonLoginDisabled()
+    await loginPage.isAlertReqEmailShown()
+
+    await loginPage.fillCredentials('1', '')
+    await loginPage.isButtonLoginDisabled()
+    await loginPage.isAlertReqPassShown()
+
+    await loginPage.fillCredentials('', '2')
+    await loginPage.isButtonLoginDisabled()
+    await loginPage.isAlertReqEmailShown()
+
+    await loginPage.fillCredentials('1', '2')
+    await loginPage.isButtonLoginEnabled()
+  })
+
+  test('Login with invalid user name', async ({ page }) => {
+    setPageErrorListener(page)
+    const loginPage = await gotoLoginPage(page)
+    await loginPage.login(invalidUser, validPass)
+
+    await loginPage.isAlertWrongAccShown()
+  })
+
+  test('Login with invalid password', async ({ page }) => {
+    setPageErrorListener(page)
+    const loginPage = await gotoLoginPage(page)
+    await loginPage.login(PlatformUser, invalidPass)
+
+    await loginPage.isAlertWrongPassShown()
+  })
+
+  test('Login with valid credentials', async ({ page }) => {
+    setPageErrorListener(page)
+    const loginPage = await gotoLoginPage(page)
     await loginPage.login(PlatformUser, '1234')
 
     const selectWorkspacePage = new SelectWorkspacePage(page)
     await selectWorkspacePage.selectWorkspace('SanityTest')
   })
 })
+
--- tests/sanity/tests/model/login-page.ts
+++ tests/sanity/tests/model/login-page.ts
@@ -7,6 +7,10 @@ export class LoginPage {
   readonly inputPassword: Locator
   readonly buttonLogin: Locator
   readonly linkSignUp: Locator
+  readonly alrtWrongPass: string
+  readonly alrtWrongAcc: string
+  readonly alrtReqEmail: string
+  readonly alrtReqPass: string
 
   constructor (page: Page) {
     this.page = page
@@ -14,6 +18,10 @@ export class LoginPage {
     this.inputPassword = page.locator('input[name=current-password]')
     this.buttonLogin = page.locator('button', { hasText: 'Log In' })
     this.linkSignUp = page.locator('a.title', { hasText: 'Sign Up' })
+    this.alrtWrongPass = 'Invalid password'
+    this.alrtWrongAcc = 'Account not found'
+    this.alrtReqEmail = 'Required field Email'
+    this.alrtReqPass = 'Required field Password'
   }
 
   async goto (): Promise<void> {
@@ -21,9 +29,43 @@ export class LoginPage {
   }
 
   async login (email: string, password: string): Promise<void> {
+    await this.fillCredentials(email, password)
+    await this.isButtonLoginEnabled()
+    await this.buttonLogin.click()
+  }
+
+  private async isAlertShown (msgAlert: string): Promise<void> {
+    await expect.soft(this.page // Use soft assertion to check all the messages
+      .getByText(msgAlert), 'Alert not shown: ' + msgAlert)
+      .toBeVisible()
+  }
+
+  async fillCredentials (email: string, password: string): Promise<void> {
     await this.inputEmail.fill(email)
     await this.inputPassword.fill(password)
-    expect(await this.buttonLogin.isEnabled()).toBe(true)
-    await this.buttonLogin.click()
+  }
+
+  async isAlertWrongAccShown (): Promise<void> {
+    await this.isAlertShown(this.alrtWrongAcc)
+  }
+
+  async isAlertWrongPassShown (): Promise<void> {
+    await this.isAlertShown(this.alrtWrongPass)
+  }
+
+  async isAlertReqEmailShown (): Promise<void> {
+    await this.isAlertShown(this.alrtReqEmail)
+  }
+
+  async isAlertReqPassShown (): Promise<void> {
+    await this.isAlertShown(this.alrtReqPass)
+  }
+
+  async isButtonLoginEnabled (): Promise<void> {
+    await expect(this.buttonLogin, 'Login button is not enabled').toBeEnabled()
+  }
+
+  async isButtonLoginDisabled (): Promise<void> {
+    await expect(this.buttonLogin, 'Login button is not disabled').toBeDisabled()
   }
 }
