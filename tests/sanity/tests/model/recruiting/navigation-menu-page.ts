94f477691ce791be5c1c10795f97324e2c47a2d3
Jasmin <jasmin@hardcoreeng.com>
2024-04-18 07:45:48 +0200

    Update navigation-menu-page.ts
    Signed-off-by: Jasmin Music <jasmin@hardcoreeng.com>

--- tests/sanity/tests/model/recruiting/navigation-menu-page.ts
+++ tests/sanity/tests/model/recruiting/navigation-menu-page.ts
@@ -1,38 +1,39 @@
-
-import { type Locator, type Page } from '@playwright/test';
+import { type Locator, type Page } from '@playwright/test'
 
 export class NavigationMenuPage {
-    readonly page: Page;
+  readonly page: Page
 
-    constructor(page: Page) {
-        this.page = page;
-    }
+  constructor (page: Page) {
+    this.page = page
+  }
 
-    // Locator methods
-    readonly buttonApplications = (): Locator => this.page.locator('a[href$="candidates"]', { hasText: 'Applications' });
-    readonly buttonMyApplications = (): Locator => this.page.locator('a[href$="my-applications"]', { hasText: 'My applications' });
-    readonly buttonTalents = (): Locator => this.page.locator('a[href$="talents"]', { hasText: 'Talents' });
-    readonly buttonVacancies = (): Locator => this.page.locator('a[href$="vacancies"]', { hasText: 'Vacancies' });
-    readonly buttonCompanies = (): Locator => this.page.locator('a[href$="organizations"]', { hasText: 'Companies' });
+  // Locator methods
+  readonly buttonApplications = (): Locator => this.page.locator('a[href$="candidates"]', { hasText: 'Applications' })
+  readonly buttonMyApplications = (): Locator =>
+    this.page.locator('a[href$="my-applications"]', { hasText: 'My applications' })
 
-    // Action methods to click on each button
-    async clickButtonApplications(): Promise<void> {
-        await this.buttonApplications().click();
-    }
+  readonly buttonTalents = (): Locator => this.page.locator('a[href$="talents"]', { hasText: 'Talents' })
+  readonly buttonVacancies = (): Locator => this.page.locator('a[href$="vacancies"]', { hasText: 'Vacancies' })
+  readonly buttonCompanies = (): Locator => this.page.locator('a[href$="organizations"]', { hasText: 'Companies' })
 
-    async clickButtonMyApplications(): Promise<void> {
-        await this.buttonMyApplications().click();
-    }
+  // Action methods to click on each button
+  async clickButtonApplications (): Promise<void> {
+    await this.buttonApplications().click()
+  }
 
-    async clickButtonTalents(): Promise<void> {
-        await this.buttonTalents().click();
-    }
+  async clickButtonMyApplications (): Promise<void> {
+    await this.buttonMyApplications().click()
+  }
 
-    async clickButtonVacancies(): Promise<void> {
-        await this.buttonVacancies().click();
-    }
+  async clickButtonTalents (): Promise<void> {
+    await this.buttonTalents().click()
+  }
 
-    async clickButtonCompanies(): Promise<void> {
-        await this.buttonCompanies().click();
-    }
+  async clickButtonVacancies (): Promise<void> {
+    await this.buttonVacancies().click()
+  }
+
+  async clickButtonCompanies (): Promise<void> {
+    await this.buttonCompanies().click()
+  }
 }
