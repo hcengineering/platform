608ea96cc8b2fb0a740d683bf55165965503092f
Alexander Platov <alexander.platov@hardcoreeng.com>
2024-04-24 19:19:32 +0300

    Fixed workbench for guests (#5459)
    Signed-off-by: Alexander Platov <alexander.platov@hardcoreeng.com>

--- plugins/guest-resources/src/index.ts
+++ plugins/guest-resources/src/index.ts
@@ -1,4 +1,5 @@
 import { type Resources } from '@hcengineering/platform'
+import { type DefSeparators } from '@hcengineering/ui'
 import CreatePublicLink from './components/CreatePublicLink.svelte'
 import GuestApp from './components/GuestApp.svelte'
 
@@ -8,3 +9,5 @@ export default async (): Promise<Resources> => ({
     CreatePublicLink
   }
 })
+
+export const workbenchGuestSeparators: DefSeparators = [null, { minSize: 20, size: 30, maxSize: 50, float: 'aside' }]
