b4aceb7af3c2a5efd5c36d2649cf00addae1698e
Andrey Sobolev <haiodo@users.noreply.github.com>
2024-05-09 14:40:50 +0700

    Move UWS into separate plugin with proper case usage. (#5556)
    Move UWS into separate plugin with proper case usage.
    
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- server/ws/uws.sh
+++
@@ -1,8 +0,0 @@
-if test -f ./src/server_u.ts; then
-  if ! test -f ./v20.43.0.zip; then
-    wget --quiet https://github.com/uNetworking/uWebSockets.js/archive/refs/tags/v20.43.0.zip
-  fi
-  if ! test -f ./src/uws/uws.js; then
-    unzip -qq -j ./v20.43.0.zip -d ./src/uws/
-  fi
-fi
