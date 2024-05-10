26e7a2fd83f219b2cd4eca98a838552eb8ae4840
Alexander Onnikov <Alexander.Onnikov@xored.com>
2024-05-10 23:05:46 +0700

    fix: hide actions for archived teamspaces
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

--- models/document/src/index.ts
+++ models/document/src/index.ts
@@ -224,7 +224,7 @@ function defineTeamspace (builder: Builder): void {
   // Actions
 
   builder.mixin(document.class.Teamspace, core.class.Class, view.mixin.IgnoreActions, {
-    actions: [tracker.action.EditRelatedTargets]
+    actions: [tracker.action.EditRelatedTargets, tracker.action.NewRelatedIssue]
   })
 
   createAction(
@@ -256,6 +256,9 @@ function defineTeamspace (builder: Builder): void {
       category: document.category.Document,
       target: document.class.Teamspace,
       inline: true,
+      query: {
+        archived: false
+      },
       context: {
         mode: ['context', 'browser'],
         application: document.app.Documents,
