45b5af9605a9c06ace2d8c9e4d09fc13a3c0c530
Kristina Fefelova <kristin.fefelova@gmail.com>
2024-05-22 11:00:37 +0400

    Fix ui tests
    Signed-off-by: Kristina Fefelova <kristin.fefelova@gmail.com>

--- tests/sanity/tests/model/channel-page.ts
+++ tests/sanity/tests/model/channel-page.ts
@@ -12,7 +12,7 @@ export class ChannelPage {
   readonly textMessage = (): Locator => this.page.getByText('Test message')
   readonly channelName = (channel: string): Locator => this.page.getByText('general random').getByText(channel)
   readonly channelTab = (): Locator => this.page.getByRole('link', { name: 'Channels' }).getByRole('button')
-  readonly channelTable = (): Locator => this.page.locator('[class="antiTable metaColumn highlightRows"]')
+  readonly channelTable = (): Locator => this.page.getByRole('table')
   readonly channel = (channel: string): Locator => this.page.getByRole('button', { name: channel })
 
   async sendMessage (message: string): Promise<void> {
