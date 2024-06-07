838c3e39356b995e235839045e12bde2154b1fe2
JasminMus <167111741+JasminMus@users.noreply.github.com>
2024-06-07 18:08:21 +0200

    Onboarding workspace test preparation (#5708)
    Signed-off-by: Jasmin <jasmin@hardcoreeng.com>
    Signed-off-by: Keshav Ashiya <keshavashiya@outlook.com>

--- tests/sanity/tests/model/select-workspace-page.ts
+++ tests/sanity/tests/model/select-workspace-page.ts
@@ -15,13 +15,18 @@ export class SelectWorkspacePage extends CommonPage {
   buttonCreateNewWorkspace = (): Locator => this.page.locator('div.form-row button')
   workspaceButtonByName = (workspace: string): Locator => this.buttonWorkspace().filter({ hasText: workspace })
   createAnotherWorkspace = (): Locator => this.page.getByRole('link', { name: 'Create workspace' })
-
+  workspaceLogo = (): Locator => this.page.getByText('N', { exact: true })
+  workspaceList = (workspaceName: string): Locator => this.page.getByRole('button', { name: workspaceName })
   async selectWorkspace (workspace: string): Promise<void> {
     await this.workspaceButtonByName(workspace).click()
   }
 
   async enterWorkspaceName (workspaceName: string): Promise<void> {
     await this.buttonWorkspaceName().fill(workspaceName)
+  }
+
+  async clickCreateWorkspaceLogo (): Promise<void> {
+    await this.workspaceLogo().click()
   }
 
   async createWorkspace (workspaceName: string, worskpaceNew: boolean = true): Promise<void> {
@@ -36,5 +41,9 @@ export class SelectWorkspacePage extends CommonPage {
       expect(await this.buttonCreateNewWorkspace().isEnabled()).toBe(true)
       await this.buttonCreateNewWorkspace().click()
     }
+  }
+
+  async checkIfWorkspaceExists (workspace: string): Promise<void> {
+    await expect(this.workspaceList(workspace)).toBeVisible()
   }
 }
---
+++ tests/sanity/tests/text/issueOnboardingText.ts
@@ -0,0 +1,81 @@
+export const helloAndWelcomeToHuly = [
+  'Hello and Welcome to Huly! 🌟 Embark on an exciting journey to master our next-generation project management tool.',
+  "As you tackle these initial issues, you'll uncover essential features and ProTips designed to streamline your workflow.",
+  "Once you're familiar with Huly, feel free to delete these starter issues or share them with your team.",
+  'Start by Creating Your First Issue',
+  'Simply press C from any view to create a new issue.',
+  'You can also click the New Issue button for the same action.',
+  "Dynamic Features of Your Issue Editor: Huly's issue editor is versatile, supporting Markdown for rich text formatting and offering several advanced features:",
+  'Real-Time Collaborative Editor for Issue Descriptions: Uniquely designed for team synergy, the issue description in Huly allows for real-time, collaborative editing.',
+  'Your team can work together on the same description simultaneously, enhancing productivity and collaboration.',
+  'Enhanced Communication in Comments: Although comments are not collaboratively editable, you can still enrich them with @mentions, videos, and emojis.',
+  'This flexibility ensures clear and engaging communication across your team.',
+  'Enhance Team Collaboration:',
+  'Use @mention to involve teammates or reference other items within Huly, making collaboration seamless.',
+  'Drag and drop images or videos into your issues and comments for a comprehensive and visual communication experience.',
+  'Add emojis ✅ to bring personality and clarity to your discussions.',
+  'Dive into the world of Huly, where creating impactful issues and harnessing the power of collaborative project management is just the beginning.',
+  'Discover the ease and efficiency of managing your projects with us!'
+]
+
+export const toolsAndTimeBlocking = [
+  'In Huly, Todos and Issues serve distinct yet interconnected purposes.',
+  'While Issues are collaborative and represent various processes like a Feature Development cycle, Todos are personalized tasks crucial for advancing these processes.',
+  'Please read more about Todos here @Understanding Todos in Huly.',
+  'Step-by-Step Guide to Managing a Todo',
+  'Assign the Issue to Yourself: First, ensure the Issue is assigned to you. Change its status from Backlog to Todo, as shown in the image below:',
+  "Todo Creation in Personal Planner: Once you've done this, Huly automatically creates a corresponding Todo in your Personal Planner:",
+  "Find Your Unscheduled Todo: In your Personal Planner, you'll find the newly created Todo listed as Unscheduled.",
+  "Scheduling Your Todo: It’s time to plan when to work on this Todo. Let's schedule it from 15:00 to 17:00.",
+  'You can do this either by dragging the Todo to your desired time slot on the Calendar or by opening the Todo and adding one or more work slots:',
+  'Tracking Progress: After scheduling the Todo, the state of the corresponding Task in Huly changes to In Progress.',
+  'The system also accounts for the 2 hours of work committed to the task, which in this case is @HI-2.',
+  'In the upcoming sections of this tutorial, we’ll explore the multifaceted benefits and applications of Todos in process management, planning, and execution within Huly.',
+  'Example of such benefits is @Team Planning in Huly.',
+  'Stay tuned to learn how to make the most out of this powerful feature!'
+]
+
+export const navigationHully = [
+  'In Huly, we offer three intuitive ways to navigate and manage your workflow: using the Command Line, Keyboard Shortcuts, or the Mouse.',
+  'Each method is designed to enhance your productivity and ease of use within the application.',
+  '1. Command Line - The Power of Cmd/Ctrl + K:',
+  "Cmd/Ctrl + K: This is one of Huly's most powerful features. Use this command line shortcut to swiftly search for items or execute any action within the application. It's your go-to tool for quick navigation and task management.",
+  '2. Keyboard Shortcuts - For the Shortcut Enthusiast:',
+  'Discovering Shortcuts: If keyboard shortcuts are your preference, Huly has you covered. Access a comprehensive list of shortcuts by clicking on Help & Support located in the lower-left corner of the application. These shortcuts are designed to streamline your workflow and save you time.',
+  '3. Mouse Navigation - Contextual Menus and More:',
+  "Right-Click for Contextual Menus: Prefer using a mouse? Right-click on any issue to open contextual menus. These menus provide easy access to various actions and are also an excellent way to familiarize yourself with Huly's keyboard shortcuts.",
+  "Learning and Flexibility: Whether you are a fan of quick command-line actions, efficient keyboard shortcuts, or the traditional mouse-click approach, Huly adapts to your style. These navigation methods not only offer flexibility but also help you learn the application's functionality more deeply. Try them out and find the one that best suits your workflow!"
+]
+
+export const connectToGithub = [
+  "Huly's bi-directional integration with GitHub brings together the best of both platforms.",
+  'This integration means that activities on GitHub, including Issues, Pull Requests (PRs), Comments, and Change Requests, are instantly updated in your synchronized Huly project, and vice versa.',
+  'Key Features of Huly-GitHub Integration:',
+  'Bi-Directional Synchronization: Ensures real-time updates between Huly and GitHub, providing a cohesive project management experience.',
+  'Milestone-Based Board Management: Huly Milestones correspond to boards in GitHub Projects, maintaining a two-way synchronization for effective board management.',
+  'Flexible Repository Management: Manage individual repositories or entire GitHub organizations and handle multiple GitHub repositories within a single Huly Project.',
+  'Empowered by Huly Automation Capabilities:',
+  'Integration with Huly Automation: Synchronizing your GitHub projects with Huly unlocks additional automation features like Todos and Team Planning, streamlining your project management processes.',
+  'Efficient Workflows: Automations integrate tasks like assignments, status updates, and scheduling, making your workflow more efficient and reducing manual workload.',
+  'Freedom and Flexibility:',
+  'Non-restrictive Use: You have the freedom to stop using Huly at any time. If you decide to discontinue Huly, all your project information will remain intact in GitHub.',
+  "Data Continuity: There's no risk of losing your project data. Even after dropping Huly, your GitHub projects will retain all their information and history.",
+  'Setting Up Your Integration:',
+  'Create a New Project in Huly: Start by creating the project you wish to synchronize with GitHub.',
+  "Navigate to Integrations: In your Huly workspace, go to the 'Integrations' section.",
+  "Connect and Install: Choose 'Connect GitHub' and install the Huly app in your GitHub account or organization.",
+  'Repository Selection and Mapping: Select the GitHub repository you want to sync, and then map it to the corresponding project in Huly.',
+  "Leverage the power of Huly's advanced features with your GitHub projects and enjoy a more streamlined project management experience.",
+  "Remember, you're always in control and can choose to use Huly as per your project needs without any commitment."
+]
+
+export const proTip = [
+  'Issue Peek is one hundreds little nice Huly features.',
+  'Turn on/off with the space bar.',
+  'To scroll through issues, type J/K or move the mouse.'
+]
+
+export const customizeViewsAndDisplay = [
+  'Customize grouping and sort order of your list and choose what properties show up with Display Options:',
+  'Use Filters to narrow down objects by values of their fields:'
+]
