2383a93a059ef6ab28a7357043f175c53b286716
Keshav Ashiya <keshavashiya@outlook.com>
2024-06-11 20:44:06 +0530

    Revert "Add OperationContext to be accessible by triggers (#5753)"
    This reverts commit 90e171fa6a33dd1a260ce495d600c58752e592fb.
    
    Signed-off-by: Keshav Ashiya <keshavashiya@outlook.com>

--- server/core/src/types.ts
+++ server/core/src/types.ts
@@ -139,7 +139,6 @@ export interface Pipeline extends LowLevelStorage {
  * @public
  */
 export interface TriggerControl {
-  operationContext: SessionOperationContext
   ctx: MeasureContext
   workspace: WorkspaceIdWithUrl
   txFactory: TxFactory
