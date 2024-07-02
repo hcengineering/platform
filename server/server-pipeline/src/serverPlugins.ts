c77ac18747a7b2f53a47036c692d1db6e4fbd199
Andrey Sobolev <haiodo@users.noreply.github.com>
2024-07-02 18:09:44 +0700

    UBERF-7465: Move pipeline into separate plugin (#5978)
    Signed-off-by: Jasmin <jasmin@hardcoreeng.com>

---
+++ server/server-pipeline/src/serverPlugins.ts
@@ -0,0 +1,58 @@
+import { openAIId, openAIPluginImpl } from '@hcengineering/openai'
+import { addLocation } from '@hcengineering/platform'
+import { serverActivityId } from '@hcengineering/server-activity'
+import { serverAttachmentId } from '@hcengineering/server-attachment'
+import { serverCalendarId } from '@hcengineering/server-calendar'
+import { serverChunterId } from '@hcengineering/server-chunter'
+import { serverCollaborationId } from '@hcengineering/server-collaboration'
+import { serverContactId } from '@hcengineering/server-contact'
+import { serverDocumentsId } from '@hcengineering/server-controlled-documents'
+import { serverDocumentId } from '@hcengineering/server-document'
+import { serverDriveId } from '@hcengineering/server-drive'
+import { serverGmailId } from '@hcengineering/server-gmail'
+import { serverGuestId } from '@hcengineering/server-guest'
+import { serverHrId } from '@hcengineering/server-hr'
+import { serverInventoryId } from '@hcengineering/server-inventory'
+import { serverLeadId } from '@hcengineering/server-lead'
+import { serverLoveId } from '@hcengineering/server-love'
+import { serverNotificationId } from '@hcengineering/server-notification'
+import { serverRecruitId } from '@hcengineering/server-recruit'
+import { serverRequestId } from '@hcengineering/server-request'
+import { serverSettingId } from '@hcengineering/server-setting'
+import { serverTagsId } from '@hcengineering/server-tags'
+import { serverTaskId } from '@hcengineering/server-task'
+import { serverTelegramId } from '@hcengineering/server-telegram'
+import { serverTimeId } from '@hcengineering/server-time'
+import { serverTrackerId } from '@hcengineering/server-tracker'
+import { serverTrainingId } from '@hcengineering/server-training'
+import { serverViewId } from '@hcengineering/server-view'
+
+export function registerServerPlugins (): void {
+  addLocation(serverActivityId, () => import('@hcengineering/server-activity-resources'))
+  addLocation(serverAttachmentId, () => import('@hcengineering/server-attachment-resources'))
+  addLocation(serverCollaborationId, () => import('@hcengineering/server-collaboration-resources'))
+  addLocation(serverContactId, () => import('@hcengineering/server-contact-resources'))
+  addLocation(serverNotificationId, () => import('@hcengineering/server-notification-resources'))
+  addLocation(serverSettingId, () => import('@hcengineering/server-setting-resources'))
+  addLocation(serverChunterId, () => import('@hcengineering/server-chunter-resources'))
+  addLocation(serverInventoryId, () => import('@hcengineering/server-inventory-resources'))
+  addLocation(serverLeadId, () => import('@hcengineering/server-lead-resources'))
+  addLocation(serverRecruitId, () => import('@hcengineering/server-recruit-resources'))
+  addLocation(serverTaskId, () => import('@hcengineering/server-task-resources'))
+  addLocation(serverTrackerId, () => import('@hcengineering/server-tracker-resources'))
+  addLocation(serverTagsId, () => import('@hcengineering/server-tags-resources'))
+  addLocation(serverCalendarId, () => import('@hcengineering/server-calendar-resources'))
+  addLocation(serverGmailId, () => import('@hcengineering/server-gmail-resources'))
+  addLocation(serverTelegramId, () => import('@hcengineering/server-telegram-resources'))
+  addLocation(serverRequestId, () => import('@hcengineering/server-request-resources'))
+  addLocation(serverViewId, () => import('@hcengineering/server-view-resources'))
+  addLocation(serverHrId, () => import('@hcengineering/server-hr-resources'))
+  addLocation(serverLoveId, () => import('@hcengineering/server-love-resources'))
+  addLocation(serverGuestId, () => import('@hcengineering/server-guest-resources'))
+  addLocation(openAIId, () => Promise.resolve({ default: openAIPluginImpl }))
+  addLocation(serverDocumentId, () => import('@hcengineering/server-document-resources'))
+  addLocation(serverTimeId, () => import('@hcengineering/server-time-resources'))
+  addLocation(serverDriveId, () => import('@hcengineering/server-drive-resources'))
+  addLocation(serverDocumentsId, () => import('@hcengineering/server-controlled-documents-resources'))
+  addLocation(serverTrainingId, () => import('@hcengineering/server-training-resources'))
+}
