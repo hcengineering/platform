12ac6194e723f418f6ca1f586425cb2ffa2c6921
Denis Bykhov <bykhov.denis@gmail.com>
2024-07-18 19:17:06 +0500

    Fix formatting
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- server/account/src/operations.ts
+++ server/account/src/operations.ts
@@ -69,7 +69,12 @@ import {
   StorageConfiguration,
   type StorageAdapter
 } from '@hcengineering/server-core'
-import { createIndexStages, createServerPipeline, registerServerPlugins, registerStringLoaders } from '@hcengineering/server-pipeline'
+import {
+  createIndexStages,
+  createServerPipeline,
+  registerServerPlugins,
+  registerStringLoaders
+} from '@hcengineering/server-pipeline'
 import { accountPlugin } from './plugin'
 
 const WORKSPACE_COLLECTION = 'workspace'
