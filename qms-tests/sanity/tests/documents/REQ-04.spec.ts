ac3d1345cb005c44b88f46902847d98fd8a72e49
Jasmin <jasmin@hardcoreeng.com>
2024-07-25 17:07:16 +0200

    Update REQ-04.spec.ts
    Signed-off-by: Jasmin <jasmin@hardcoreeng.com>

--- qms-tests/sanity/tests/documents/REQ-04.spec.ts
+++ qms-tests/sanity/tests/documents/REQ-04.spec.ts
@@ -25,10 +25,7 @@ test.describe('ISO 13485, 4.2.4 Control of documents', () => {
     await (await page.goto(`${PlatformURI}/${HomepageURI}`))?.finished()
   })
 
-  test('TESTS-399. Tool does not influence content: Entered text in a document does not change over time', async ({
-    page,
-    browser
-  }) => {
+  test('TESTS-271. Generate a PDF from an Effective doc', async ({ page, browser }) => {
     await allure.description(
       'Requirement\nUsers need to make a resolve all comments and done documents for the Effective status'
     )
@@ -96,7 +93,72 @@ test.describe('ISO 13485, 4.2.4 Control of documents', () => {
       const cleanedActualText = actualText.trim().replace(/\s+/g, ' ')
 
       expect(cleanedActualText).toContain(expectedText)
-      await attachScreenshot('TESTS-check_content.png', page)
+      await attachScreenshot('TESTS-270-check_content.png', page)
+    })
+  })
+
+  test.skip('TESTS-270. Generate a PDF from an Effective doc', async ({ page, browser }) => {
+    await allure.description('Requirement\nGenerate a PDF from an Effective doc')
+    await allure.tms('TESTS-270', 'https://front.hc.engineering/workbench/platform/tracker/TESTS-270')
+
+    const userSecondPage = await getSecondPage(browser)
+    const completeDocument: NewDocument = {
+      template: 'HR (HR)',
+      title: `Complete document-${generateId()}`,
+      description: `Complete document description-${generateId()}`
+    }
+    const reviewer = 'Dirak Kainin'
+    const documentDetails: DocumentDetails = {
+      type: 'HR',
+      category: 'Human Resources',
+      version: 'v0.1',
+      status: DocumentStatus.DRAFT,
+      owner: 'Appleseed John',
+      author: 'Appleseed John'
+    }
+    const newContent: Content = {
+      sectionTitle: `Overview-${generateId()}`,
+      content: faker.lorem.paragraphs(10)
+    }
+
+    await prepareDocumentStep(page, completeDocument)
+    const documentContentPage = new DocumentContentPage(page)
+    await documentContentPage.updateSectionTitle('1', newContent.sectionTitle)
+    await documentContentPage.addContentToTheSection(newContent)
+
+    await test.step('2. Send for Approval', async () => {
+      await documentContentPage.buttonSendForApproval.click()
+      await documentContentPage.fillSelectApproversForm([reviewer])
+      await documentContentPage.checkDocumentStatus(DocumentStatus.IN_APPROVAL)
+      await documentContentPage.checkDocument({
+        ...documentDetails,
+        status: DocumentStatus.IN_APPROVAL,
+        version: 'v0.1'
+      })
+      await documentContentPage.checkCurrentRights(DocumentRights.VIEWING)
+    })
+
+    await test.step('3. Approve document', async () => {
+      const documentsPageSecond = new DocumentsPage(userSecondPage)
+      await (await userSecondPage.goto(`${PlatformURI}/${DocumentURI}`))?.finished()
+      await documentsPageSecond.openDocument(completeDocument.title)
+
+      const documentContentPageSecond = new DocumentContentPage(userSecondPage)
+      await documentContentPageSecond.confirmApproval()
+
+      await documentContentPageSecond.checkDocumentStatus(DocumentStatus.EFFECTIVE)
+      await documentContentPageSecond.checkDocument({
+        ...documentDetails,
+        status: DocumentStatus.EFFECTIVE,
+        version: 'v0.1'
+      })
+      await documentContentPageSecond.checkCurrentRights(DocumentRights.VIEWING)
+
+      await attachScreenshot('TESTS-270_approve_document.png', page)
+    })
+
+    await test.step('4. Download PDF', async () => {
+      await attachScreenshot('TESTS-270-check_content.png', page)
     })
   })
 })
