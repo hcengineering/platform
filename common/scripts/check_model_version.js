74d76d34a4bca8cad250319a01ae0b0c986b4772
Denis Bykhov <bykhov.denis@gmail.com>
2024-08-02 19:36:51 +0500

    Log errors in check model (#6235)

--- common/scripts/check_model_version.js
+++ common/scripts/check_model_version.js
@@ -2,12 +2,14 @@ const exec = require('child_process').exec
 
 exec('git describe --tags `git rev-list --tags --max-count=1`', (err, stdout, stderr) => {
   if (err !== null) {
+    console.log('Error', err)
     process.exit(1)
   }
   const tag = stdout.trim()
   console.log('Check changes for tag:', tag)
   exec(`git fetch --tags && git diff ${tag} --name-only`, (err, stdout, stderr) => {
     if (err !== null) {
+      console.log('Error', err)
       process.exit(1)
     }
     const changedFiles = stdout.trim().split('\n')
