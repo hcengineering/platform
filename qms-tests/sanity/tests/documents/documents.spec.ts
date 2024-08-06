ff8eca9f349efb2d9f97a3ebb9c2b88dc5e7c92f
Jasmin <jasmin@hardcoreeng.com>
2024-08-06 17:38:56 +0200

    test if this test is causing the QMS tests to fail
    Signed-off-by: Jasmin <jasmin@hardcoreeng.com>

--- qms-tests/sanity/tests/documents/documents.spec.ts
+++ qms-tests/sanity/tests/documents/documents.spec.ts
@@ -1249,7 +1249,7 @@ test.describe('QMS. Documents tests', () => {
     })
   })
 
-  test('TESTS-352. Create a document', async ({ page }) => {
+  test.skip('TESTS-352. Create a document', async ({ page }) => {
     const folderName = faker.word.words(1)
     const documentTitle = faker.word.words(2)
     await allure.description('Requirement\nUsers need to create a new document')
