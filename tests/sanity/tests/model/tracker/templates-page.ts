438056a082351dd3e43bc0bee098e62c08c045b6
Alexander Platov <alexander.platov@hardcoreeng.com>
2024-08-13 04:54:04 +0700

    Added Teamspace tests, UI fixes
    Signed-off-by: Alexander Platov <alexander.platov@hardcoreeng.com>

--- tests/sanity/tests/model/tracker/templates-page.ts
+++ tests/sanity/tests/model/tracker/templates-page.ts
@@ -41,13 +41,6 @@ export class TemplatePage extends CommonTrackerPage {
   saveTemplateButton = (): Locator => this.page.locator('text=Save template')
   editTemplateButton = (): Locator => this.page.locator('text=Edit template')
   newSpaceTypeButton = (): Locator => this.page.locator('#new-space-type')
-  spaceTypeButton = (name: string, category?: string): Locator =>
-    this.page.locator('div#navGroup-spaceTypes button.hulyTaskNavLink-container', {
-      hasText: category !== undefined ? `${name} ${category}` : name
-    })
-
-  addTaskTypeButton = (): Locator =>
-    this.page.locator('div.hulyTableAttr-header', { hasText: 'Task types' }).locator('button[data-id="btnAdd"]')
 
   async createNewTemplate (data: NewIssue): Promise<void> {
     await this.buttonNewTemplate().click()
@@ -122,15 +115,6 @@ export class TemplatePage extends CommonTrackerPage {
 
   async selectVacancies (): Promise<void> {
     await this.newSpaceTypeButton().click()
-  }
-
-  async selectSpaceType (name: string, category?: string): Promise<void> {
-    await this.spaceTypeButton(name, category).click()
-  }
-
-  async addTaskType (): Promise<void> {
-    console.log('[!!!] ', await this.page.locator('div.hulyTableAttr-header', { hasText: 'Task types' }).isVisible())
-    await this.addTaskTypeButton().click()
   }
 
   async createTemplate (templateName: string, templateContent: string): Promise<void> {
