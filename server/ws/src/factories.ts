cca77f02008b089426684f7de2bc8c051c0757d4
Andrey Sobolev <haiodo@gmail.com>
2024-09-13 22:30:28 +0700

    Remove UWS
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- server/ws/src/factories.ts
+++ server/ws/src/factories.ts
@@ -4,33 +4,5 @@ import { type ServerFactory } from './types'
  * @public
  */
 export const serverFactories: Record<string, ServerFactory> = {
-  ws: startHttpServer,
-  uweb: (sessions, handleRequest, ctx, pipelineFactory, port, enableCompression, accountsUrl, externalStorage) => {
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const serverHttp = require('./server_u')
-      return serverHttp.startUWebsocketServer(
-        sessions,
-        handleRequest,
-        ctx,
-        pipelineFactory,
-        port,
-        enableCompression,
-        accountsUrl,
-        externalStorage
-      )
-    } catch (err: any) {
-      console.error('uwebsocket.js is not supported, switcg back to nodejs ws', err)
-      return startHttpServer(
-        sessions,
-        handleRequest,
-        ctx,
-        pipelineFactory,
-        port,
-        enableCompression,
-        accountsUrl,
-        externalStorage
-      )
-    }
-  }
+  ws: startHttpServer
 }
