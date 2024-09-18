96e10ae9874cd62f0b5ba7e5bd654cf6faab5778
Alexander Onnikov <Alexander.Onnikov@xored.com>
2024-09-18 12:35:24 +0700

    chore: update tiptap to v2.6.6 (#6596)
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

--- plugins/text-editor-resources/src/components/editor/editorProps.ts
+++ plugins/text-editor-resources/src/components/editor/editorProps.ts
@@ -1,1 +1,5 @@
-export const defaultEditorAttributes: Record<string, string> = { class: 'text-editor-view' }
+export const defaultEditorAttributes: Record<string, string> = {
+  class: 'text-editor-view',
+  // https://github.com/ueberdosis/tiptap/issues/3357
+  tabindex: '0'
+}
