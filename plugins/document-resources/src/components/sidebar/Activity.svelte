ba8d3a77f56b8d8beb9ba8bd88850c6dcc51075a
Alexander Onnikov <Alexander.Onnikov@xored.com>
2024-09-18 16:17:29 +0700

    UBERF-8164 Display activity in wiki document sidebar
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

---
+++ plugins/document-resources/src/components/sidebar/Activity.svelte
@@ -0,0 +1,97 @@
+<!--
+//
+// Copyright © 2024 Hardcore Engineering Inc.
+//
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+-->
+<script lang="ts">
+  import activity, { ActivityMessage } from '@hcengineering/activity'
+  import { ActivityFilter, ActivityMessagePresenter, canGroupMessages } from '@hcengineering/activity-resources'
+  import { SortingOrder } from '@hcengineering/core'
+  import { Document } from '@hcengineering/document'
+  import { createQuery } from '@hcengineering/presentation'
+  import { Label, Lazy, Scroller } from '@hcengineering/ui'
+
+  export let value: Document
+
+  const query = createQuery()
+
+  let messages: ActivityMessage[] = []
+  let filteredMessages: ActivityMessage[] = []
+
+  $: query.query(
+    activity.class.ActivityMessage,
+    { attachedTo: value._id, space: value.space },
+    (res) => {
+      messages = res
+    },
+    {
+      sort: {
+        createdOn: SortingOrder.Ascending
+      }
+    }
+  )
+</script>
+
+{#key value._id}
+  <div class="h-full flex-col clear-mins">
+    <div class="header">
+      <div class="title"><Label label={activity.string.Activity} /></div>
+      <ActivityFilter
+        {messages}
+        object={value}
+        showFilter={false}
+        on:update={(e) => {
+          filteredMessages = e.detail
+        }}
+      />
+    </div>
+
+    <div class="divider" />
+
+    {#if filteredMessages.length > 0}
+      <Scroller padding="0.75rem 0.25rem">
+        {#each filteredMessages as message, index}
+          {@const canGroup = canGroupMessages(message, filteredMessages[index - 1])}
+          <Lazy>
+            <ActivityMessagePresenter
+              value={message}
+              doc={value}
+              hideLink={true}
+              type={canGroup ? 'short' : 'default'}
+            />
+          </Lazy>
+        {/each}
+      </Scroller>
+    {/if}
+  </div>
+{/key}
+
+<style lang="scss">
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 1.25rem;
+    height: 3rem;
+    min-height: 3rem;
+    border-bottom: 1px solid var(--theme-divider-color);
+
+    .title {
+      flex-grow: 1;
+      font-weight: 500;
+      color: var(--caption-color);
+      user-select: none;
+    }
+  }
+</style>
