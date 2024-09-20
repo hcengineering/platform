25ab61f48896e0535fc236fd776618f98657afd8
Alexander Platov <alexander.platov@hardcoreeng.com>
2024-09-20 22:04:06 +0700

    Added Checking styles in a Document (#6631)
    Signed-off-by: Alexander Platov <alexander.platov@hardcoreeng.com>

--- tests/sanity/tests/model/documents/document-content-page.ts
+++ tests/sanity/tests/model/documents/document-content-page.ts
@@ -1,6 +1,7 @@
 import { type Locator, type Page, expect } from '@playwright/test'
 import { CommonPage } from '../common-page'
 import { uploadFile } from '../../utils'
+import path from 'path'
 
 export class DocumentContentPage extends CommonPage {
   readonly page: Page
@@ -35,7 +36,9 @@ export class DocumentContentPage extends CommonPage {
   readonly buttonInsertInnerRow = (row: number = 0): Locator =>
     this.page.locator('table.proseTable').locator('tr').nth(row).locator('div.table-row-insert button')
 
-  readonly buttonToolbarLink = (): Locator => this.page.locator('div.text-editor-toolbar button[data-id="btnLink"]')
+  readonly buttonOnToolbar = (id: string): Locator =>
+    this.page.locator(`div.text-editor-toolbar button[data-id="${id}"]`)
+
   readonly inputFormLink = (): Locator => this.page.locator('form[id="text-editor:string:Link"] input')
   readonly buttonFormLinkSave = (): Locator =>
     this.page.locator('form[id="text-editor:string:Link"] button[type="submit"]')
@@ -61,6 +64,18 @@ export class DocumentContentPage extends CommonPage {
 
   readonly slashActionItemsPopup = (): Locator => this.page.locator('.selectPopup')
 
+  readonly codeBlock = (hasText: string): Locator => this.page.locator('pre.proseCodeBlock > code', { hasText })
+  readonly inputFormNote = (): Locator => this.page.locator('form[id="text-editor:string:ConfigureNote"] textarea')
+  readonly colorFormNote = (color: string): Locator =>
+    this.page.locator(`form[id="text-editor:string:ConfigureNote"] div.colorBox.${color}`)
+
+  readonly setFormNote = (): Locator =>
+    this.page.locator('form[id="text-editor:string:ConfigureNote"] div.antiCard-footer button[type="submit"]')
+
+  readonly inputImageFile = (): Locator => this.page.locator('input[id="imageInput"]')
+  readonly imageInContent = (): Locator => this.page.locator('p img[data-type="image"]')
+  readonly noteInContent = (hasText: string): Locator => this.page.locator('p span[data-mark="note"]', { hasText })
+
   async checkDocumentTitle (title: string): Promise<void> {
     await expect(this.buttonDocumentTitle()).toHaveValue(title)
   }
@@ -69,10 +84,10 @@ export class DocumentContentPage extends CommonPage {
     await expect(this.buttonLockedInTitle()).toBeVisible({ timeout: 1000 })
   }
 
-  async addContentToTheNewLine (newContent: string): Promise<string> {
+  async addContentToTheNewLine (newContent: string, newLine: boolean = true): Promise<string> {
     await expect(this.inputContent()).toBeVisible()
     await expect(this.inputContent()).toHaveJSProperty('contentEditable', 'true')
-    await this.inputContent().pressSequentially(`\n${newContent}`)
+    await this.inputContent().pressSequentially(`${newLine ? '\n' : ''}${newContent}`)
     const endContent = await this.inputContent().textContent()
     return endContent ?? ''
   }
@@ -202,12 +217,68 @@ export class DocumentContentPage extends CommonPage {
 
   async addLinkToText (text: string, link: string): Promise<void> {
     await expect(this.page.locator('p', { hasText: text })).toBeVisible()
-    await this.page.locator('p', { hasText: text }).click()
-    await this.page.locator('p', { hasText: text }).dblclick()
-    await this.buttonToolbarLink().click()
+    await this.page.locator('p', { hasText: text }).click({ clickCount: 3 })
+    await this.buttonOnToolbar('btnLink').click()
 
     await this.inputFormLink().fill(link)
     await this.buttonFormLinkSave().click()
+  }
+
+  async clickButtonOnTooltip (id: string): Promise<void> {
+    await this.buttonOnToolbar(id).click()
+  }
+
+  async selectLine (text: string): Promise<void> {
+    const loc: Locator = this.page.locator('p', { hasText: text }).first()
+    await expect(loc).toBeVisible()
+    await loc.click({ clickCount: 3 })
+  }
+
+  async applyToolbarCommand (text: string, btnId: string): Promise<void> {
+    await this.selectLine(text)
+    await this.clickButtonOnTooltip(btnId)
+  }
+
+  async addSeparator (text: string): Promise<void> {
+    await this.selectLine(text)
+    await this.clickLeftMenu()
+    await this.clickPopupItem('Separator line')
+  }
+
+  async goToByTOC (text: string): Promise<void> {
+    await this.tocItems().first().click()
+    await this.clickPopupItem(text)
+  }
+
+  async clickLeftMenu (): Promise<void> {
+    await this.leftMenu().click()
+  }
+
+  async changeCodeBlockLanguage (text: string, oldLang: string, lang: string): Promise<void> {
+    await this.codeBlock(text).locator('button.antiButton', { hasText: oldLang }).nth(1).click()
+    await this.selectMenuItem(this.page, lang, true)
+  }
+
+  async applyNote (text: string, color: string, note: string): Promise<void> {
+    await this.applyToolbarCommand(text, 'btnNote')
+    await this.inputFormNote().fill(note)
+    await this.colorFormNote(color).click()
+    await this.setFormNote().click()
+  }
+
+  async checkNote (text: string, color: string, note: string): Promise<void> {
+    await expect(this.noteInContent(text)).toBeVisible()
+    await expect(this.noteInContent(text)).toHaveAttribute('data-kind', color)
+    await expect(this.noteInContent(text)).toHaveAttribute('title', note)
+  }
+
+  async addImage (text: string): Promise<void> {
+    await this.selectLine(text)
+    await this.inputImageFile().setInputFiles(path.join(__dirname, '../../files/cat.jpeg'))
+  }
+
+  async checkImage (width: number = 215): Promise<void> {
+    await expect(this.imageInContent()).toHaveAttribute('width', width.toString())
   }
 
   async checkLinkInTheText (text: string, link: string): Promise<void> {
