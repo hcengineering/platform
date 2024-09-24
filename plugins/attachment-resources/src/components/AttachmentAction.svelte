ba5a7525d3ea74c1e81a02648060e5b233a30f21
Alexander Onnikov <Alexander.Onnikov@xored.com>
2024-09-24 13:10:34 +0700

    fix: adjust image size in message attachments (#6698)
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

---
+++ plugins/attachment-resources/src/components/AttachmentAction.svelte
@@ -0,0 +1,45 @@
+<!--
+// Copyright © 2024 Hardcore Engineering Inc.
+//
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+-->
+<script lang="ts">
+  import { type AnySvelteComponent, ButtonIcon, IconSize } from '@hcengineering/ui'
+  import { Asset, IntlString } from '@hcengineering/platform'
+  import { ComponentType } from 'svelte'
+
+  export let label: IntlString
+  export let icon: Asset | AnySvelteComponent | ComponentType
+  export let iconProps: any | undefined = undefined
+  export let size: IconSize = 'small'
+  export let action: (ev: MouseEvent) => Promise<void> | void = async () => {}
+  export let opened: boolean = false
+  export let dataId: string | undefined = undefined
+
+  function onClick (ev: MouseEvent): void {
+    ev.stopPropagation()
+    ev.preventDefault()
+    void action(ev)
+  }
+</script>
+
+<ButtonIcon
+  {icon}
+  {iconProps}
+  iconSize={size}
+  size="small"
+  kind="tertiary"
+  pressed={opened}
+  {dataId}
+  tooltip={{ label }}
+  on:click={onClick}
+/>
--- plugins/attachment-resources/src/components/AttachmentPreview.svelte
+++ plugins/attachment-resources/src/components/AttachmentPreview.svelte
@@ -1,6 +1,6 @@
 <!--
 // Copyright © 2020, 2021 Anticrm Platform Contributors.
-// Copyright © 2021 Hardcore Engineering Inc.
+// Copyright © 2021, 2024 Hardcore Engineering Inc.
 //
 // Licensed under the Eclipse Public License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License. You may
@@ -58,28 +58,28 @@
     }}
   >
     <AttachmentImagePreview {value} size={imageSize} />
-    <div class="actions conner">
+    <div class="actions">
       <AttachmentActions attachment={value} {isSaved} {removable} />
     </div>
   </div>
 {:else if type === 'audio'}
   <div class="buttonContainer">
     <AudioPlayer {value} />
-    <div class="actions conner" style:padding={'0.125rem 0.25rem'}>
+    <div class="actions" style:padding={'0.125rem 0.25rem'}>
       <AttachmentActions attachment={value} {isSaved} {removable} />
     </div>
   </div>
 {:else if type === 'video'}
   <div class="content buttonContainer flex-center">
     <AttachmentVideoPreview {value} preload={videoPreload} />
-    <div class="actions conner">
+    <div class="actions">
       <AttachmentActions attachment={value} {isSaved} {removable} />
     </div>
   </div>
 {:else}
   <div class="flex buttonContainer extraWidth">
     <AttachmentPresenter {value} />
-    <div class="actions conner">
+    <div class="actions">
       <AttachmentActions attachment={value} {isSaved} {removable} />
     </div>
   </div>
@@ -100,17 +100,10 @@
       position: absolute;
       top: 0.25rem;
       right: 0.25rem;
-      padding: 0.25rem;
+      padding: 0.125rem;
       background-color: var(--theme-comp-header-color);
       border: 1px solid var(--theme-divider-color);
-      border-radius: 0.75rem;
-
-      &.conner {
-        top: 0;
-        right: 0;
-        padding: 0.25rem;
-        border-radius: 0 0.4rem 0 0.25rem;
-      }
+      border-radius: 0.375rem;
     }
   }
 
