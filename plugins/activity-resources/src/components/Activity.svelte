0572dbe195028fbecff89adf27578a5af79bfb36
Kristina Fefelova <kristin.fefelova@gmail.com>
2024-09-27 11:32:02 +0400

    Fix tests
    Signed-off-by: Kristina Fefelova <kristin.fefelova@gmail.com>

--- plugins/activity-resources/src/components/Activity.svelte
+++ plugins/activity-resources/src/components/Activity.svelte
@@ -279,6 +279,7 @@
             hideLink={true}
             type={canGroup ? 'short' : 'default'}
             isHighlighted={selectedMessageId === message._id}
+            withShowMore
           />
         {:else}
           <Lazy>
@@ -288,6 +289,7 @@
               hideLink={true}
               type={canGroup ? 'short' : 'default'}
               isHighlighted={selectedMessageId === message._id}
+              withShowMore
             />
           </Lazy>
         {/if}
