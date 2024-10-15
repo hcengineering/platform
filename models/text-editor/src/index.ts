8f57a0df765d99bb5ce14499d6dfa930d981d99c
Alexander Onnikov <Alexander.Onnikov@xored.com>
2024-10-15 14:26:29 +0700

    fix: revert back unused import
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

--- models/text-editor/src/index.ts
+++ models/text-editor/src/index.ts
@@ -31,6 +31,7 @@ import {
   type TextEditorActionKind
 } from '@hcengineering/text-editor'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { EditorKitOptions } from '@hcengineering/text-editor-resources'
 import textEditor from './plugin'
 
 export { textEditorOperation } from './migration'
