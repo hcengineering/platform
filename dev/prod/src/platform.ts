7eec2b55eb5662b88b55b0c300b4a59de0ea3528
Alexey Zinoviev <alexey.zinoviev@xored.com>
2024-10-23 19:19:41 +0400

    Qfix: fix setting ui metadata from config (#7025)
    Signed-off-by: Alexey Zinoviev <alexey.zinoviev@xored.com>

--- dev/prod/src/platform.ts
+++ dev/prod/src/platform.ts
@@ -151,7 +151,7 @@ export interface Config {
   BRANDING_URL?: string
   TELEGRAM_BOT_URL?: string
   AI_URL?:string
-
+  DISABLE_SIGNUP?: string
   // Could be defined for dev environment
   FRONT_URL?: string
   PREVIEW_CONFIG?: string
@@ -292,6 +292,7 @@ export async function configurePlatform() {
   // tryOpenInDesktopApp(config.APP_PROTOCOL ?? 'huly://')
 
   setMetadata(login.metadata.AccountsUrl, config.ACCOUNTS_URL)
+  setMetadata(login.metadata.DisableSignUp, config.DISABLE_SIGNUP === 'true')
   setMetadata(presentation.metadata.FilesURL, config.FILES_URL)
   setMetadata(presentation.metadata.UploadURL, config.UPLOAD_URL)
   setMetadata(presentation.metadata.CollaboratorUrl, config.COLLABORATOR_URL)
