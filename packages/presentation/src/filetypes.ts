2390b0d7612614e075af5aa0ad145bb87a5af666
Alexander Onnikov <aonnikov@hardcoreeng.com>
2024-10-24 17:29:11 +0700

    UBERF-8530 Fix empty image/video upload (#7030)
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

--- packages/presentation/src/filetypes.ts
+++ packages/presentation/src/filetypes.ts
@@ -13,6 +13,7 @@
 // limitations under the License.
 //
 
+import { Analytics } from '@hcengineering/analytics'
 import { type Blob, type Ref } from '@hcengineering/core'
 import { getResource } from '@hcengineering/platform'
 import { type PopupAlignment } from '@hcengineering/ui'
@@ -36,7 +37,13 @@ export async function getFileMetadata (file: FileOrBlob, uuid: Ref<Blob>): Promi
     return undefined
   }
 
-  return await metadataProvider(file, uuid)
+  try {
+    return await metadataProvider(file, uuid)
+  } catch (err) {
+    console.error(err)
+    Analytics.handleError(err as Error)
+    return undefined
+  }
 }
 
 /**
