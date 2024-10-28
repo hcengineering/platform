10133524e918bb4ff5959d3f634c82463ef3d450
Artem Savchenko <armisav@gmail.com>
2024-10-28 15:34:14 +0700

    UBERF-8520: Add test management model
    Signed-off-by: Artem Savchenko <armisav@gmail.com>

--- models/all/src/migration.ts
+++ models/all/src/migration.ts
@@ -52,6 +52,7 @@ import { productsOperation } from '@hcengineering/model-products'
 import { requestOperation } from '@hcengineering/model-request'
 import { analyticsCollectorOperation } from '@hcengineering/model-analytics-collector'
 import { workbenchOperation } from '@hcengineering/model-workbench'
+import { testManagementOperation } from '@hcengineering/model-test-management'
 
 export const migrateOperations: [string, MigrateOperation][] = [
   ['core', coreOperation],
@@ -92,5 +93,6 @@ export const migrateOperations: [string, MigrateOperation][] = [
   // We should call notification migration after activityServer and chunter
   ['notification', notificationOperation],
   ['analyticsCollector', analyticsCollectorOperation],
-  ['workbench', workbenchOperation]
+  ['workbench', workbenchOperation],
+  ['testManagement', testManagementOperation]
 ]
