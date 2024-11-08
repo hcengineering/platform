65e0d099c6eeaefc5115f6db90378afba30d834f
Denis Bykhov <bykhov.denis@gmail.com>
2024-11-08 20:44:42 +0500

    Fix
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- server/postgres/src/utils.ts
+++ server/postgres/src/utils.ts
@@ -307,6 +307,9 @@ export function inferType (val: any): string {
     }
   }
   if (typeof val === 'object') {
+    if (val == null) {
+      return ''
+    }
     if (val instanceof Date) {
       return '::text'
     }
