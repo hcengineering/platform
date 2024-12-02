281aba5cf728fb35f1e1287d4dc56148cc20d6cb
Kristina Fefelova <kristin.fefelova@gmail.com>
2024-12-02 20:26:19 +0400

    Replace personal read with timestamp
    Signed-off-by: Kristina Fefelova <kristin.fefelova@gmail.com>

--- packages/postgres/migrations/04_notification.sql
+++ packages/postgres/migrations/04_notification.sql
@@ -3,8 +3,5 @@ CREATE TABLE IF NOT EXISTS notification
     social_id  VARCHAR(255) NOT NULL,
     message_id INT8         NOT NULL,
 
-    read       BOOLEAN      NOT NULL DEFAULT false,
-    archived   BOOLEAN      NOT NULL DEFAULT false,
-
     PRIMARY KEY (social_id, message_id)
 );
