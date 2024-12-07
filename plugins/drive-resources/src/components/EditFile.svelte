defae4fb913876479df93c74107ea5fd19c43b56
Dakshesh Jain <dakshesh.jain14@gmail.com>
2024-12-07 22:21:22 +0530

    fix: formatting issue
    Signed-off-by: Dakshesh Jain <dakshesh.jain14@gmail.com>

--- plugins/drive-resources/src/components/EditFile.svelte
+++ plugins/drive-resources/src/components/EditFile.svelte
@@ -42,7 +42,13 @@
 </script>
 
 {#if object !== undefined && version !== undefined && blob !== undefined && contentType !== undefined}
-  <FilePreview file={blob} {contentType} name={version.title} metadata={version.metadata} fit={contentType !== 'application/pdf'} />
+  <FilePreview
+    file={blob}
+    {contentType}
+    name={version.title}
+    metadata={version.metadata}
+    fit={contentType !== 'application/pdf'}
+  />
 
   {#if object.versions > 1}
     <div class="w-full mt-6">
