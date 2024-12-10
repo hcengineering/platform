4e1ea29e71a403ad2f2d798d465abae4f9c03021
Alexander Onnikov <Alexander.Onnikov@xored.com>
2024-12-10 23:41:02 +0700

    tool: restore controlled docs content
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

--- dev/tool/src/markup.ts
+++ dev/tool/src/markup.ts
@@ -14,9 +14,21 @@
 //
 
 import { loadCollabYdoc, saveCollabYdoc, yDocCopyXmlField } from '@hcengineering/collaboration'
-import { type MeasureContext, type WorkspaceId, makeCollabYdocId } from '@hcengineering/core'
+import core, {
+  type Blob,
+  type Doc,
+  type MeasureContext,
+  type Ref,
+  type TxCreateDoc,
+  type WorkspaceId,
+  DOMAIN_TX,
+  makeCollabYdocId,
+  makeDocCollabId
+} from '@hcengineering/core'
 import document, { type Document } from '@hcengineering/document'
+import documents from '@hcengineering/controlled-documents'
 import { DOMAIN_DOCUMENT } from '@hcengineering/model-document'
+import { DOMAIN_DOCUMENTS } from '@hcengineering/model-controlled-documents'
 import { type StorageAdapter } from '@hcengineering/server-core'
 import { type Db } from 'mongodb'
 
@@ -43,7 +55,7 @@ export async function restoreWikiContentMongo (
   try {
     while (true) {
       const doc = await iterator.next()
-      if (doc === null) return
+      if (doc === null) break
 
       processedCnt++
       if (processedCnt % 100 === 0) {
@@ -87,4 +99,113 @@ export async function restoreWikiContentMongo (
     printStats()
     await iterator.close()
   }
+}
+
+export interface RestoreControlledDocContentParams {
+  dryRun: boolean
+}
+
+export async function restoreControlledDocContentMongo (
+  ctx: MeasureContext,
+  db: Db,
+  workspaceId: WorkspaceId,
+  storageAdapter: StorageAdapter,
+  params: RestoreWikiContentParams
+): Promise<void> {
+  const iterator = db.collection<Doc>(DOMAIN_DOCUMENTS).find({
+    _class: {
+      $in: [
+        documents.class.ControlledDocument,
+        documents.class.ControlledDocumentSnapshot
+      ]
+    }
+  })
+
+  let processedCnt = 0
+  let restoredCnt = 0
+
+  function printStats (): void {
+    console.log('...processed', processedCnt, 'restored', restoredCnt)
+  }
+
+  try {
+    while (true) {
+      const doc = await iterator.next()
+      if (doc === null) break
+
+      const restored = await restoreControlledDocContentForDoc(ctx, db, workspaceId, storageAdapter, params, doc, 'content')
+      if (restored) {
+        restoredCnt++
+      }
+
+      processedCnt++
+      if (processedCnt % 100 === 0) {
+        printStats()
+      }
+    }
+  } finally {
+    printStats()
+    await iterator.close()
+  }
+}
+
+export async function restoreControlledDocContentForDoc (
+  ctx: MeasureContext,
+  db: Db,
+  workspaceId: WorkspaceId,
+  storageAdapter: StorageAdapter,
+  params: RestoreWikiContentParams,
+  doc: Doc,
+  attribute: string
+): Promise<boolean> {
+  const tx = await db.collection<TxCreateDoc<Doc>>(DOMAIN_TX).findOne({
+    _class: core.class.TxCreateDoc,
+    objectId: doc._id,
+    objectClass: doc._class
+  })
+
+  // It is expected that tx contains attribute with content in old collaborative doc format
+  // the original value here looks like '65b7f82f4d422b89d4cbdd6f:HEAD:0'
+  const attribures = tx?.attributes ?? {}
+  const value = (attribures as any)[attribute] as string
+  if (value == null || !value.includes(':')) {
+    console.log('no content to restore', doc._class, doc._id)
+    return false
+  }
+
+  const currentYdocId = value.split(':')[0] as Ref<Blob>
+  const ydocId = makeCollabYdocId(makeDocCollabId(doc, attribute))
+
+  // Ensure that we don't have new content in storage
+  const stat = await storageAdapter.stat(ctx, workspaceId, ydocId)
+  if (stat !== undefined) {
+    console.log('content already restored', doc._class, doc._id, ydocId)
+    return false
+  }
+
+  console.log('restoring content', doc._id, currentYdocId, '-->', ydocId)
+  if (!params.dryRun) {
+    try {
+      const stat = await storageAdapter.stat(ctx, workspaceId, currentYdocId)
+      if (stat === undefined) {
+        console.log('no content to restore', doc._class, doc._id, ydocId)
+        return false
+      }
+
+      const buffer = await storageAdapter.read(ctx, workspaceId, currentYdocId)
+      await storageAdapter.put(
+        ctx,
+        workspaceId,
+        ydocId,
+        Buffer.concat(buffer as any),
+        'application/ydoc',
+        buffer.length
+      )
+    } catch (err: any) {
+      console.error('failed to restore content for', doc._class, doc._id, err)
+      return false
+    }
+  }
+
+  return true
 }
