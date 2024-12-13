c7e4deec326913898ab96350bdd55a84d331e5e4
Andrey Sobolev <haiodo@gmail.com>
2024-12-13 17:55:02 +0700

    QFix list of workspaces
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- server/account/src/operations.ts
+++ server/account/src/operations.ts
@@ -1665,7 +1665,7 @@ export async function getAllWorkspaces (
   }
 
   return (await db.workspace.find({})).map((it) => {
-    it.accounts = it.accounts.map((it) => it.toString())
+    it.accounts = (it.accounts ?? []).map((it) => it.toString())
     return it
   })
 }
