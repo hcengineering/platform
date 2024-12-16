566fe1173dd7f3db02bc9f107a4a8f57438317ab
Denis Bykhov <bykhov.denis@gmail.com>
2024-12-16 15:36:19 +0500

    Restrict applications to vacany where the user is not a participant
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- plugins/recruit-resources/src/components/CreateApplication.svelte
+++ plugins/recruit-resources/src/components/CreateApplication.svelte
@@ -220,9 +220,10 @@
   const spaceQuery = createQuery()
 
   let vacancy: Vacancy | undefined
+  const me = getCurrentAccount()._id
 
   $: if (_space) {
-    spaceQuery.query(recruit.class.Vacancy, { _id: _space }, (res) => {
+    spaceQuery.query(recruit.class.Vacancy, { _id: _space, members: me }, (res) => {
       vacancy = res.shift()
     })
   }
@@ -329,7 +330,11 @@
     <div class="flex-grow">
       <SpaceSelect
         _class={recruit.class.Vacancy}
-        spaceQuery={{ archived: false, ...($selectedTypeStore !== undefined ? { type: $selectedTypeStore } : {}) }}
+        spaceQuery={{
+          archived: false,
+          members: me,
+          ...($selectedTypeStore !== undefined ? { type: $selectedTypeStore } : {})
+        }}
         spaceOptions={orgOptions}
         readonly={preserveVacancy}
         label={recruit.string.Vacancy}
