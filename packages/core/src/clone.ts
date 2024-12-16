96b512930ef6c69d89364ccc13030ed29b00a127
Denis Bykhov <bykhov.denis@gmail.com>
2024-12-16 16:51:13 +0500

    Fix extra proxys
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- packages/core/src/clone.ts
+++ packages/core/src/clone.ts
@@ -1,3 +1,5 @@
+import { Hierarchy } from './hierarchy'
+
 const se = typeof Symbol !== 'undefined'
 const ste = se && typeof Symbol.toStringTag !== 'undefined'
 
@@ -54,9 +56,8 @@ export function clone (obj: any, as?: (doc: any, m: any) => any, needAs?: (value
       if (type === 'Array') {
         result[key] = clone(value, as, needAs)
       } else if (type === 'Object') {
-        const m = needAs?.(value)
-        const valClone = clone(value, as, needAs)
-        result[key] = m !== undefined && as !== undefined ? as(valClone, m) : valClone
+        const valClone = clone(Hierarchy.toDoc(value), as, needAs)
+        result[key] = valClone
       } else if (type === 'Date') {
         result[key] = new Date(value.getTime())
       } else {
