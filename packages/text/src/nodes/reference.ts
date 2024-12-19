f23047fbeb4a8c4ce31cbf469d011b24cb2776ef
Alexander Onnikov <Alexander.Onnikov@xored.com>
2024-12-19 17:48:43 +0700

    UBERF-8969 Rewrite extractReferences to use JSON only
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

--- packages/text/src/nodes/reference.ts
+++ packages/text/src/nodes/reference.ts
@@ -13,41 +13,8 @@
 // limitations under the License.
 //
 
-import { Class, Doc, Ref } from '@hcengineering/core'
 import { Node, mergeAttributes } from '@tiptap/core'
-import { Node as ProseMirrorNode } from '@tiptap/pm/model'
 import { getDataAttribute } from './utils'
-
-/**
- * @public
- */
-export interface Reference {
-  objectId: Ref<Doc>
-  objectClass: Ref<Class<Doc>>
-  parentNode: ProseMirrorNode | null
-}
-
-/**
- * @public
- */
-export function extractReferences (content: ProseMirrorNode): Array<Reference> {
-  const result: Array<Reference> = []
-
-  content.descendants((node, _pos, parent): boolean => {
-    if (node.type.name === ReferenceNode.name) {
-      const objectId = node.attrs.id as Ref<Doc>
-      const objectClass = node.attrs.objectclass as Ref<Class<Doc>>
-      const e = result.find((e) => e.objectId === objectId && e.objectClass === objectClass)
-      if (e === undefined) {
-        result.push({ objectId, objectClass, parentNode: parent })
-      }
-    }
-
-    return true
-  })
-
-  return result
-}
 
 export interface ReferenceOptions {
   renderLabel: (props: { options: ReferenceOptions, node: any }) => string
