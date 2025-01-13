71d72ba20cc565f3ae3fd2937c2b986fa824a25a
Andrey Sobolev <haiodo@gmail.com>
2025-01-13 16:49:50 +0700

    Fix front base image build
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- dev/base-image/build.sh
+++ dev/base-image/build.sh
@@ -3,4 +3,4 @@
 docker build -t hardcoreeng/base -f base.Dockerfile ${DOCKER_EXTRA} .
 docker build -t hardcoreeng/rekoni-base -f rekoni.Dockerfile ${DOCKER_EXTRA} .
 docker build -t hardcoreeng/print-base -f print.Dockerfile ${DOCKER_EXTRA} .
-docker build -t hardcoreeng/front-base -f print.Dockerfile ${DOCKER_EXTRA} .
+docker build -t hardcoreeng/front-base -f front.Dockerfile ${DOCKER_EXTRA} .
