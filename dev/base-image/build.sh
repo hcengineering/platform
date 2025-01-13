36eca5feefb7a13d86678021d3f671b17b52a716
Andrey Sobolev <haiodo@gmail.com>
2025-01-13 15:52:13 +0700

    Fix docker base build
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- dev/base-image/build.sh
+++ dev/base-image/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build -t hardcoreeng/base -f base.Dockerfile .
-docker build -t hardcoreeng/rekoni-base -f rekoni.Dockerfile .
-docker build -t hardcoreeng/print-base -f print.Dockerfile .
-docker build -t hardcoreeng/front-base -f print.Dockerfile .
+docker build -t hardcoreeng/base -f base.Dockerfile ${DOCKER_EXTRA} .
+docker build -t hardcoreeng/rekoni-base -f rekoni.Dockerfile ${DOCKER_EXTRA} .
+docker build -t hardcoreeng/print-base -f print.Dockerfile ${DOCKER_EXTRA} .
+docker build -t hardcoreeng/front-base -f print.Dockerfile ${DOCKER_EXTRA} .
