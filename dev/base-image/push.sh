5c43e9d6460e774f9874b17208c6d241724a2892
Andrey Sobolev <haiodo@gmail.com>
2025-01-13 16:01:38 +0700

    Fix push
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- dev/base-image/push.sh
+++ dev/base-image/push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker push hardcoreeng/base ${DOCKER_EXTRA}
-docker push hardcoreeng/rekoni-base ${DOCKER_EXTRA}
-docker push hardcoreeng/print-base ${DOCKER_EXTRA}
-docker push hardcoreeng/front-base ${DOCKER_EXTRA}
+docker push hardcoreeng/base
+docker push hardcoreeng/rekoni-base
+docker push hardcoreeng/print-base
+docker push hardcoreeng/front-base
