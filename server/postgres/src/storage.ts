a2eb0de7f5d77b76d634414f649065e19ad8ae55
Andrey Sobolev <haiodo@users.noreply.github.com>
2025-01-17 20:13:04 +0700

    QFIX: PG query and SES (#7700)
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- server/postgres/src/storage.ts
+++ server/postgres/src/storage.ts
@@ -1267,7 +1267,7 @@ abstract class PostgresAdapterBase implements DbAdapter {
             switch (type) {
               case 'common':
                 if (Array.isArray(val) && val.includes(null)) {
-                  res.push(`(${tkey} = ANY(${vars.addArray(val, inferType(val))})) OR ${tkey} IS NULL)`)
+                  res.push(`(${tkey} = ANY(${vars.addArray(val, inferType(val))})) OR ${tkey} IS NULL`)
                 } else {
                   if (val.length > 0) {
                     res.push(`${tkey} = ANY(${vars.addArray(val, inferType(val))})`)
