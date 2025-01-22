c9a2d0c5d0fb1780fc538396d2c764a090179909
Alexander Onnikov <Alexander.Onnikov@xored.com>
2025-01-22 16:46:16 +0700

    UBERF-9226 Workspace reindex tool
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

--- server/indexer/src/indexer/indexer.ts
+++ server/indexer/src/indexer/indexer.ts
@@ -165,6 +165,7 @@ export class FullTextIndexPipeline implements FullTextPipeline {
   triggerIndexing = (): void => {}
 
   async startIndexing (indexing: () => void): Promise<void> {
+    this.cancelling = false
     this.verify = this.verifyWorkspace(this.metrics, indexing)
     void this.verify.then(() => {
       this.indexing = this.doIndexing(indexing)
@@ -280,6 +281,19 @@ export class FullTextIndexPipeline implements FullTextPipeline {
       )
       await this.addMigration(ctx, docStructure)
     }
+  }
+
+  async clearIndex (): Promise<void> {
+    const ctx = this.metrics
+    const migrations = await this.storage.findAll<MigrationState>(ctx, core.class.MigrationState, {
+      plugin: coreId,
+      state: {
+        $in: ['verify-indexes-v2', 'full-text-indexer-v4', 'full-text-structure-v4']
+      }
+    })
+
+    const refs = migrations.map((it) => it._id)
+    await this.storage.clean(ctx, DOMAIN_MIGRATION, refs)
   }
 
   broadcastClasses = new Set<Ref<Class<Doc>>>()
