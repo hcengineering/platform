2e374ad460a102244432b5bbdd68f7f2d1efa741
Andrey Sobolev <haiodo@gmail.com>
2025-01-23 16:07:16 +0700

    Fix publish script should fail on error
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- desktop-package/scripts/publish-version.sh
+++ desktop-package/scripts/publish-version.sh
@@ -5,8 +5,23 @@ rawVersion=$(node common/scripts/show_tag.js)
 version=${rawVersion:1:${#rawVersion}-2}
 
 cd desktop-package/deploy
-aws s3api put-object --endpoint $endpoint --bucket $bucket --key ${version}-mac.yml --body latest-mac.yml --acl public-read 2>&1 > /dev/null
-aws s3api put-object --endpoint $endpoint --bucket $bucket --key ${version}-linux.yml --body latest-linux.yml --acl public-read 2>&1 > /dev/null
-aws s3api put-object --endpoint $endpoint --bucket $bucket --key ${version}.yml --body latest.yml --acl public-read 2>&1 > /dev/null
 
-echo Successully published version ${version} to ${endpoint}/${bucket}
+aws s3api put-object --endpoint $endpoint --bucket $bucket --key ${version}-mac.yml --body latest-mac.yml --acl public-read 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Error uploading ${version}-mac.yml" >&2
+  exit 1
+fi
+
+aws s3api put-object --endpoint $endpoint --bucket $bucket --key ${version}-linux.yml --body latest-linux.yml --acl public-read 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Error uploading ${version}-linux.yml" >&2
+  exit 1
+fi
+
+aws s3api put-object --endpoint $endpoint --bucket $bucket --key ${version}.yml --body latest.yml --acl public-read 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Error uploading ${version}.yml" >&2
+  exit 1
+fi
+
+echo Successfully published version ${version} to ${endpoint}/${bucket}
