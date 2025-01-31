a234217219035a038ba65b5593669507fa4e728e
Denis Bykhov <bykhov.denis@gmail.com>
2025-01-31 18:02:53 +0500

    Minor cards fixes
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- plugins/card/src/analytics.ts
+++ plugins/card/src/analytics.ts
@@ -1,5 +1,6 @@
 export enum CardEvents {
   NewCardButtonClicked = 'card.card.NewButtonClicked',
+  MasterTagRemoved = 'card.card.MasterTagRemoved',
   CardCreated = 'card.card.Created',
   CardOpened = 'card.card.Opened'
 }
