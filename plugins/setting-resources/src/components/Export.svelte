3c99de29e33af9e1ee5281e1d057973ea18f2677
Anna Khismatullina <anna.khismatullina@gmail.com>
2025-02-11 18:23:09 +0700

    Fix UI
    Signed-off-by: Anna Khismatullina <anna.khismatullina@gmail.com>

--- plugins/setting-resources/src/components/Export.svelte
+++ plugins/setting-resources/src/components/Export.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import core, { Class, Doc, Ref } from '@hcengineering/core'
-  import { Button, DropdownLabels } from '@hcengineering/ui'
+  import { Class, Doc, Ref } from '@hcengineering/core'
+  import { Button, DropdownLabelsIntl, Header, Breadcrumb, Scroller } from '@hcengineering/ui'
   import setting from '../plugin'
 
   const classItems = [
@@ -17,27 +17,54 @@
   let selectedType: string = 'JSON'
 
   async function exportData (): Promise<void> {
-    // Здесь будет логика экспорта
+    // Export logic will be here
     console.log('Exporting', selectedClass, selectedType)
   }
 </script>
 
-<div class="flex-col flex-gap-4">
-  <div class="flex-row flex-gap-4">
-    <DropdownLabels
-      label={core.string.Class}
-      items={classItems}
-      bind:selected={selectedClass}
-    />
-    <DropdownLabels
-      label={setting.string.Type}
-      items={typeItems}
-      bind:selected={selectedType}
-    />
+<div class="hulyComponent">
+  <Header adaptive={'disabled'}>
+    <Breadcrumb icon={setting.icon.Export} label={setting.string.Export} size={'large'} isCurrent />
+  </Header>
+  <div class="hulyComponent-content__column content">
+    <Scroller align={'center'} padding={'var(--spacing-3)'} bottomPadding={'var(--spacing-3)'}>
+      <div class="hulyComponent-content">
+        <div class="flex-row-center p-2 flex-no-shrink">
+          <div class="p-1 min-w-80">
+            <!-- label={setting.string.DataToExport} -->
+            <div class="antiGrid-row">
+              <div class="antiGrid-row__header">
+                {'Class to export'}
+              </div>
+              <div class="antiGrid-row__content">
+                <DropdownLabelsIntl
+                  items={classItems}
+                  bind:selected={selectedClass}
+                />
+              </div>
+            </div>
+            <div class="antiGrid-row">
+              <div class="antiGrid-row__header">
+                {'File type'}
+              </div>
+              <div class="antiGrid-row__content">
+                <DropdownLabelsIntl
+                  items={typeItems}
+                  bind:selected={selectedType}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex-row-center p-2">
+          <Button
+            label={setting.string.Export}
+            kind={'primary'}
+            size={'medium'}
+            on:click={exportData}
+          />
+        </div>
+      </div>
+    </Scroller>
   </div>
-  <Button
-    label={setting.string.Export}
-    primary
-    on:click={exportData}
-  />
 </div>
