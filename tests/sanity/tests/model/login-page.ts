36e7c8ebbd2f8e198055bea20e79f4a6721e2c51
Andrey Sobolev <haiodo@gmail.com>
2025-02-27 23:05:46 +0700

    Fix ui tests
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- tests/sanity/tests/model/login-page.ts
+++ tests/sanity/tests/model/login-page.ts
@@ -12,6 +12,7 @@ export class LoginPage {
   inputPassword = (): Locator => this.page.locator('input[name=current-password]')
   buttonLogin = (): Locator => this.page.locator('button', { hasText: 'Log In' })
   loginWithPassword = (): Locator => this.page.locator('a', { hasText: 'Login with password' })
+  signUpWithPassword = (): Locator => this.page.locator('a', { hasText: 'Sign up with password' })
   linkSignUp = (): Locator => this.page.locator('a.title', { hasText: 'Sign Up' })
   invalidCredentialsMessage = (): Locator =>
     this.page.getByText('Account not found or the provided credentials are incorrect')
@@ -38,6 +39,7 @@ export class LoginPage {
 
   async clickSignUp (): Promise<void> {
     await this.linkSignUp().click()
+    await this.signUpWithPassword().click()
   }
 
   async clickOnRecover (): Promise<void> {
