eb4e02420a41e587e72f97a41856314df3837e8b
erkanyldz58 <erkanyldz008@gmail.com>
2025-03-03 11:09:07 +0300

    fix

--- scripts/create-workspace.sh
+++ scripts/create-workspace.sh
@@ -1,6 +1,6 @@
 cd ./dev/tool
-rushx run-local create-workspace ws1 -w DevWorkspace # Create workspace
-rushx run-local create-account user1 -p 1234 -f John -l Appleseed # Create account
-rushx run-local configure ws1 --list --enable '*' # Enable all modules, even if they are not yet intended to be used by a wide audience.
-rushx run-local assign-workspace user1 ws1 # Assign workspace to user.
-rushx run-local confirm-email user1 # To allow the creation of additional test workspaces.
+rushx run-local create-workspace kubik -w DevWorkspace # Create workspace
+rushx run-local create-account erkanyldz008@gmail.com -p 1234 -f Erkan -l Yıldız # Create account
+rushx run-local configure kubik --list --enable '*' # Enable all modules, even if they are not yet intended to be used by a wide audience.
+rushx run-local assign-workspace erkanyldz009@gmail.com kubik # Assign workspace to user.
+rushx run-local confirm-email erkanyldz009@gmail.com # To allow the creation of additional test workspaces.
