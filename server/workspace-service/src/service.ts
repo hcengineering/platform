4150bcdec6fe24f0f8e9dbe91dd14efbbbadb251
Andrey Sobolev <haiodo@gmail.com>
2025-03-05 11:28:28 +0700

    UBERF-9550: Fix backup verification memory usage
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- server/workspace-service/src/service.ts
+++ server/workspace-service/src/service.ts
@@ -583,7 +583,7 @@ export class WorkspaceWorker {
         50000,
         ['blob'],
         sharedPipelineContextVars,
-        true,
+        true, // Do a full check
         (_p: number) => {
           if (progress !== Math.round(_p)) {
             progress = Math.round(_p)
