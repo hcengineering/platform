ccef31b16e3251b11bd721f047f5ed5551ea0117
Andrey Sobolev <haiodo@users.noreply.github.com>
2025-03-07 18:53:44 +0700

    UBERF-9560: Filter query fixes (#8161)
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- server/postgres/src/__tests__/conversion.spec.ts
+++ server/postgres/src/__tests__/conversion.spec.ts
@@ -1,4 +1,4 @@
-import { convertArrayParams, decodeArray } from '../utils'
+import { convertArrayParams, decodeArray, filterProjection } from '../utils'
 
 describe('array conversion', () => {
   it('should handle undefined parameters', () => {
@@ -53,5 +53,49 @@ describe('array decoding', () => {
 
   it('should decode array with multiple escaped characters', () => {
     expect(decodeArray('{"first \\"quote\\"","second \\"quote\\""}')).toEqual(['first "quote"', 'second "quote"'])
+  })
+})
+
+describe('projection', () => {
+  it('mixin query projection', () => {
+    const data = {
+      '638611f18894c91979399ef3': {
+        Источник_6386125d8894c91979399eff: 'Workable'
+      },
+      attachments: 1,
+      avatar: null,
+      avatarProps: null,
+      avatarType: 'color',
+      channels: 3,
+      city: 'Poland',
+      docUpdateMessages: 31,
+      name: 'Mulkuha,Muklyi',
+      'notification:mixin:Collaborators': {
+        collaborators: []
+      },
+      'recruit:mixin:Candidate': {
+        Title_63f38419efefd99805238bbd: 'Backend-RoR',
+        Trash_64493626f9b50e77bf82d231: 'Нет',
+        __mixin: 'true',
+        applications: 1,
+        onsite: null,
+        remote: null,
+        skills: 18,
+        title: '',
+        Опытработы_63860d5c8894c91979399e73: '2018',
+        Уровеньанглийского_63860d038894c91979399e6f: 'UPPER'
+      }
+    }
+    const projected = filterProjection<any>(data, {
+      'recruit:mixin:Candidate.Уровеньанглийского_63860d038894c91979399e6f': 1,
+      _class: 1,
+      space: 1,
+      modifiedOn: 1
+    })
+    expect(projected).toEqual({
+      'recruit:mixin:Candidate': {
+        Уровеньанглийского_63860d038894c91979399e6f: 'UPPER'
+      }
+    })
   })
 })
