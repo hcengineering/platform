24df32949960f8215d7a584698f4a42074093e65
Nikolay Chunosov <Chunosov.N@gmail.com>
2025-04-03 03:08:06 +0700

    handle attachments in inbound mail
    Signed-off-by: Nikolay Chunosov <Chunosov.N@gmail.com>

--- services/mail/pod-inbound-mail/src/message.ts
+++ services/mail/pod-inbound-mail/src/message.ts
@@ -13,7 +13,19 @@
 // limitations under the License.
 //
 import { encode } from 'jwt-simple'
+import { getClient as getAccountClient } from '@hcengineering/account-client'
+import { createRestTxOperations } from '@hcengineering/api-client'
+import { type Card } from '@hcengineering/card'
 import {
+  type RestClient as CommunicationClient,
+  createRestClient as getCommunicationClient
+} from '@hcengineering/communication-rest-client'
+import { type CreateFileEvent, type CreateMessageEvent, RequestEventType } from '@hcengineering/communication-sdk-types'
+import { MessageType } from '@hcengineering/communication-types'
+import contact, { PersonSpace } from '@hcengineering/contact'
+import {
+  type Blob,
+  type MeasureContext,
   type PersonId,
   type Ref,
   type TxOperations,
@@ -22,19 +34,17 @@ import {
   RateLimiter,
   systemAccountUuid
 } from '@hcengineering/core'
-import { getClient as getAccountClient } from '@hcengineering/account-client'
-import {
-  type RestClient as CommunicationClient,
-  createRestClient as getCommunicationClient
-} from '@hcengineering/communication-rest-client'
-import { type CreateMessageEvent, RequestEventType } from '@hcengineering/communication-sdk-types'
-import { MessageType } from '@hcengineering/communication-types'
-import { createRestTxOperations } from '@hcengineering/api-client'
-import { type Card } from '@hcengineering/card'
-import contact, { PersonSpace } from '@hcengineering/contact'
 import mail from '@hcengineering/mail'
+import { buildStorageFromConfig, storageConfigFromEnv } from '@hcengineering/server-storage'
 import config from './config'
 import { ensureGlobalPerson, ensureLocalPerson } from './person'
+
+export interface Attachment {
+  id: string
+  name: string
+  data: Buffer
+  contentType: string
+}
 
 function generateToken (): string {
   return encode(
@@ -47,14 +57,16 @@ function generateToken (): string {
 }
 
 export async function createMessages (
+  ctx: MeasureContext,
   mailId: string,
   from: { address: string, name: string },
   tos: { address: string, name: string }[],
   subject: string,
   content: string,
+  attachments: Attachment[],
   inReplyTo?: string
 ): Promise<void> {
-  console.log(`[${mailId}] Sending message ${from.address} --> ${tos.map((to) => to.address).join(',')}`)
+  ctx.info('Sending message', { mailId, from: from.address, to: tos.map((to) => to.address).join(',') })
 
   const accountClient = getAccountClient(config.accountsUrl, generateToken())
   const wsInfo = await accountClient.selectWorkspace(config.workspaceUrl)
@@ -62,13 +74,14 @@ export async function createMessages (
   const txClient = await createRestTxOperations(transactorUrl, wsInfo.workspace, wsInfo.token)
   const msgClient = getCommunicationClient(wsInfo.endpoint, wsInfo.workspace, wsInfo.token)
 
-  const fromPerson = await ensureGlobalPerson(accountClient, mailId, from)
+  const fromPerson = await ensureGlobalPerson(ctx, accountClient, mailId, from)
   if (fromPerson === undefined) {
-    console.error(`[${mailId}] Unable to create message without a proper FROM`)
+    ctx.error('Unable to create message without a proper FROM', { mailId })
     return
   }
   try {
     await ensureLocalPerson(
+      ctx,
       txClient,
       mailId,
       fromPerson.uuid,
@@ -77,20 +90,21 @@ export async function createMessages (
       fromPerson.firstName,
       fromPerson.lastName
     )
-  } catch (err) {
-    console.error(`[${mailId}] Failed to ensure local FROM person`, err)
-    console.error(`[${mailId}] Unable to create message without a proper FROM`)
+  } catch (error) {
+    ctx.error('Failed to ensure local FROM person', { error, mailId })
+    ctx.error('Unable to create message without a proper FROM', { mailId })
     return
   }
 
   const toPersons: { address: string, uuid: PersonUuid, socialId: PersonId }[] = []
   for (const to of tos) {
-    const toPerson = await ensureGlobalPerson(accountClient, mailId, to)
+    const toPerson = await ensureGlobalPerson(ctx, accountClient, mailId, to)
     if (toPerson === undefined) {
       continue
     }
     try {
       await ensureLocalPerson(
+        ctx,
         txClient,
         mailId,
         toPerson.uuid,
@@ -99,24 +113,54 @@ export async function createMessages (
         toPerson.firstName,
         toPerson.lastName
       )
-    } catch (err) {
-      console.error(`[${mailId}] Failed to ensure local TO person, skip`, err)
+    } catch (error) {
+      ctx.error('Failed to ensure local TO person, skip', { error, mailId })
       continue
     }
     toPersons.push({ address: to.address, ...toPerson })
   }
   if (toPersons.length === 0) {
-    console.error(`[${mailId}] Unable to create message without a proper TO`)
+    ctx.error('Unable to create message without a proper TO', { mailId })
     return
   }
 
   const modifiedBy = fromPerson.socialId
   const participants = [fromPerson.socialId, ...toPersons.map((p) => p.socialId)]
 
+  const attachedBlobs: Attachment[] = []
+  if (config.storageConfig !== undefined) {
+    const storageConfig = storageConfigFromEnv()
+    const storageAdapter = buildStorageFromConfig(storageConfig)
+    try {
+      for (const a of attachments ?? []) {
+        try {
+          await storageAdapter.put(
+            ctx,
+            {
+              uuid: wsInfo.workspace,
+              url: wsInfo.workspaceUrl,
+              dataId: wsInfo.workspaceDataId
+            },
+            a.id,
+            a.data,
+            a.contentType
+          )
+          attachedBlobs.push(a)
+          ctx.info('Uploaded attachment', { mailId, blobId: a.id, name: a.name, contentType: a.contentType })
+        } catch (error) {
+          ctx.error('Failed to upload attachment', { name: a.name, error, mailId })
+        }
+      }
+    } finally {
+      await storageAdapter.close()
+    }
+  }
+
   try {
-    const spaces = await getPersonSpaces(txClient, mailId, fromPerson.uuid, from.address)
+    const spaces = await getPersonSpaces(ctx, txClient, mailId, fromPerson.uuid, from.address)
     if (spaces.length > 0) {
       await saveMessageToSpaces(
+        ctx,
         txClient,
         msgClient,
         mailId,
@@ -125,18 +169,25 @@ export async function createMessages (
         modifiedBy,
         subject,
         content,
+        attachedBlobs,
         inReplyTo
       )
     }
-  } catch (err) {
-    console.error(`[${mailId}] Failed to save message to personal spaces of ${fromPerson.uuid} (${from.address})`, err)
+  } catch (error) {
+    ctx.error('Failed to save message to personal spaces', {
+      error,
+      mailId,
+      personUuid: fromPerson.uuid,
+      email: from.address
+    })
   }
 
   for (const to of toPersons) {
     try {
-      const spaces = await getPersonSpaces(txClient, mailId, to.uuid, to.address)
+      const spaces = await getPersonSpaces(ctx, txClient, mailId, to.uuid, to.address)
       if (spaces.length > 0) {
         await saveMessageToSpaces(
+          ctx,
           txClient,
           msgClient,
           mailId,
@@ -145,16 +196,18 @@ export async function createMessages (
           modifiedBy,
           subject,
           content,
+          attachedBlobs,
           inReplyTo
         )
       }
-    } catch (err) {
-      console.error(`[${mailId}] Failed to save message spaces of ${to.socialId} (${to.address})`, err)
+    } catch (error) {
+      ctx.error('Failed to save message spaces', { error, mailId, personUuid: to.uuid, email: to.address })
     }
   }
 }
 
 async function getPersonSpaces (
+  ctx: MeasureContext,
   client: TxOperations,
   mailId: string,
   personUuid: PersonUuid,
@@ -164,12 +217,13 @@ async function getPersonSpaces (
   const personRefs = persons.map((p) => p._id)
   const spaces = await client.findAll(contact.class.PersonSpace, { person: { $in: personRefs } })
   if (spaces.length === 0) {
-    console.log(`[${mailId}] No personal space found for ${personUuid} (${email}), skip`)
+    ctx.info('No personal space found, skip', { mailId, personUuid, email })
   }
   return spaces
 }
 
 async function saveMessageToSpaces (
+  ctx: MeasureContext,
   client: TxOperations,
   msgClient: CommunicationClient,
   mailId: string,
@@ -178,16 +232,17 @@ async function saveMessageToSpaces (
   modifiedBy: PersonId,
   subject: string,
   content: string,
+  attachments: Attachment[],
   inReplyTo?: string
 ): Promise<void> {
   const rateLimiter = new RateLimiter(10)
   for (const space of spaces) {
     await rateLimiter.add(async () => {
-      console.log(`[${mailId}] Saving message to space ${space._id}`)
+      ctx.info('Saving message to space', { mailId, space: space._id })
 
       const route = await client.findOne(mail.class.MailRoute, { mailId })
       if (route !== undefined) {
-        console.log(`[${mailId}] Message is already in the thread ${route.threadId}, skip`)
+        ctx.info('Message is already in the thread, skip', { mailId, threadId: route.threadId })
         return
       }
 
@@ -196,7 +251,7 @@ async function saveMessageToSpaces (
         const route = await client.findOne(mail.class.MailRoute, { mailId: inReplyTo })
         if (route !== undefined) {
           threadId = route.threadId as Ref<Card>
-          console.log(`[${mailId}] Found existing thread ${threadId}`)
+          ctx.info('Found existing thread', { mailId, threadId })
         }
       }
       if (threadId === undefined) {
@@ -217,20 +272,32 @@ async function saveMessageToSpaces (
           modifiedBy
         )
         threadId = newThreadId as Ref<Card>
-        console.log(`[${mailId}] Created new thread ${threadId}`)
+        ctx.info('Created new thread', { mailId, threadId })
       }
 
-      const event: CreateMessageEvent = {
+      const msgEvent: CreateMessageEvent = {
         type: RequestEventType.CreateMessage,
         messageType: MessageType.Message,
         card: threadId,
         content,
         creator: modifiedBy
       }
+      const { id: messageId } = (await msgClient.event(msgEvent)) as any
+      ctx.info('Created message', { mailId, messageId })
 
-      await msgClient.event(event)
-
-      // TODO: process attachments
+      for (const a of attachments) {
+        const fileEvent: CreateFileEvent = {
+          type: RequestEventType.CreateFile,
+          card: threadId,
+          message: messageId,
+          blobId: a.id as Ref<Blob>,
+          fileType: a.contentType,
+          filename: a.name,
+          size: a.data.length,
+          creator: modifiedBy
+        }
+        await msgClient.event(fileEvent)
+      }
 
       await client.createDoc(
         mail.class.MailRoute,
