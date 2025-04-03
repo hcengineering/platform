296d7320ea531b6530c233a0dc84d436090b619d
Mikhail_Svetlov <jegnum@gmail.com>
2025-04-03 22:55:11 +0400

    add .md to ignore

--- packages/theme/.eslintrc.js
+++ packages/theme/.eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
   extends: ['./node_modules/@hcengineering/platform-rig/profiles/ui/eslint.config.json'],
-  parserOptions: { tsconfigRootDir: __dirname }
+  parserOptions: { tsconfigRootDir: __dirname },
+  ignorePatterns: ["**/*.md"]
 }
