b3bc947ecef81f743a548f283f528317b9d6b4bb
Alexey Zinoviev <alexey.zinoviev@xored.com>
2025-04-03 16:41:57 +0400

    Qfix idp signup Signed-off-by: Alexey Zinoviev <alexey.zinoviev@xored.com>

--- pods/authProviders/src/utils.ts
+++ pods/authProviders/src/utils.ts
@@ -79,7 +79,7 @@ export async function handleProviderAuth (
     const state = safeParseAuthState(rawState)
     const branding = getBranding(brandings, state?.branding)
 
-    if (state.inviteId != null && state.inviteId !== '' && state.autoJoin == null) {
+    if (state.inviteId != null && state.inviteId !== '' && state.autoJoin !== true) {
       loginInfo = await joinWithProvider(
         measureCtx,
         db,
@@ -100,7 +100,7 @@ export async function handleProviderAuth (
         first,
         last,
         socialKey,
-        signUpDisabled === true || state.autoJoin != null
+        signUpDisabled === true || state.autoJoin === true
       )
     }
 
