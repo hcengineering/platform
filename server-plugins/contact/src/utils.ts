6544946cdcaf935cea189df0a9c49524573ec22f
Kristina Fefelova <kristin.fefelova@gmail.com>
2025-04-07 16:46:36 +0400

    Add user to card collaborators when card created in personal space
    Signed-off-by: Kristina Fefelova <kristin.fefelova@gmail.com>

--- server-plugins/contact/src/utils.ts
+++ server-plugins/contact/src/utils.ts
@@ -14,7 +14,13 @@
 //
 
 import { TriggerControl } from '@hcengineering/server-core'
-import contact, { Employee, type Person, pickPrimarySocialId, SocialIdentityRef } from '@hcengineering/contact'
+import contact, {
+  Employee,
+  type Person,
+  PersonSpace,
+  pickPrimarySocialId,
+  SocialIdentityRef
+} from '@hcengineering/contact'
 import { AccountUuid, parseSocialIdString, PersonId, type Ref, toIdMap } from '@hcengineering/core'
 
 export async function getCurrentPerson (control: TriggerControl): Promise<Person | undefined> {
@@ -258,4 +264,8 @@ export async function getAccountBySocialKey (control: TriggerControl, socialKey:
   )
 
   return employee[0]?.personUuid ?? null
+}
+
+export async function getPersonSpaces (control: TriggerControl): Promise<Pick<PersonSpace, '_id' | 'person'>[]> {
+  return await control.queryFind(control.ctx, contact.class.PersonSpace, {}, { projection: { _id: 1, person: 1 } })
 }
