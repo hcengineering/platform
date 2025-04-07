5571dc0ee18182e88913726f098629fe9d185b20
Chunosov <Chunosov.N@gmail.com>
2025-04-07 23:44:04 +0700

    remove my-space application (#8488)
    Signed-off-by: Nikolay Chunosov <Chunosov.N@gmail.com>

--- models/all/src/index.ts
+++ models/all/src/index.ts
@@ -104,7 +104,6 @@ import testManagement, {
   testManagementId,
   createModel as testManagementModel
 } from '@hcengineering/model-test-management'
-import mySpace, { mySpaceId, createModel as mySpaceModel } from '@hcengineering/model-my-space'
 import { mailId, createModel as mailModel } from '@hcengineering/model-mail'
 
 import {
@@ -450,17 +449,6 @@ export default function buildModel (enabled: string[] = ['*'], disabled: string[
     ],
     [surveyModel, surveyId],
     [presenceModel, presenceId],
-    [
-      mySpaceModel,
-      mySpaceId,
-      {
-        label: mySpace.string.ConfigLabel,
-        description: mySpace.string.ConfigDescription,
-        enabled: false,
-        beta: true,
-        classFilter: defaultFilter
-      }
-    ],
     [mailModel, mailId],
     [chatModel, chatId],
     [inboxModel, inboxId],
