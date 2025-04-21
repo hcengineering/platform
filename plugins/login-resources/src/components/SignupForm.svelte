91c30f2b8752f9b81c93dbdeabc2447dfae65e28
Alexey Zinoviev <alexey.zinoviev@xored.com>
2025-04-21 23:45:38 +0400

    uberf-10303: only otp sign up Signed-off-by: Alexey Zinoviev <alexey.zinoviev@xored.com>

--- plugins/login-resources/src/components/SignupForm.svelte
+++ plugins/login-resources/src/components/SignupForm.svelte
@@ -15,23 +15,22 @@
 -->
 <script lang="ts">
   import { OK, Severity, Status } from '@hcengineering/platform'
-  import presentation from '@hcengineering/presentation'
-  import { logIn } from '@hcengineering/workbench'
+
   import BottomActionComponent from './BottomAction.svelte'
   import login from '../plugin'
   import { getPasswordValidationRules } from '../validations'
-  import { goTo, signUp } from '../utils'
+  import { goTo } from '../utils'
   import Form from './Form.svelte'
-  import { BottomAction, LoginMethods, OtpLoginSteps, signUpOtp } from '../index'
+  import { OtpLoginSteps, signUpOtp } from '../index'
   import type { Field } from '../types'
   import OtpForm from './OtpForm.svelte'
 
   export let signUpDisabled = false
   export let navigateUrl: string | undefined = undefined
 
-  let method: LoginMethods = LoginMethods.Otp
   let fields: Array<Field>
   let form: Form
+  let withPassword = false
 
   $: {
     fields = [
@@ -40,7 +39,7 @@
       { id: 'email', name: 'username', i18n: login.string.Email }
     ]
 
-    if (method === LoginMethods.Password) {
+    if (withPassword) {
       fields.push({
         id: 'new-password',
         name: 'password',
@@ -73,35 +72,23 @@
     func: async () => {
       status = new Status(Severity.INFO, login.status.ConnectingToServer, {})
 
-      if (method === LoginMethods.Password) {
-        const [loginStatus, result] = await signUp(object.username, object.password, object.first, object.last)
+      const [otpStatus, result] = await signUpOtp(object.username, object.first, object.last)
+      status = otpStatus
 
-        status = loginStatus
-
-        if (result != null) {
-          await logIn(result)
-          goTo('confirmationSend')
-        }
-      } else {
-        const [otpStatus, result] = await signUpOtp(object.username, object.first, object.last)
-        status = otpStatus
-
-        if (result?.sent === true && otpStatus === OK) {
-          step = OtpLoginSteps.Otp
-          otpRetryOn = result.retryOn
-        }
+      if (result?.sent === true && otpStatus === OK) {
+        step = OtpLoginSteps.Otp
+        otpRetryOn = result.retryOn
       }
     }
   }
 
-  let changeMethodAction: BottomAction
-  $: changeMethodAction = {
-    i18n: method === LoginMethods.Password ? login.string.SignUpWithCode : login.string.SignUpWithPassword,
+  let withPasswordAction: BottomAction
+  $: withPasswordAction = {
+    i18n: withPassword ? login.string.SetPasswordLater : login.string.SetPasswordNow,
     func: () => {
-      method = method === LoginMethods.Password ? LoginMethods.Otp : LoginMethods.Password
-      if (method === LoginMethods.Password) {
-        step = OtpLoginSteps.Email
-      }
+      withPassword = !withPassword
+      step = OtpLoginSteps.Email
+
       setTimeout(() => {
         if (form != null) {
           form.invalidate()
@@ -125,13 +112,14 @@
     {signUpDisabled}
     {navigateUrl}
     loginState="signup"
+    password={object.password}
     retryOn={otpRetryOn}
     on:step={handleStep}
   />
 {/if}
 
 <div class="action">
-  <BottomActionComponent action={changeMethodAction} />
+  <BottomActionComponent action={withPasswordAction} />
 </div>
 
 <style lang="scss">
