56add32de3efc8b1ac487322127911d991707f0a
Anton Alexeyev <alexeyev.anton@gmail.com>
2025-04-25 16:27:34 +0700

    Fix the description of a user creation in dev mode in README (#8492)
    Signed-off-by: Anton Alexeyev <alexeyev.anton@gmail.com>
    Signed-off-by: utkaka <alexeyev.anton@gmail.com>

--- scripts/create-workspace.sh
+++
@@ -1,5 +0,0 @@
-cd ./dev/tool
-rushx run-local create-account user1 -p 1234 -f John -l Appleseed # Create account
-rushx run-local create-workspace ws1 email:user1
-rushx run-local configure ws1 --list --enable '*' # Enable all modules, even if they are not yet intended to be used by a wide audience.
-rushx run-local assign-workspace user1 ws1 # Assign workspace to user.
