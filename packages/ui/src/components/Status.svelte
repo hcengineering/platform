5c1fa3128ea24ffc76c64173a7faa232125e96c2
Anton Alexeyev <alexeyev.anton@gmail.com>
2025-05-21 10:56:58 +0700

    Add a read only warning to the status
    Signed-off-by: Anton Alexeyev <alexeyev.anton@gmail.com>

--- packages/ui/src/components/Status.svelte
+++ packages/ui/src/components/Status.svelte
@@ -4,19 +4,33 @@
 
   import Info from './icons/Info.svelte'
   import Label from './Label.svelte'
+  import { AccountRole, getCurrentAccount } from '@hcengineering/core'
+  import ui from '../plugin'
 
   export let status: Status
   export let overflow: boolean = true
+
+  const account = getCurrentAccount()
+  $: isReadOnly = account?.role === AccountRole.ReadOnlyGuest
 </script>
 
-<div class="flex-center container {status.severity}" class:overflow-label={overflow}>
-  {#if status.severity !== Severity.OK}
+{#if isReadOnly}
+  <div class="flex-center container {Severity.INFO}" class:overflow-label={overflow}>
     <Info size={'small'} />
     <span class="text-sm ml-2" class:overflow-label={overflow}>
+        <Label label={ui.string.ReadOnlyModeWarning}/>
+      </span>
+  </div>
+{:else}
+  <div class="flex-center container {status.severity}" class:overflow-label={overflow}>
+    {#if status.severity !== Severity.OK}
+      <Info size={'small'} />
+      <span class="text-sm ml-2" class:overflow-label={overflow}>
       <Label label={status.code} params={status.params} />
     </span>
-  {/if}
-</div>
+    {/if}
+  </div>
+{/if}
 
 <style lang="scss">
   .container {
