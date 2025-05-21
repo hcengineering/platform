abdb6126c5bf17e04d760ef3c60b254096f8bab6
Kristina Fefelova <kristin.fefelova@gmail.com>
2025-05-21 20:41:46 +0400

    set min messages count per file
    Signed-off-by: Kristina Fefelova <kristin.fefelova@gmail.com>

--- services/msg2file/src/worker.ts
+++ services/msg2file/src/worker.ts
@@ -175,7 +175,7 @@ async function applyPatchesToGroup (
     const file = await getFile(storage, ctx, workspace, group.blobId)
     const parsedFile = await parseFileStream(file)
     const patchesByMessage = groupByArray(group.patches, (it) => it.message)
-    const updatedMessages: Message[] = parsedFile.messages.map((message) => {
+    const updatedMessages = parsedFile.messages.map((message) => {
       const patches = patchesByMessage.get(message.id) ?? []
       if (patches.length === 0) {
         return message
@@ -183,7 +183,7 @@ async function applyPatchesToGroup (
         return applyPatches(message, patches)
       }
     })
-    const blob = await uploadGroupFile(ctx, storage, workspace, parsedFile.metadata, updatedMessages)
+    const blob = await uploadGroupFile(ctx, storage, workspace, parsedFile.metadata, updatedMessages.map(deserializeMessage))
     await createGroup(
       client,
       group.card,
@@ -253,6 +253,14 @@ async function newMessages2file (
       messagesFromExistingGroup.length > 0 ? messages.slice(messagesFromExistingGroup.length) : messages
 
     await pushMessagesToExistingGroup(client, card, messagesFromExistingGroup, ctx, storage, workspace)
+
+    if (messages.length < config.MinSyncMessagesCount) {
+      const ids = [...messagesFromExistingGroup].map((it) => it.id)
+      await removeMessages(db, workspace, card._id, [...ids])
+      await removePatches(db, workspace, card._id, [...ids])
+      break
+    }
+
     const savedMessages = await createNewGroup(client, card, newMessages, ctx, storage, workspace)
 
     const ids = [...messagesFromExistingGroup, ...savedMessages].map((it) => it.id)
