ebb33fa9a27c643d84d53e155714c042b054989d
Alexey Aristov <aav@acm.org>
2025-05-27 14:28:44 +0200

    minor change (for gh troubleshooting)
    Signed-off-by: Alexey Aristov <aav@acm.org>

--- plugins/telegram-resources/src/api.ts
+++ plugins/telegram-resources/src/api.ts
@@ -10,6 +10,7 @@
 //
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
 
 import { concatLink } from '@hcengineering/core'
 import { getMetadata } from '@hcengineering/platform'
