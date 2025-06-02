51cc82b175858e992326bbe6b636e89bfaf65a05
Andrey Sobolev <haiodo@users.noreply.github.com>
2025-06-02 17:56:17 +0700

    Enable cpu profiling for ui tests (#9149)
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- ws-tests/profile-start.sh
+++ ws-tests/profile-start.sh
@@ -1,3 +1,2 @@
 echo "Start profiling on server"
-token=$(./tool.sh generate-token --admin anticrm@hc.engineering sanity-ws)
-curl -X PUT "http://huly.local:3334/api/v1/manage?token=${token}&operation=profile-start"
+./tool-europe.sh profile http://localhost:3335 start
