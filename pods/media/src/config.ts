8fbcf07855f0c0ed9fb31816bac2a7b3312959b1
Alexander Onnikov <Alexander.Onnikov@xored.com>
2025-06-11 12:45:42 +0700

    feat: use queue for transcoding tasks
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

---
+++ pods/media/src/config.ts
@@ -0,0 +1,37 @@
+//
+// Copyright © 2025 Hardcore Engineering Inc.
+//
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+import { config as dotenvConfig } from 'dotenv'
+
+dotenvConfig()
+
+export interface Config {
+  Secret: string
+}
+
+const config: Config = (() => {
+  const params: Partial<Config> = {
+    Secret: process.env.SECRET
+  }
+
+  const missingEnv = (Object.keys(params) as Array<keyof Config>).filter((key) => params[key] === undefined)
+
+  if (missingEnv.length > 0) {
+    throw Error(`Missing config for attributes: ${missingEnv.join(', ')}`)
+  }
+
+  return params as Config
+})()
+
+export default config
