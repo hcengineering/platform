ea7ea41bee313a2e970f20a683fd797131c4bb71
Alexey Zinoviev <alexey.zinoviev@xored.com>
2025-07-14 17:45:02 +0700

    uberf-12325: move qms tests to CR, use CR db for accounts in workspace tests Signed-off-by: Alexey Zinoviev <alexey.zinoviev@xored.com>
    Signed-off-by: Alexey Zinoviev <alexey.zinoviev@xored.com>

--- qms-tests/tool-local.sh
+++ qms-tests/tool-local.sh
@@ -3,8 +3,8 @@ export MINIO_ACCESS_KEY=minioadmin
 export MINIO_SECRET_KEY=minioadmin
 export MINIO_ENDPOINT=localhost:9000
 export MONGO_URL=mongodb://localhost:27017
-export DB_URL=mongodb://localhost:27017
-export ACCOUNT_DB_URL=mongodb://localhost:27017
+export DB_URL=postgresql://root@localhost:26257/defaultdb?sslmode=disable
+export ACCOUNT_DB_URL=postgresql://root@localhost:26257/defaultdb?sslmode=disable
 export ACCOUNTS_URL=http://localhost:3000
 export TRANSACTOR_URL=ws://localhost:3333
 export SERVER_SECRET=secret
