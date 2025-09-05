be7df4aef1e194eec6b8748f60ecefed0927d597
Alexander Onnikov <aonnikov@hardcoreeng.com>
2025-09-05 23:36:11 +0700

    EQMS-1642 Show training title in tables (#9793)
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

---
+++ plugins/training-resources/src/components/TrainingTitlePresenter.svelte
@@ -0,0 +1,22 @@
+<!--
+  Copyright © 2023 Hardcore Engineering Inc.
+-->
+
+<script lang="ts">
+  import { type Training } from '@hcengineering/training'
+  import { DocNavLink } from '@hcengineering/view-resources'
+
+  export let value: Training
+  export let disabled: boolean = false
+  export let accent: boolean = false
+</script>
+
+{#if value}
+  <div class="content-halfcontent-color">
+    <DocNavLink object={value} {disabled} noOverflow accent>
+      <span class="whitespace-nowrap" class:fs-bold={accent}>
+        {value.title}
+      </span>
+    </DocNavLink>
+  </div>
+{/if}
