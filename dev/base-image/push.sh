aa3d3552c0ba4a9f7f90698a50d0fea795999ef7
Alexander Onnikov <Alexander.Onnikov@xored.com>
2025-09-15 13:03:16 +0700

    add slim base image
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

--- dev/base-image/push.sh
+++ dev/base-image/push.sh
@@ -4,6 +4,7 @@
 VERSION=${VERSION:-"latest"}
 
 docker push hardcoreeng/base:${VERSION}
+docker push hardcoreeng/base-slim:${VERSION}
 docker push hardcoreeng/rekoni-base:${VERSION}
 docker push hardcoreeng/print-base:${VERSION}
 docker push hardcoreeng/front-base:${VERSION}
