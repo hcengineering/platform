5bf4ff640b1c2f8adb870b09d3ea1c7b353c8a1a
Alexey Zinoviev <alexey.zinoviev@xored.com>
2025-09-17 12:04:30 +0400

    uberf-13587: remove home deps Signed-off-by: Alexey Zinoviev <alexey.zinoviev@xored.com>

--- dev/prod/src/platform.ts
+++ dev/prod/src/platform.ts
@@ -78,7 +78,6 @@ import {emojiId} from '@hcengineering/emoji'
 import billingPlugin, {billingId} from '@hcengineering/billing'
 import { hulyMailId } from '@hcengineering/huly-mail'
 import { aiAssistantId } from '@hcengineering/ai-assistant'
-import {homeId} from '@hcengineering/home'
 
 import '@hcengineering/activity-assets'
 import '@hcengineering/analytics-collector-assets'
@@ -136,7 +135,6 @@ import '@hcengineering/emoji-assets'
 import '@hcengineering/billing-assets'
 import '@hcengineering/huly-mail-assets'
 import '@hcengineering/ai-assistant-assets'
-import '@hcengineering/home-assets'
 
 import { coreId } from '@hcengineering/core'
 import presentation, {
@@ -380,7 +378,6 @@ function configureI18n(): void {
   addStringsLoader(billingId, async (lang: string) => await import(`@hcengineering/billing-assets/lang/${lang}.json`))
   addStringsLoader(hulyMailId, async (lang: string) => await import(`@hcengineering/huly-mail-assets/lang/${lang}.json`))
   addStringsLoader(aiAssistantId, async (lang: string) => await import(`@hcengineering/ai-assistant-assets/lang/${lang}.json`))
-  addStringsLoader(homeId, async (lang: string) => await import(`@hcengineering/home-assets/lang/${lang}.json`))
 }
 
 export async function configurePlatform() {
@@ -593,7 +590,6 @@ export async function configurePlatform() {
   addLocation(billingId, () => import(/* webpackChunkName: "achievement" */ '@hcengineering/billing-resources'))
   addLocation(hulyMailId, () => import(/* webpackChunkName: "achievement" */ '@hcengineering/huly-mail-resources'))
   addLocation(aiAssistantId, () => import(/* webpackChunkName: "achievement" */ '@hcengineering/ai-assistant-resources'))
-  addLocation(homeId, () => import(/* webpackChunkName: "home" */ '@hcengineering/home-resources'))
 
   setMetadata(client.metadata.FilterModel, 'ui')
   setMetadata(client.metadata.ExtraPlugins, ['preference' as Plugin])
