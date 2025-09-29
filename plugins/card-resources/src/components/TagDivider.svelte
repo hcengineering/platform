6953b821565a3d02dc5771a084907a27e36e179b
Artem Savchenko <armisav@gmail.com>
2025-09-29 17:39:01 +0700

    Update card feed view
    Signed-off-by: Artem Savchenko <armisav@gmail.com>

--- plugins/card-resources/src/components/TagDivider.svelte
+++ plugins/card-resources/src/components/TagDivider.svelte
@@ -11,7 +11,7 @@
 <!-- See the License for the specific language governing permissions and -->
 <!-- limitations under the License. -->
 
-<div class="divider"/>
+<div class="divider" />
 
 <style lang="scss">
   .divider {
