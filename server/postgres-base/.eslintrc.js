f351ee1404603ab802a791d37af9c802ad4ac5e4
Andrey Sobolev <haiodo@users.noreply.github.com>
2025-10-01 14:52:13 +0700

    use of postgress-base (#9989)
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- server/postgres-base/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['./node_modules/@hcengineering/platform-rig/profiles/node/eslint.config.json'],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: './tsconfig.json'
-  }
-}
