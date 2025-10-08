4cc3dde93f6c89707487fdfa4648ec0f4ad267b5
Andrey Sobolev <haiodo@gmail.com>
2025-10-08 20:37:54 +0700

    Add connection tests

--- packages/client-resources/jest.config.js
+++ packages/client-resources/jest.config.js
@@ -3,5 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   roots: ['./src'],
-  coverageReporters: ['text-summary', 'html', 'lcov']
+  coverageReporters: ['text-summary', 'html', 'lcov'],
+  testTimeout: 10000, // 10 seconds timeout for all tests
+  forceExit: true // Force exit after tests complete to handle lingering timers
 }
--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   roots: ['./src'],
-  coverageReporters: ['text-summary', 'html', 'lcov']
+  coverageReporters: ['text-summary', 'html', 'lcov'],
+  testTimeout: 10000 // 10 seconds timeout for all tests
 }
