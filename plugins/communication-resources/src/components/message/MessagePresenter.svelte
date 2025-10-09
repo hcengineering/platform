e495b4c45d7cd12f3db91d36a3a5a57e55950cf3
Kristina Fefelova <kristin.fefelova@gmail.com>
2025-10-10 02:06:36 +0400

    Fix ui
    Signed-off-by: Kristina Fefelova <kristin.fefelova@gmail.com>

--- plugins/communication-resources/src/components/message/MessagePresenter.svelte
+++ plugins/communication-resources/src/components/message/MessagePresenter.svelte
@@ -148,7 +148,6 @@
   $: showActions = !isEditing && !readonly
 </script>
 
-{message.language}
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
