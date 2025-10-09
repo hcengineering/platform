8e5be85a81812f6ddfc7fadd2a92ff698844f926
Andrey Sobolev <haiodo@gmail.com>
2025-10-09 23:22:16 +0700

    Remove moved to huly.server packages
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- server/elastic/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-  roots: ["./src"],
-  coverageReporters: ["text-summary", "html"]
-}
