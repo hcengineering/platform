a60a3fa264b1e8c16964c1c3e314338b10dd3be9
Alexey Zinoviev <alexey.zinoviev@xored.com>
2025-10-13 12:55:43 +0400

    Chore: improve sass warning suppression for dev-server Signed-off-by: Alexey Zinoviev <alexey.zinoviev@xored.com>
    Signed-off-by: Alexey Zinoviev <alexey.zinoviev@xored.com>

--- common/scripts/sass-quiet.js
+++ common/scripts/sass-quiet.js
@@ -1,22 +1,31 @@
 // Wrapper around sass to suppress legacy-js-api deprecation warnings
 // Should be removed when sass-preprocessor is upgraded to the version that works with modern sass api
-const sass = require('sass');
 
-// Capture original stderr.write
-const originalStderrWrite = process.stderr.write.bind(process.stderr);
-
-// Override stderr.write to filter out sass deprecation warnings
-process.stderr.write = (chunk, encoding, callback) => {
-  if (typeof chunk === 'string' && chunk.includes('DEPRECATION WARNING [legacy-js-api]')) {
-    // Suppress the warning
-    if (typeof encoding === 'function') {
-      encoding();
-    } else if (typeof callback === 'function') {
-      callback();
+// Install the stderr filter globally as early as possible
+if (!process.stderr._originalWrite) {
+  process.stderr._originalWrite = process.stderr.write;
+  
+  process.stderr.write = function (chunk, encoding, callback) {
+    const chunkStr = typeof chunk === 'string' ? chunk : (chunk?.toString?.() || '');
+    
+    // Filter out sass legacy API deprecation warnings
+    if (chunkStr.includes('Deprecation Warning [legacy-js-api]') || 
+        chunkStr.includes('DEPRECATION WARNING [legacy-js-api]') ||
+        chunkStr.includes('The legacy JS API is deprecated') ||
+        chunkStr.includes('More info: https://sass-lang.com/d/legacy-js-api')) {
+      // Suppress the warning - just call the callback
+      if (typeof encoding === 'function') {
+        encoding();
+      } else if (typeof callback === 'function') {
+        callback();
+      }
+      return true;
     }
-    return true;
-  }
-  return originalStderrWrite(chunk, encoding, callback);
-};
+    
+    return process.stderr._originalWrite.call(this, chunk, encoding, callback);
+  };
+}
+
+const sass = require('sass');
 
 module.exports = sass;
