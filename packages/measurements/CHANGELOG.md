48b57fa16302e2cc6c70ac1aac3ef668109d5bf8
Andrey Sobolev <haiodo@gmail.com>
2025-10-16 00:56:03 +0700

    Move packages to core, since they are not utils

---
+++ packages/measurements/CHANGELOG.md
@@ -0,0 +1,30 @@
+# Change Log - @hcengineering/measurements
+
+This log was last generated on Tue, 14 Oct 2025 03:02:36 GMT and should not be manually modified.
+
+## 0.7.13
+Tue, 14 Oct 2025 03:02:36 GMT
+
+### Patches
+
+- Remove zero value measure with override values
+
+## 0.7.12
+Sat, 11 Oct 2025 19:18:56 GMT
+
+### Patches
+
+- rollback eslint
+
+## 0.7.11
+Sat, 11 Oct 2025 17:58:53 GMT
+
+### Patches
+
+- Fix eslint deps
+
+## 0.7.10
+Fri, 10 Oct 2025 12:32:59 GMT
+
+_Initial release_
+
---
+++ packages/measurements/src/types.ts
@@ -0,0 +1,132 @@
+/**
+ * @public
+ */
+export type ParamType = string | number | boolean | undefined
+
+/**
+ * @public
+ */
+export type ParamsType = Record<string, ParamType>
+
+/**
+ * @public
+ */
+export type FullParamsType = Record<string, any>
+
+/**
+ * @public
+ */
+export interface MetricsData {
+  operations: number
+  value: number
+  topResult?: {
+    value: number
+    time?: number
+    params: FullParamsType
+  }[]
+}
+
+export interface OperationLogEntry {
+  op: string
+  params: ParamsType
+  start: number
+  end: number
+}
+export interface OperationLog {
+  ops: OperationLogEntry[]
+  start: number
+  end: number
+}
+
+/**
+ * @public
+ */
+export interface Metrics extends MetricsData {
+  namedParams: ParamsType
+  params: Record<string, Record<string, MetricsData>>
+  measurements: Record<string, Metrics>
+
+  opLog?: Record<string, OperationLog>
+}
+
+/**
+ * @public
+ */
+export interface MeasureLogger {
+  info: (message: string, obj?: Record<string, any>) => void
+  error: (message: string, obj?: Record<string, any>) => void
+
+  warn: (message: string, obj?: Record<string, any>) => void
+
+  logOperation: (operation: string, time: number, params: ParamsType) => void
+
+  childLogger?: (name: string, params: Record<string, any>) => MeasureLogger
+
+  close: () => Promise<void>
+}
+
+export interface WithOptions {
+  span?: true | false | 'disable' | 'skip' | 'inherit' // 'none' means no span will be created, 'disable' means context will be tracing disabled
+  log?: boolean
+  inheritParams?: boolean
+
+  // Passed context metadata
+  meta?: Record<string, string | number | boolean>
+}
+
+/**
+ * @public
+ */
+export interface MeasureContext<Q = any> {
+  id?: string
+
+  // Context data will be copied referenced for all child contexts.
+  contextData: Q
+  // Create a child metrics context
+  newChild: (
+    name: string,
+    params: ParamsType,
+    opt?: {
+      fullParams?: FullParamsType
+      logger?: MeasureLogger
+      span?: WithOptions['span'] // By default true
+      meta?: Record<string, string | number | boolean>
+    }
+  ) => MeasureContext
+
+  metrics?: Metrics
+
+  with: <T>(
+    name: string,
+    params: ParamsType,
+    op: (ctx: MeasureContext<Q>) => T | Promise<T>,
+    fullParams?: FullParamsType | (() => FullParamsType),
+    opt?: WithOptions
+  ) => Promise<T>
+
+  withSync: <T>(
+    name: string,
+    params: ParamsType,
+    op: (ctx: MeasureContext<Q>) => T,
+    fullParams?: FullParamsType | (() => FullParamsType),
+    opt?: WithOptions
+  ) => T
+
+  extractMeta: () => Record<string, string | number | boolean>
+
+  logger: MeasureLogger
+
+  parent?: MeasureContext
+  getParams: () => ParamsType
+
+  measure: (name: string, value: number, override?: boolean) => void
+
+  // Capture error
+  error: (message: string, obj?: Record<string, any>) => void
+  info: (message: string, obj?: Record<string, any>) => void
+  warn: (message: string, obj?: Record<string, any>) => void
+
+  // Mark current context as complete
+  // If no value is passed, time difference will be used.
+  end: (value?: number) => void
+}
