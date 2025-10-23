7875691caa8371af022358d4b1f21e2619f7214b
Artyom Savchenko <armisav@gmail.com>
2025-10-23 09:04:16 +0700

    Support favorite card types (#10130)

--- models/card/src/index.ts
+++ models/card/src/index.ts
@@ -23,6 +23,7 @@ import {
   type CreateCardExtension,
   DOMAIN_CARD,
   type FavoriteCard,
+  type FavoriteType,
   type MasterTag,
   type ParentInfo,
   type Role,
@@ -169,6 +170,11 @@ export class TRole extends TAttachedDoc implements Role {
 export class TFavoriteCard extends TPreference implements FavoriteCard {
   declare attachedTo: Ref<Card>
   application!: string
+}
+
+@Model(card.class.FavoriteType, preference.class.Preference)
+export class TFavoriteType extends TPreference implements FavoriteType {
+  declare attachedTo: Ref<MasterTag>
 }
 
 @Mixin(card.mixin.CreateCardExtension, card.class.MasterTag)
@@ -347,6 +353,7 @@ export function createModel (builder: Builder): void {
     TCardSection,
     TCardViewDefaults,
     TFavoriteCard,
+    TFavoriteType,
     TCreateCardExtension
   )
 
