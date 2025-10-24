7fab3b963894b4aa3c78e0fa04406f96eba79097
Alexander Onnikov <Alexander.Onnikov@xored.com>
2025-10-24 17:54:19 +0700

    fix: recreate root bar extensions on workspace change
    Signed-off-by: Alexander Onnikov <Alexander.Onnikov@xored.com>

--- packages/ui/src/components/internal/RootBarExtension.svelte
+++ packages/ui/src/components/internal/RootBarExtension.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { location as locationStore } from '../../location'
+  import { location as locationStore, workspaceId } from '../../location'
   import { rootBarExtensions } from '../../utils'
   import Component from '../Component.svelte'
 
@@ -15,18 +15,20 @@
   $: sorted = $rootBarExtensions.sort((a, b) => a[1].order - b[1].order)
 </script>
 
-{#each sorted as ext (ext[1].id)}
-  {#if ext[0] === position}
-    <div id={ext[1].id} class="clear-mins">
-      <Component
-        is={ext[1].component}
-        props={ext[1].props}
-        on:close={() => {
-          rootBarExtensions.update((cur) => {
-            return cur.filter((it) => it[1].id !== ext[1].id)
-          })
-        }}
-      />
-    </div>
-  {/if}
-{/each}
+{#key $workspaceId}
+  {#each sorted as ext (ext[1].id)}
+    {#if ext[0] === position}
+      <div id={ext[1].id} class="clear-mins">
+        <Component
+          is={ext[1].component}
+          props={ext[1].props}
+          on:close={() => {
+            rootBarExtensions.update((cur) => {
+              return cur.filter((it) => it[1].id !== ext[1].id)
+            })
+          }}
+        />
+      </div>
+    {/if}
+  {/each}
+{/key}
