3df5362f33cfe1e33acf4caa4c4aea0c0d432fb1
Andrey Sobolev <haiodo@gmail.com>
2025-11-08 19:23:13 +0700

    Take upstream script

---
+++ scripts/takeUpstream.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git subtree pull --prefix=foundations/utils git@github.com:hcengineering/huly.utils.git main
+git subtree pull --prefix=foundations/core git@github.com:hcengineering/huly.core.git main
+git subtree pull --prefix=foundations/server git@github.com:hcengineering/huly.server.git main
+git subtree pull --prefix=foundations/net git@github.com:hcengineering/huly.net.git main
+
+git subtree pull --prefix=foundations/hulylake git@github.com:hcengineering/hulylake.git master
+git subtree pull --prefix=foundations/communication git@github.com:hcengineering/communication.git main
