6d88298cbfa4f0c54f7033b344da54f6c1dca7ef
Andrey Sobolev <haiodo@gmail.com>
2025-11-08 19:25:30 +0700

    Add HulyPulse

--- scripts/takeUpstream.sh
+++ scripts/takeUpstream.sh
@@ -6,4 +6,5 @@ git subtree pull --prefix=foundations/server git@github.com:hcengineering/huly.s
 git subtree pull --prefix=foundations/net git@github.com:hcengineering/huly.net.git main
 
 git subtree pull --prefix=foundations/hulylake git@github.com:hcengineering/hulylake.git master
+git subtree pull --prefix=foundations/hulypulse git@github.com:hcengineering/hulypulse.git main
 git subtree pull --prefix=foundations/communication git@github.com:hcengineering/communication.git main
