c55ab0c6f805c4de842e16365559eff279fad1da
Andrey Sobolev <haiodo@gmail.com>
2025-11-28 13:46:50 +0700

    Fix diff and fix build
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- scripts/git-diff.sh
+++ scripts/git-diff.sh
@@ -4,7 +4,7 @@ outDir=$(pwd)/${2:-"diff"}
 rm -rf "$outDir"
 
 mkdir -p "$outDir"
-git diff --name-only $1 | grep -v .json | grep -v .md | while read p
+git diff --name-only $1 | grep -v .json | grep -v .md | grep -v bitrix | grep -v board | grep -v Dockerfile | grep -v qms- | while read p
 do
     base=$(dirname $p)
     echo "Processing $p"
