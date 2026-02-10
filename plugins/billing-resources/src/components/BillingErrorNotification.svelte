cb94f5c03ea9b0a5b61bc4a779f46e97f6e2e4f7
Artem Savchenko <armisav@gmail.com>
2026-02-10 17:11:14 +0700

    Fix year in copyright
    Signed-off-by: Artem Savchenko <armisav@gmail.com>

--- plugins/billing-resources/src/components/BillingErrorNotification.svelte
+++ plugins/billing-resources/src/components/BillingErrorNotification.svelte
@@ -1,6 +1,6 @@
 <!--
 //
-// Copyright © 2025 Hardcore Engineering Inc.
+// Copyright © 2026 Hardcore Engineering Inc.
 //
 // Licensed under the Eclipse Public License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License. You may
