4f7419d93d413b0c1940710f13a22bc1d5116b3f
Denis Bykhov <bykhov.denis@gmail.com>
2026-02-28 20:10:53 +0500

    Fix patch)
    Signed-off-by: Denis Bykhov <bykhov.denis@gmail.com>

--- common/scripts/docker_patch.sh
+++ common/scripts/docker_patch.sh
@@ -4,12 +4,17 @@
 # It can be run interactively or with the --version flag.
 
 VERSION=""
+PLATFORM="linux/amd64"
 REMAINING_ARGS=()
 
 while [[ $# -gt 0 ]]; do
   case $1 in
     --version)
       VERSION="$2"
+      shift 2
+      ;;
+    --platform)
+      PLATFORM="$2"
       shift 2
       ;;
     *)
@@ -29,7 +34,8 @@ if [ -z "$VERSION" ]; then
 fi
 
 export DOCKER_VERSION="$VERSION"
-echo "Starting docker build and push for version: $DOCKER_VERSION"
+export DOCKER_EXTRA="--platform=$PLATFORM"
+echo "Starting docker build and push for version: $DOCKER_VERSION ($PLATFORM)"
 
 # We use 'node common/scripts/install-run-rush.js' to ensure we use the right rush version
 node "$(dirname "$0")/install-run-rush.js" docker:build "${REMAINING_ARGS[@]}"
