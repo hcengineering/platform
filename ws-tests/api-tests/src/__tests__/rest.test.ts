0a7abd8a65b4d25f9d1c7c5799ae592bbb29f313
Artyom Savchenko <armisav@gmail.com>
2026-03-23 15:33:06 +0700

    Try to fix unstable 'find avg' tests (#10669)
    * Try to fix unstable 'find avg' tests
    
    Signed-off-by: Artem Savchenko <armisav@gmail.com>
    
    * Clean up
    
    Signed-off-by: Artem Savchenko <armisav@gmail.com>
    
    ---------
    
    Signed-off-by: Artem Savchenko <armisav@gmail.com>

--- ws-tests/api-tests/src/__tests__/rest.test.ts
+++ ws-tests/api-tests/src/__tests__/rest.test.ts
@@ -175,12 +175,12 @@ describe('rest-api-server', () => {
   it('find avg', async () => {
     const conn = connect()
     await checkFindPerformance(conn) // 5ms max per operation
-  })
+  }, 10000)
 
   it('find avg-europe', async () => {
     const conn = connect(apiWorkspace2)
     await checkFindPerformance(conn) // 5ms max per operation
-  })
+  }, 10000)
 
   it('add space', async () => {
     const conn = connect()
