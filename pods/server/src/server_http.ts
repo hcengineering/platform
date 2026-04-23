dfa360b240e46449aec47ffb3a38ade154d22b4a
Don Kendall <dkendall@ledoweb.com>
2026-04-23 19:36:03 -0400

    fix: restore (s as any) cast in server_http.ts removed by ESLint autofix
    Signed-off-by: Don Kendall <dkendall@ledoweb.com>

--- pods/server/src/server_http.ts
+++ pods/server/src/server_http.ts
@@ -515,7 +515,8 @@ export function startHttpServer (
           }, 1000)
         }
         if ('upgrade' in s) {
-          void cs.send(ctx, { id: -1, result: { state: 'upgrading', stats: s.upgradeInfo } }, false, false).then(() => {
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+          void cs.send(ctx, { id: -1, result: { state: 'upgrading', stats: (s as any).upgradeInfo } }, false, false).then(() => {
             cs.close()
           })
         }
